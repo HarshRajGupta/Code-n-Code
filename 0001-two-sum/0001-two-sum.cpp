08a1b05731259a3d10be0dfde5f3fd799079057f
Harsh Raj Gupta <1amharsh.rg@gmail.com>
2022-10-23 09:53:54 +0530

    Time: 36 ms (50.03%), Space: 11.4 MB (15.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,8 +6,7 @@ public:
             if(mp[target-nums[i]] != 0) {
                 return {mp[target-nums[i]] - 1, i};
             }
-            else
-                mp[nums[i]] = i+1;
+            mp[nums[i]] = i+1;
         }
      return {};   
     }
