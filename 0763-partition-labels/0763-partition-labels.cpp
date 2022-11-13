3f0381b322908c60289e8b10eba1bdab4bbb9881
Harsh Raj Gupta <1amharsh.rg@gmail.com>
2022-11-13 12:53:43 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (68.42%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -3,27 +3,19 @@ class Solution
     public:
         vector<int> partitionLabels(string s)
         {
-            vector<vector < int>> posi(26), preCompute(s.size(), vector<int> (26));
-            vector<int> prev(26), lastPos(26), res;
-            for (int i = 0; i < s.size(); ++i)
-            {
-                posi[s[i] - 'a'].push_back(i);
-                prev[s[i] - 'a']++;
-                preCompute[i] = prev;
-                lastPos[s[i]-'a'] = i;
+            vector < int> posi(26, -1);
+            for(int i = 0; i < s.size(); ++i) {
+                posi[s[i]-'a'] = i;
             }
-            string t = s;
-            int pos = 0, cnt = 0, MAX = 0;
-            while(pos < s.size()) {
-                int MAX = max(MAX, lastPos[s[pos]-'a']);
-                pos++;
-                cnt++;
-                cout << pos << " "<< MAX << '\n';
-                if (MAX < pos) {
-                    res.push_back(cnt);
-                    cnt = 0;
+            int r = 0, p = -1;
+            vector<int> ans;
+            for(int i = 0; i < s.size(); ++i) {
+                r = max(r, posi[s[i]-'a']);
+                if (r == i) {
+                    ans.push_back(r-p);
+                    p = r;
                 }
             }
-            return res;
+            return ans;
         }
 };
