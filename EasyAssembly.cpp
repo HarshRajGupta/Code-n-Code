8c5f71fed20692d8493385c5d9f09a3b3a7f6afa
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-12-14 07:20:42 +0530

    EasyAssembly

--- EasyAssembly.cpp
+++ EasyAssembly.cpp
@@ -50,7 +50,7 @@ void solve(void) {
     int split = 0;
     _for(i, n) {
         _for(j, sz(arr[i]) - 1) {
-            // debug(*upper_bound(all(a), arr[i][j]), *lower_bound(all(a), arr[i][j + 1]))
+            debug(*upper_bound(all(a), arr[i][j]), *lower_bound(all(a), arr[i][j + 1]))
             if (*upper_bound(all(a), arr[i][j]) != *lower_bound(all(a), arr[i][j + 1])) {
                 split++;
             }
