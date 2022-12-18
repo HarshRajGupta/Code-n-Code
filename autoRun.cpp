272ad4e967f931b0f49cd8e69d3c01575b7701b6
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-12-18 21:11:55 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -43,6 +43,7 @@ public:
             else pos.push(A[i]);
         }
         for (int i = 0; i < N / 2; ++i) {
+            debug(i, pos, neg, maxi, mini)
             int m = neg.empty() || pos.empty();
             if (m == 0) {
                 ans.push(pos.top() * neg.top());
@@ -50,7 +51,6 @@ public:
                 neg.pop();
             }
             else {
-                debug(i, pos, neg, maxi, mini)
                 while (!pos.empty()) {
                     maxi.push(pos.top());
                     mini.push(pos.top());
