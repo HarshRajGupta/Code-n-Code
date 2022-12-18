9444dbcfcf3261ef54bc122e12cb16cb2b041b32
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-12-18 08:43:33 +0530

    leetCode_biweekly@18Dec

--- leetCode_biweekly@18Dec.cpp
+++ leetCode_biweekly@18Dec.cpp
@@ -47,7 +47,8 @@ public:
             if (degree[i] % 2)
                 oddDeg.push_back(i);
         }
-        // debug(oddDeg)
+        debug(oddDeg)
+        cout << graph[3][4];
         if (oddDeg.size() != 0 || oddDeg.size() != 2 || oddDeg.size() != 4) return false;
         if (oddDeg.size() == 0) return true;
         if (oddDeg.size() == 2) {
