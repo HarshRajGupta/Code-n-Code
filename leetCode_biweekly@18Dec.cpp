e9c5503e4be93505dbbea41bc95db2b6e536b9d2
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-12-18 08:54:40 +0530

    leetCode_biweekly@18Dec

--- leetCode_biweekly@18Dec.cpp
+++ leetCode_biweekly@18Dec.cpp
@@ -41,6 +41,7 @@ public:
             degree[i[0] - 1]++;
             ++degree[i[1] - 1];
             graph[i[0] - 1][i[1] - 1] = true;
+            graph[i[1] - 1][i[0] - 1] = true;
         }
         for (auto i = 0; i < n; ++i) {
             if (degree[i] % 2)
