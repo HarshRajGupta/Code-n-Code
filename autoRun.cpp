d41a5c1f0989eacdf2e8c217a0efb874a1399918
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-12-24 21:36:41 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -1,4 +1,136 @@
-#ifndef __FAST_IO
+// #ifndef __FAST_IO
+// #pragma GCC optimize("O3", "Ofast", "unroll-loops", "omit-frame-pointer")
+// #pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
+// #endif
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// #ifndef debug
+// #define debug(...) ;
+// #endif
+
+// const uint64_t MOD = 1e9 + 7;
+// const char ln = '\n';
+
+// #define int long long
+// #define ll long double
+
+// #define Y std::cout << "YES";
+// #define N std::cout << "NO";
+
+// #define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
+// #define rep(i, a, n) for(auto i = a; i < n; ++i)
+// #define bw(i, n) for(int32_t i = (int32_t)n; i >= 0; --i)
+
+// #define v std::vector
+// #define maxHeap std::priority_queue<int>
+// #define minHeap std::priority_queue<int, std::vector<int>, std::greater<int>>
+
+// #define ft first
+// #define sd second
+// #define sz(x) ((int)(x).size())
+// #define all(x) (x).begin(), (x).end()
+
+// int findNonMultiples(int arr[],
+//                      int n, int k)
+// {
+//     // Stores all unique multiples
+//     set<int> multiples;
+//     // Iterate the array
+//     for (int i = 0; i < n; ++i) {
+//         debug(i)
+//         if (multiples.find(arr[i])
+//                 == multiples.end()) {
+
+//             for (int j = 1;
+//                     j <= k / arr[i]; j++) {
+//                 multiples.insert(arr[i] * j);
+//             }
+//         }
+//     }
+
+//     // Returning only the count of
+//     // numbers that are not divisible
+//     // by any of the array elements
+//     return k - multiples.size();
+// }
+
+// // Function to count the total values
+// // in the range [L, R]
+// int countValues(int x, int y, int r) {
+//     int arr[] = {x, y};
+//     int n = 2;
+//     return findNonMultiples(arr, n, r);
+// }
+// int xNotY(int x, int y, int n) {
+//     int divX = n / x;
+//     int divLCM = n / ((x * y) / __gcd(x, y));
+//     return (divX - divLCM);
+// }
+// int notXY(int c, int d, int c) {
+//     int x = b / c;
+//     int y = b / d;
+//     int k = (c * d) / __gcd(c, d);
+//     int z = b / k;
+//     return b + 1 - x - y + z;
+// }
+// int tot1(int x, int y, int n) {
+//     int ans = xNotY(x, y, n) + xNotY(y, x, n) + notXY(x, y, n);
+//     // cout << n << " " << ans << " " << xNotY(x, y, n) << " " << xNotY(y, x, n) << " " << notXY(1, n, x, y) << endl;
+//     return ans;
+// }
+// int tot(int x, int y, int n) {
+//     int ans = xNotY(x, y, n) + xNotY(y, x, n) + notXY(x, y, n);
+//     // cout << n << " " << ans << " " << xNotY(x, y, n) << " " << xNotY(y, x, n) << " " << notXY(1, n, x, y) << endl;
+//     return ans;
+// }
+
+// int minimizeSet(int d1, int d2, int u1, int u2) {
+//     int l = 0, r = INT_MAX;
+//     while (l <= r) {
+//         int mid = (l + r) / 2;
+//         debug(l, r)
+//         int ans = tot1(d1, d2, mid);
+//         if (ans == u1 + u2) {
+//             l = mid;
+//             break;
+//         }
+//         else if (ans > u1 + u2) {
+//             r = mid - 1;
+//         } else l = mid + 1;
+//     }
+//     int ans = max(l, r);
+//     cout << l << " " << r << endl;
+//     for (int i = ans - 1; i >= 1; --i) {
+//         cout << i << endl;
+//         int a = tot(d1, d2, i);
+//         if (a >= u1 + u2)
+//             ans = i;
+//         else break;
+//     }
+//     return ans;
+// }
+// void solve(void) {
+//     /* Code */
+//     cout << minimizeSet(2, 4, 3, 7);
+//     cout << 1;
+// }
+
+// signed main(void) {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+// #ifdef __TAG1
+//     __TAG1
+// #endif
+//     solve();
+// #ifdef __TAG2
+//     __TAG2
+// #endif
+//     return 0;
+// }
+
+#ifndef __FAST_IO 
 #pragma GCC optimize("O3", "Ofast", "unroll-loops", "omit-frame-pointer")
 #pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
 #endif
@@ -32,60 +164,12 @@ const char ln = '\n';
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-int notXY(int a, int b, int c, int d) {
-    int x = b / c - (a - 1) / c;
-    int y = b / d - (a - 1) / d;
-    int k = (c * d) / __gcd(c, d);
-    int z = b / k - (a - 1) / k;
-    debug(x, y, z, k)
-    return b - a + 1 - x - y + z;
-}
-
-int findNonMultiples(int arr[],
-                     int n, int k)
-{
-    // Stores all unique multiples
-    set<int> multiples;
-
-    // Iterate the array
-    for (int i = 0; i < n; ++i) {
-
-        // For finding duplicates
-        // only once
-        if (multiples.find(arr[i])
-                == multiples.end()) {
-
-            // Inserting all multiples
-            // into the set
-            for (int j = 1;
-                    j <= k / arr[i]; j++) {
-                multiples.insert(arr[i] * j);
-            }
-        }
-    }
-
-    // Returning only the count of
-    // numbers that are not divisible
-    // by any of the array elements
-    return k - multiples.size();
-}
-
-// Function to count the total values
-// in the range [L, R]
-int countValues(int x, int y, int r){
-    int arr[] = {x, y};
-    int n = 2;
-    return findNonMultiples(arr, n, r)
-           - findNonMultiples(arr, n, 1);
-}
-
 void solve(void) {
-    /* Code */
-    cout << countValues(2, 4, 13);
+    cout << 1;
 }
 
 signed main(void) {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false); 
     cin.tie(NULL);
 #ifdef __TAG1
     __TAG1
