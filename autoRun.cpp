c3763e8dc12709d94538f71e99b960e2013ed193
HarshRajGupta <HarshRajGupta@hotmail.com>
2022-12-26 13:24:39 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -42,8 +42,8 @@ int notXY(int x, int y, int n) {
 int tot(int x, int y, int n, int u1, int u2) {
     int ans = notXY(x, y, n);
     if (u1 + u2 < ans) return 1;
-    ans += max(u1, xNotY(y, x, n));
-    ans += max(u2, xNotY(x, y, n));
+    ans += min(u1, xNotY(y, x, n));
+    ans += min(u2, xNotY(x, y, n));
     debug(x, y, n, u1, u2, xNotY(x, y, n), xNotY(y, x, n), notXY(x, y, n), ans)
     if (u1 + u2 < ans) return 1;
     if (u1 + u2 > ans) return -1;
