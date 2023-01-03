eb877f4a98fc7e51465b86faaa6adc01e7a13684
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-01-03 20:55:47 +0530

    hello@2023

--- hello@2023.cpp
+++ hello@2023.cpp
@@ -32,33 +32,45 @@ const char ln = '\n';
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
+v<v<int>> pre(1001, {0, 0});
 void ans(int n) {
-    if (n%2 == 0){
-        cout << "{-1, 1},\n";
+    if (n % 2 == 0) {
+        pre[n][0] = 1, pre[n][1] = -1;
         return;
     }
     for (int i = -5000; i < 0; ++i) {
         for (int j = 1; j <= 5000; ++j) {
             if (i + j == (((n + 1) / 2)*i + ((n - 1) / 2)*j)) {
-                cout << '{' << i << ", " << j << "}, \n";
+                pre[n][0] = i, pre[n][1] = j;
                 return;
             }
             if (i + j == (((n - 1) / 2)*i + ((n + 1) / 2)*j)) {
-                cout << '{' << j << ", " << i << "}, \n";
+                pre[n][0] = j, pre[n][1] = i;
                 return;
             }
         }
     }
-    debug(n)
 }
 
 void solve(void) {
-    for (int i = 3; i <= 1000; ++i) ans(i);
+    int n; cin >> n;
+    if (n <= 1 || n == 3) {
+        cout << "NO";
+        return;
+    } else {
+        cout << "YES\n";
+        _for(i, n) {
+            if (i & 1) cout << pre[n][1] << " ";
+            else cout << pre[n][0] << " ";
+        }
+    }
 }
 
 signed main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    pre[2][0] = 1, pre[2][1] = -1;
+    for (int i = 3; i <= 1000; ++i) ans(i);
 #ifdef __TAG1
     __TAG1
 #endif
