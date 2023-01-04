30a76e1b9b8c8e35b1635cc337c21db601924e92
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-01-04 22:08:18 +0530

    start72

--- start72.cpp
+++ start72.cpp
@@ -1,157 +1,48 @@
-#ifndef __FAST_IO
-#pragma GCC optimize("O3", "Ofast", "unroll-loops", "omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
-#endif
+#include <iostream>
+#include <vector>
 
-#include <bits/stdc++.h>
 using namespace std;
 
-#ifndef debug
-#define debug(...) ;
-#endif
-
-#ifndef __RUN
-#define __RUN solve(), std::cout << '\n'
-#endif
-
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define int long long
-#define ll long double
-
-#define Y std::cout << "YES";
-#define N std::cout << "NO";
-
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int32_t i = (int32_t)n; i >= 0; --i)
-
-#define v std::vector
-#define maxHeap std::priority_queue<int>
-#define minHeap std::priority_queue<int, std::vector<int>, std::greater<int>>
-
-#define ft first
-#define sd second
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-int pz(const int k, const int n) {
-    int res = 1;
-    _for(i, n) res *= k;
-    return res;
-}
-
-bool tp(const int k, int n) {
-    int l = log(n) / log(k), res = 1;
-    _for(i, l) res *= k;
-    if (res == n) return true;
-    return false;
-}
-
-bool powerSum(v<int> &arr, const int k, int n, map<int, int> np) {
-    debug(n)
-    if (np[n]) return false;
-    if (n == 0) return true;
-    int z = log(abs(n)) / log(k);
-    if (z >= sz(arr)) {
-        return false;
+// Returns the longest good sequence with respect to the given binary array.
+// If multiple possible longest sequences exist, any of them can be returned.
+vector<int> longestGoodSequence(vector<int>& array) {
+    int n = array.size();
+    // We will keep track of the current good sequence using a stack.
+    // The stack will store the indices of the good sequence.
+    vector<int> stack;
+    for (int i = 0; i < n; i++) {
+        // If the current element is a 1, we can always add it to the good sequence.
+        if (array[i] == 1) {
+            stack.push_back(i);
+        } else {
+            // If the current element is a 0, we need to check whether it is part of a good sequence.
+            // If it is, we will add it to the good sequence. Otherwise, we will discard it.
+            int cnt_ones = 0, cnt_zeros = 0;
+            for (int j = i; j < n; j++) {
+                if (array[j] == 1) {
+                    cnt_ones++;
+                } else {
+                    cnt_zeros++;
+                }
+                if (cnt_ones - cnt_zeros == j - i) {
+                    stack.push_back(i);
+                    break;
+                }
+            }
+        }
     }
-    np[n] = 1;
-    if (n > 0) {
-        if (tp(k, n)) {
-            if (arr[z] <= 0) {
-                arr[z]++;
-                np[n] = 0;
-                return true;
-            }
-            np[n] = 0;
-            return false;
-        }
-        auto t1 = arr, t2 = arr;
-        if (t1[z] <= 0ll) {
-            t1[z]++;
-            if (powerSum(t1, k, n - pz(k, z), np)) {
-                arr = t1;
-                np[n] = 0;
-                return true;
-            }
-        }
-        if (z + 1 >= sz(arr)) {
-            np[n] = 0;
-            return false;
-        }
-        if (t2[z + 1] <= 0) {
-            t2[z + 1]++;
-            if (powerSum(t2, k, n - pz(k, z + 1), np)) {
-                arr = t2;
-                np[n] = 0;
-                return true;
-            }
-        }
-        np[n] = 0;
-        return false;
-    } else {
-        int m = n * -1;
-        if (tp(k, m)) {
-            if (arr[z] >= 0) {
-                arr[z]--;
-                np[n] = 0;
-                return true;
-            }
-            np[n] = 0;
-            return false;
-        }
-        auto t1 = arr, t2 = arr;
-        if (t1[z] >= 0) {
-            --t1[z];
-            if (powerSum(t1, k, n + pz(k, z), np)) {
-                arr = t1;
-                np[n] = 0;
-                return true;
-            }
-        }
-        if (z + 1 >= sz(arr)) {
-            np[n] = 0;
-            return false;
-        }
-        if (t2[z + 1] >= 0) {
-            t2[z + 1]--;
-            if (powerSum(t2, k, n + pz(k, z + 1), np)) {
-                arr = t2;
-                np[n] = 0;
-                return true;
-            }
-        }
-        np[n] = 0;
-        return false;
-    }
-    np[n] = 0;
-    return false;
+    // The final good sequence is the one stored in the stack.
+    return stack;
 }
 
-void solve() {
-    int n, k, s; cin >> n >> k >> s;
-    v<int> arr(n);
-    map<int, int> np;
-    if (powerSum(arr, k, s, np)) {
-        _for(i, n) cout << arr[i] << " ";
-    } else {
-        cout << -2;
+int main() {
+    // Let's test the function with a sample input.
+    vector<int> array = {0, 1, 1, 0, 1, 1};
+    vector<int> result = longestGoodSequence(array);
+    cout << "Longest good sequence: ";
+    for (int i : result) {
+        cout << i + 1 << " ";
     }
-}
-
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-#ifdef __TAG1
-    __TAG1
-#endif
-    uint32_t tCs = 1u;
-    cin >> tCs;
-    for (uint32_t tC = 0u; tC++ < tCs; __RUN);
-#ifdef __TAG2
-    __TAG2
-#endif
+    cout << endl;
     return 0;
 }
