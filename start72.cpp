d1439c6f0e50d829f1ab5e55a5a0c0620cca626f
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-01-04 22:11:19 +0530

    start72

--- start72.cpp
+++ start72.cpp
@@ -1,10 +1,41 @@
-#include <iostream>
-#include <vector>
+#ifndef __FAST_IO
+#pragma GCC optimize("O3", "Ofast", "unroll-loops", "omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
+#endif
 
+#include <bits/stdc++.h>
 using namespace std;
 
-// Returns the longest good sequence with respect to the given binary array.
-// If multiple possible longest sequences exist, any of them can be returned.
+#ifndef debug
+#define debug(...) ;
+#endif
+
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+#define Y std::cout << "YES";
+#define N std::cout << "NO";
+
+#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int32_t i = (int32_t)n; i >= 0; --i)
+
+#define v std::vector
+#define maxHeap std::priority_queue<int>
+#define minHeap std::priority_queue<int, std::vector<int>, std::greater<int>>
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
 vector<int> longestGoodSequence(vector<int>& array) {
     int n = array.size();
     // We will keep track of the current good sequence using a stack.
@@ -35,14 +66,27 @@ vector<int> longestGoodSequence(vector<int>& array) {
     return stack;
 }
 
-int main() {
-    // Let's test the function with a sample input.
-    vector<int> array = {0, 1, 1, 0, 1, 1};
-    vector<int> result = longestGoodSequence(array);
-    cout << "Longest good sequence: ";
-    for (int i : result) {
-        cout << i + 1 << " ";
-    }
-    cout << endl;
+void solve() {
+    /* Code */
+    int n; cin >> n;
+    v<int> arr(n);
+    _for(i, n) cin >> arr[i];
+    auto ans = longestGoodSequence(arr);
+    cout << sz(ans) << ln;
+    _for(i, sz(ans)) cout << ans[i] << " ";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    uint32_t tCs = 1u;
+    cin >> tCs;
+    for (uint32_t tC = 0u; tC++ < tCs; __RUN);
+#ifdef __TAG2
+    __TAG2
+#endif
     return 0;
 }
