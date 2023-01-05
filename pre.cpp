56e18edfdf341e254011678cedae2392c65295b0
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-01-05 21:06:36 +0530

    pre

--- pre.cpp
+++ pre.cpp
@@ -1,37 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+int main()
+{
+    // Initialize set
+    set<int> s;
 
-int main() {
-    int N, K, S;
-    cin >> N >> K >> S; // read in the input
+    s.insert(1);
+    s.insert(4);
+    s.insert(2);
+    s.insert(5);
+    s.insert(3);
 
-    // check if an SS-good sequence is possible
-    // if (S < -N || S > N) {
-    //     cout << -2 << endl; // if not, print -2 and exit
-    //     return 0;
-    // }
+    // finding position of 6
+    auto pos = s.find(0);
 
-    // initialize the SS-good sequence
-    vector<int> B(N);
-    int sum = 0;
-    for (int i = N-1; i >= 0; i--) {
-        if (sum + K <= S) {
-            B[i] = 1;
-            sum += K;
-        } else if (sum - K >= S) {
-            B[i] = -1;
-            sum -= K;
-        } else {
-            B[i] = 0;
-        }
-        K *= 2;
-    }
-
-    // print the SS-good sequence
-    for (int i = 0; i < N; i++) {
-        cout << B[i] << " ";
-    }
-    cout << endl;
+    // print element if it is present in set
+    if(pos != s.end())
+        cout << "Element found at position : "<< *pos;
+    else
+        cout << "Element not present in set";
 
     return 0;
 }
