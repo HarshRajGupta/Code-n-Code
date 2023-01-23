9f519a2ea930e72aa36b669fbd3eca0f9e2de4a8
Harsh Raj Gupta <1amharsh.rg@gmail.com>
2023-01-23 12:54:49 +0530

    Time: 9 ms (6.10%), Space: 9 MB (91.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -7,19 +7,39 @@ class Solution {
     }
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0, j = 0, s = m;
-        while(i < m || j < n) {
-            if (i < m && j < n) {
-                if (nums1[i] > nums2[j]) {
-                    shift(nums1, i, m);
-                    nums1[i] = nums2[j++];
-                    ++i;
-                } else ++i;
-            } else if (j < n) {
-                shift(nums1, i, m);
-                nums1[i] = nums2[j++];
-                ++i;
-            } else break;
+        // int i = 0, j = 0, s = m;
+        // while(i < m || j < n) {
+        //     if (i < m && j < n) {
+        //         if (nums1[i] > nums2[j]) {
+        //             shift(nums1, i, m);
+        //             nums1[i] = nums2[j++];
+        //             ++i;
+        //         } else ++i;
+        //     } else if (j < n) {
+        //         shift(nums1, i, m);
+        //         nums1[i] = nums2[j++];
+        //         ++i;
+        //     } else break;
+        // }
+        for(int i = 0; i < n; ++i) {
+            nums1[i+m] = nums2[i];
         }
+        for(int i = (n+m)/2 + (n+m)%2; i > 1;) {
+            for (int j = 0; j+i < (n+m); ++j) {
+                if (nums1[j+i] < nums1[j]) {
+                    int t = nums1[j];
+                    nums1[j] = nums1[i+j];
+                    nums1[i+j] = t;
+                }
+            }
+            i = (i+1)/2;
+        }
+        for (int j = 0; j+1 < (n+m); ++j) {
+                if (nums1[j+1] < nums1[j]) {
+                    int t = nums1[j];
+                    nums1[j] = nums1[1+j];
+                    nums1[1+j] = t;
+                }
+            }
     }
 };
