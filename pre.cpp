efc911ac0bf705b4d8c646dcafcf1829859edf4b
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-01 19:16:58 +0530

    pre

--- pre.cpp
+++ pre.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> dp(1e9 + 1, -1);
 
 int ans(int n) {
-    if (n == 1 || n <= 0) return 0;
+    // if (n == 1 || n <= 0) return 0;
     if (dp[n] != -1) return dp[n];
     if (n % 6 == 0) return dp[n] = min(ans(n / 2), ans(n / 3)) + 1;
     if (n % 3 == 0) return dp[n] = min(ans(n / 3) + 1, ans(n / 2) + 2);
@@ -21,6 +21,7 @@ void solve() {
 
 signed main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
+    dp[1] = 0, dp[0] = 0;
     uint32_t tCs; cin >> tCs;
     for (uint32_t tC = 0; tC++ < tCs; solve(), std::cout << '\n');
     return 0;
