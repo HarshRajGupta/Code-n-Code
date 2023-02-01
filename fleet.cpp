02ce2022203fad9ba6f20d8acc5dcf93ba1be8a2
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-01 19:11:41 +0530

    fleet

--- fleet.cpp
+++ fleet.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 #ifndef debug
-#define debug(...) ;
+#define debug(...)
 #endif
 
 #ifndef __RUN
@@ -36,26 +36,31 @@ const char ln = '\n';
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-const int64_t ceil (const long double &__NUM, const long double __DEN = 1) {
-    if (__NUM > __DEN * ((int64_t)(__NUM / __DEN)))
-        return (__NUM / __DEN) + 1;
-    return (__NUM / __DEN);
+vector<int> dp(1e9 + 1, -1);
+
+int ans(int n) {
+    if (n == 1 || n <= 0) return 0;
+    if (dp[n] != -1) return dp[n];
+    if (n % 6 == 0) return dp[n] = min(ans(n / 2), ans(n / 3)) + 1;
+    if (n % 3 == 0) return dp[n] = min(ans(n / 3) + 1, ans(n / 2) + 2);
+    if (n % 2 == 0 && (n - 1) % 3 == 0) return dp[n] = min(ans(n / 2) + 1, ans(n / 3) + 2);
+    if (n % 2 == 0 && (n - 2) % 3 == 0) return dp[n] = min(ans(n / 2) + 1, ans(n / 3) + 3);
+    if ((n - 1) % 6 == 0) return dp[n] = min(ans(n / 2), ans(n / 3)) + 2;
+    return dp[n] = ans(n - 1) + 1;
 }
 
 void solve() {
-    /* Code */
-    cout << ceil(2.0);
+    int n; cin >> n;
+    cout << ans(n);
 }
 
 signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 #ifdef __TAG1
     __TAG1
 #endif
-    uint32_t tCs = 1u;
-    cin >> tCs;
-    for (uint32_t tC = 0u; tC++ < tCs; __RUN);
+    uint32_t tCs; cin >> tCs;
+    for (uint32_t tC = 0; tC++ < tCs; __RUN);
 #ifdef __TAG2
     __TAG2
 #endif
