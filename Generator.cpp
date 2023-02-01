716f36e703c37b5faa58ed1133350b6dea920779
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-01 08:43:46 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -4,14 +4,13 @@ using namespace std;
 void generate() {
     cout << "100\n";
     for (int i = 0; i < 10; ++i) {
-        srand(time(0));
         cout << rand() % 10 << ' ';
     }
     cout << '\n';
 }
 
 signed main() {
-    // srand(time(0));
+    srand(time(0));
     int testCase = 10; cout << testCase << endl;
     while (testCase--) generate();
     return 0;
