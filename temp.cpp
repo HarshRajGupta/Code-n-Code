0253ffe73b5bd45d5ac86b09cf653b799ce65fc3
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-01 09:14:00 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 #ifndef debug
-#define debug(...) ;
+#define debug(...)
 #endif
 
 #ifndef __RUN
@@ -27,7 +27,7 @@ const char ln = '\n';
 #define rep(i, a, n) for(auto i = a; i < n; ++i)
 #define bw(i, n) for(int32_t i = (int32_t)n; i >= 0; --i)
 
-#define v std::vector
+template<class T> using v = std::vector<T>;
 #define maxHeap std::priority_queue<int>
 #define minHeap std::priority_queue<int, std::vector<int>, std::greater<int>>
 
@@ -36,19 +36,19 @@ const char ln = '\n';
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-void solve() { 
+void solve() {
     /* Code */
+    v<int> arr= {1, 2, 3};
+    debug(arr);
 }
 
 signed main() {
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 #ifdef __TAG1
     __TAG1
 #endif
-    uint32_t tCs = 1u; 
-    cin >> tCs;
-    for(uint32_t tC = 0u; tC++ < tCs; __RUN);
+    uint32_t tCs; cin >> tCs;
+    for (uint32_t tC = 0; tC++ < tCs; __RUN);
 #ifdef __TAG2
     __TAG2
 #endif
