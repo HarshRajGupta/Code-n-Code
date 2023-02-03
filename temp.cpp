189827a791ed54866a348cb3ec6767d41bbcc9ec
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-03 20:34:31 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -2,32 +2,35 @@
 using namespace std;
 
 int main() {
-    int n, a[1005];
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
+    int t; cin >> t;
+    while (t--) {
+        int n, a[1005];
+        cin >> n;
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
-    int ans = 0, sum = 0, neg_count = 0;
-    for (int i = 0; i < n; i++) {
-        sum += a[i];
-        if (a[i] < 0)
-            neg_count++;
-        if (neg_count % 2 == 0)
-            ans = max(ans, sum);
-        else
-            sum = 0;
+        int ans = 0, sum = 0, neg_count = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            if (a[i] < 0)
+                neg_count++;
+            if (neg_count % 2 == 0)
+                ans = max(ans, sum);
+            else
+                sum = 0;
+        }
+        neg_count = 0;
+        sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            if (a[i] < 0)
+                neg_count++;
+            if (neg_count % 2 != 0)
+                ans = max(ans, sum);
+            else
+                sum = 0;
+        }
+        cout << ans << endl;
     }
-    neg_count = 0;
-    sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += a[i];
-        if (a[i] < 0)
-            neg_count++;
-        if (neg_count % 2 != 0)
-            ans = max(ans, sum);
-        else
-            sum = 0;
-    }
-    cout << ans << endl;
     return 0;
 }
