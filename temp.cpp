e9caa919d0e658efa808bd14007b69dff7b9bd5f
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-03 20:41:33 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,36 +1,66 @@
-#include<bits/stdc++.h>
+#ifndef __FAST_IO
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int t; cin >> t;
-    while (t--) {
-        int n, a[1005];
-        cin >> n;
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
+#ifndef debug
+#define debug(...)
+#endif
 
-        int ans = 0, sum = 0, neg_count = 0;
-        for (int i = 0; i < n; i++) {
-            sum += a[i];
-            if (a[i] < 0)
-                neg_count++;
-            if (neg_count % 2 == 0)
-                ans = max(ans, sum);
-            else
-                sum = 0;
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n, sum = 0, loop = 1; cin >> n;
+    int arr[n];
+    _for(i, n) {cin >> arr[i]; sum += arr[i];}
+    while (loop) {
+        loop = 0;
+        _for(i, n - 1) {
+            if ((arr[i] + arr[i + 1]) < 0) {
+                sum -= (arr[i] + arr[i + 1]);
+                arr[i] *= -1;
+                arr[i + 1] *= -1;
+                sum += (arr[i] + arr[i + 1]);
+                loop = 1;
+            }
         }
-        neg_count = 0;
-        sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += a[i];
-            if (a[i] < 0)
-                neg_count++;
-            if (neg_count % 2 != 0)
-                ans = max(ans, sum);
-            else
-                sum = 0;
-        }
-        cout << ans << endl;
     }
+    cout << sum;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    uint32_t tCs; cin >> tCs;
+    for (uint32_t tC = 0; tC++ < tCs; __RUN);
+#ifdef __TAG2
+    __TAG2
+#endif
     return 0;
 }
