6d1bdbf76e3975e89e64379e5e6b67d725180091
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-03 20:33:56 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,70 +1,33 @@
-#ifndef __FAST_IO
-#pragma GCC optimize("O3", "Ofast", "unroll-loops", "omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
-#endif
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#ifndef debug
-#define debug(...)
-#endif
+int main() {
+    int n, a[1005];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
-#ifndef __RUN
-#define __RUN solve(), std::cout << '\n'
-#endif
-
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define int long long
-#define ll long double
-
-#define Y std::cout << "YES";
-#define N std::cout << "NO";
-
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int32_t i = (int32_t)n; i >= 0; --i)
-
-template <class T> using v = std::vector<T>;
-template <class T> using maxHeap = std::priority_queue<T>;
-template <class T> using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-
-void scan(int &number) {
-    bool negative = false;
-    register int c;
-    number = 0;
-    c = getchar();
-    if (c == '-') {
-        negative = true;
-        c = getchar();
+    int ans = 0, sum = 0, neg_count = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        if (a[i] < 0)
+            neg_count++;
+        if (neg_count % 2 == 0)
+            ans = max(ans, sum);
+        else
+            sum = 0;
     }
-    for (; (c > 47 && c < 58); c = getchar()) number = number * 10 + c - 48;
-    if (negative) number *= -1;
-}
-
-#define ft first
-#define sd second
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-    int n; scan(n);
-    v<int> arr(n);
-    _for(i, n) scan(arr[i]);
-    debug(arr)
-}
-
-signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-#ifdef __TAG1
-    __TAG1
-#endif
-    int tCs; scan(tCs);
-    for (uint32_t tC = 0; tC++ < tCs; __RUN);
-#ifdef __TAG2
-    __TAG2
-#endif
+    neg_count = 0;
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        if (a[i] < 0)
+            neg_count++;
+        if (neg_count % 2 != 0)
+            ans = max(ans, sum);
+        else
+            sum = 0;
+    }
+    cout << ans << endl;
     return 0;
 }
