253cb97852f467cf82d314bfcaeb05cd62746e2a
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-04 13:27:41 +0530

    hack

--- hack.cpp
+++ hack.cpp
@@ -1,174 +1,121 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-#define int               long long
-#define pb                push_back
-#define ppb               pop_back
-#define pf                push_front
-#define ppf               pop_front
-#define all(x)            (x).begin(),(x).end()
-#define uniq(v)           (v).erase(unique(all(v)),(v).end())
-#define sz(x)             (int)((x).size())
-#define fr                first
-#define sc                second
-#define pii               pair<int,int>
-#define mem1(a)           memset(a,-1,sizeof(a))
-#define mem0(a)           memset(a,0,sizeof(a))
-#define mod               998244353
-#define endl             "\n"
-#define inf               1e15
-#define ninf              1e-15
- 
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
- 
- 
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifdef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> pbds;
+typedef long long ll;
+#define pb push_back
+#define F first
+#define S second
+#define OM_NAMAH_SHIVAY ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+#define minusone cout<<"-1"<<endl 
+
+void build(int i,int l,int r,ll a[],vector<ll> &t){
+    if(l==r){
+        t[i]=a[l];
+    }
+    else{
+        int mid=(l+r)/2;
+        build(2*i+1,l,mid,a,t);
+        build(2*i+2,mid+1,r,a,t);
+        t[i]=t[2*i+1]+t[2*i+2];
+    }
+}
+void propagate(int i,int l,int r,vector<ll> &t,vector<ll> &p){
+    t[i]+=(ll)(r-l+1)*p[i];
+    if(l!=r){
+        p[2*i+1]+=p[i];
+        p[2*i+2]+=p[i];
+    }
+    p[i]=0;
+}
+void update(int i,int tl,int tr,int l,int r,ll val,vector<ll> &t,vector<ll> &p){
+    propagate(i,tl,tr,t,p);
+    if(tl>=l&&tr<=r){
+        p[i]+=val;
+    }
+    else{
+        int mid=(tl+tr)/2;
+        if(mid>=l){
+            update(2*i+1,tl,mid,l,r,val,t,p);
+        }
+        if(mid+1<=r){
+            update(2*i+2,mid+1,tr,l,r,val,t,p);
+        }
+        propagate(2*i+1,tl,mid,t,p);
+        propagate(2*i+2,mid+1,tr,t,p);
+        t[i]=t[2*i+1]+t[2*i+2];
+    }
+}
+ll query(int i,int tl,int tr,int l,int r,vector<ll> &t,vector<ll> &p){
+    propagate(i,tl,tr,t,p);
+    if(tl>=l&&tr<=r){
+        return t[i];
+    }
+    else{
+        int mid=(tl+tr)/2;
+        ll k=0;
+        if(mid>=l){
+            k+=query(2*i+1,tl,mid,l,r,t,p);
+        }
+        if(mid+1<=r){
+            k+=query(2*i+2,mid+1,tr,l,r,t,p);
+        }
+        return k;
+    }
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
 #endif
- 
- 
- 
-template<typename T>
-struct Fenwick {
-    vector<T> tree;
-    void  resz(int n)
+    OM_NAMAH_SHIVAY;
+    int cs;
+    cin>>cs;
+    while(cs--)
     {
-        tree.resize(n + 1);
-    }
- 
-    T query(int i) {
-        int sum = 0;
-        while (i) {
-            sum += tree[i];
-            i -= i & (-i);
-        }
-        return sum;
-    }
-    void update(int i, int n, T val) {
-        while (i <= n) {
-            // cout << i << " * " << val << endl;
-            tree[i] += val;
-            i += i & (-i);
-        }
-    }
-};
-
-bool check(vector<int> v,int n,int m, int k)
-{
-    
-    for(int i=0;i<k;i++)
-        v[i]/=n;
-    int temp=0;
-    bool bl = false;
-    for(int i=0;i<k;i++)
-    {
-        if(v[i]&1 )
-            {   if(temp+v[i]<m)
-                temp+=v[i],v[i]=0;
-                else
-                    temp=m,v[i]=0,bl=true;
-        }
-    }
-    // debug(v,temp);
-    if(temp && (m-temp)&1)
-        temp--;
-    // debug(v,temp);
-    if((m-temp)%2 == 0)
-    { for(int i=0;i<k;i++)
-    {
-        if(v[i]%2 ==0)
-                temp+=v[i];
-    }}
-    if(temp>=m)
-        bl = true;
-    return bl;
-}
- 
-
-int fun(vector<int> &a)
-{
-    int count=0;
-    for(int i=0;i<26;i++)
-        if(a[i])
-            count++;
-    return count;
-}
- 
- 
-signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    int test;
-    cin >> test;
-
-     while(test--)
-     {
-        int n;
-        cin >> n;
-        int q;
-        cin >> q;
-        vector<int> v(n);
-        for(int i=0;i<n;i++)
-            cin >> v[i];
-
-        Fenwick<int> fn;
-        fn.resz(n);
-        while(q--)
+        int n,q;
+        cin>>n>>q;
+        int a[n+1];
+        for(int i=1;i<=n;i++)
         {
-            int t;
-            cin >> t;
-            if(t==1)
+            cin>>a[i];
+        }
+        vector<ll> t(4*n),p(4*n);
+        while(q--){
+            int y;
+            cin>>y;
+            if(y==1)
             {
                 int l,r;
-                cin >> l >> r;
-                fn.update(l,n,1);
-                fn.update(r+1,n,-1);
-
+                cin>>l>>r;
+                update(0,0,n-1,l-1,r-1,1,t,p);
             }
             else
             {
                 int x;
-                cin >> x;
-                int c = fn.query(x);
-                fn.update(x,n,-c);
-                if(x+1<=n)
-                fn.update(x+1,n,c);
-
-                while(c-- && v[x-1]>=10) 
+                cin>>x;
+                int k=query(0,0,n-1,x-1,x-1,t,p);
+                if(k>5)k=5;
+                int f=a[x];
+                while(k--)
                 {
-                    int temp = v[x-1];
-                    int a = 0;
-                    while(v[x-1])
-                    {
-                        a+=(v[x-1]%10);
-                        v[x-1]/=10;
+                    int g=0;
+                    while(f){
+                        g+=f%10;
+                        f/=10;
                     }
-                    v[x-1] = a;
-
+                    f=g;
                 }
-                cout << v[x-1] << endl;
-
+                cout<<f<<endl;
             }
         }
-
-      }
+    }
     return 0;
- 
-  }
+}
