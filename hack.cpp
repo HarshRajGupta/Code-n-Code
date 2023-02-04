9bdaed12d5aa86c3dfc408d5ae1cae55a7862834
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-04 13:27:06 +0530

    hack

--- hack.cpp
+++ hack.cpp
@@ -40,10 +40,8 @@ void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#ifndef ONLINE_JUDGE
+#ifdef ONLINE_JUDGE
 #define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
 #endif
  
  
