d8dff19fc89f0d4163bbfebcbbbf6ee0e127b18f
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-04 16:28:56 +0530

    fleet

--- fleet.cpp
+++ fleet.cpp
@@ -1,61 +1,35 @@
-#ifndef __FAST_IO
-#pragma GCC optimize ("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#endif
-
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef debug
-#define debug(...)
-#endif
-
-#ifndef __RUN
-#define __RUN solve(), std::cout << '\n'
-#endif
-
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define int long long
-#define ll long double
-
-template <class T> using v = std::vector<T>;
-template <class T> using xHeap = std::priority_queue<T>;
-template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-
-#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int64_t i = n; i >= 0; --i)
-
-#define ft first
-#define sd second
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
 void solve() {
-    int n; cin >> n;
-    string s; cin >> s;
-    int l = n;
-    _for(i, n / 2) {
-        if (s[i] == s[n - 1 - i]) {
-            cout << l;
+    int n, m; cin >> n;
+    map<int, int> mp;
+    for (int i = 0; i < n; ++i) {
+        cin >> m;
+        ++mp[m];
+    }
+    int ans = 0;
+    for (auto i : mp) {
+        int f = i.second;
+        if (f == 1) {
+            cout << -1 << '\n';
             return;
         }
-        l -= 2;
+        else if (f % 3 == 0) {
+            ans += f / 3;
+        } else if (f % 3 == 2) {
+            ans += f / 3;
+            ++ans;
+        } else {
+            ans += (f / 3);
+            ++ans;
+        }
     }
-    cout << l;
+    cout << ans << '\n';
 }
 
 signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-#ifdef __TAG1
-    __TAG1
-#endif
-    uint32_t tCs; cin >> tCs;
-    for (uint32_t tC = 0; tC++ < tCs; __RUN);
-#ifdef __TAG2
-    __TAG2
-#endif
+    int testCase; cin >> testCase;
+    while (testCase--) solve();
     return 0;
 }
