8213254d96a5709c7b45ce623918ac38acba8596
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-04 16:45:18 +0530

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -2,34 +2,35 @@
 using namespace std;
 
 
-int ans(vector<string> pixels) {
-    vector<int> row(pixels.size()), col(pixels[0].size());
-    for (int i = 0; i < pixels.size(); ++i) {
-        for (int j = 0; j < pixels[0].size(); ++j) {
-            if (pixels[i][j] == '1') {
-                ++row[i];
-                ++col[j];
-            }
-        }
-    }
-    debug(row, col)
-    int MAX = 0;
-    for (int i = 0; i < pixels.size(); ++i) {
-        for (int j = 0; j < pixels[0].size(); ++j) {
-            MAX = max(MAX, abs((int)((int)row.size() + (int)col.size() - row[i] - col[j] - row[i] - col[j])));
-        }
-    }
-    return MAX;
-}
+// int ans(vector<string> pixels) {
+//     vector<int> row(pixels.size()), col(pixels[0].size());
+//     for (int i = 0; i < pixels.size(); ++i) {
+//         for (int j = 0; j < pixels[0].size(); ++j) {
+//             if (pixels[i][j] == '1') {
+//                 ++row[i];
+//                 ++col[j];
+//             }
+//         }
+//     }
+//     debug(row, col)
+//     int MAX = 0;
+//     for (int i = 0; i < pixels.size(); ++i) {
+//         for (int j = 0; j < pixels[0].size(); ++j) {
+//             MAX = max(MAX, abs((int)((int)row.size() + (int)col.size() - row[i] - col[j] - row[i] - col[j])));
+//         }
+//     }
+//     return MAX;
+// }
 
 void solve() {
     int n; cin >> n;
-    // vector<string> s;
+    vector<string> s;
     // for (int i = 0; i < n; ++i) {
     //     cin >> s[i];
     // }
     // cout << ans(s);
-    cout << 1;
+    // cout << 1;
+    debug(s)
 }
 
 signed main() {
