a44fa3806ac2da00949df21b0219d2fde06a29f7
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-04 13:26:16 +0530

    hack

--- hack.cpp
+++ hack.cpp
@@ -1,85 +1,176 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Fenwick
-{
-    int *bit;
-    int n;
-
-} Fenwick;
-
-int find(Fenwick *fen, int r)
-{
-    int ret = 0;
-    for (; r >= 0; r = (r & (r + 1)) - 1)
-        ret += fen->bit[r];
-    return ret;
-}
-
-void update(Fenwick *fen, int idx, int delta)
-{
-    for (; idx < fen->n; idx = idx | (idx + 1))
-        fen->bit[idx] += delta;
-}
-
-int change(int n)
-{
-    int ans = 0;
-    while (n)
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define mod               998244353
+#define endl             "\n"
+#define inf               1e15
+#define ninf              1e-15
+ 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+ 
+ 
+ 
+template<typename T>
+struct Fenwick {
+    vector<T> tree;
+    void  resz(int n)
     {
-        ans += n % 10;
-        n /= 10;
+        tree.resize(n + 1);
     }
-    return ans;
-}
-
-void solve()
-{
-    int n, q;
-    scanf("%d %d", &n, &q);
-    int *arr = (int *)malloc(sizeof(int) * n);
-    Fenwick *Tree = (Fenwick *)malloc(sizeof(Fenwick));
-    Tree->n = n + 2;
-    Tree->bit = (int *)calloc(Tree->n, sizeof(int));
-
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-
-    for (int i = 0; i < q; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        if (a == 1)
-        {
-            int l, r;
-            scanf("%d %d", &l, &r);
-            update(Tree, l, 1);
-            update(Tree, r + 1, -1);
+ 
+    T query(int i) {
+        int sum = 0;
+        while (i) {
+            sum += tree[i];
+            i -= i & (-i);
         }
-        else
+        return sum;
+    }
+    void update(int i, int n, T val) {
+        while (i <= n) {
+            // cout << i << " * " << val << endl;
+            tree[i] += val;
+            i += i & (-i);
+        }
+    }
+};
+
+bool check(vector<int> v,int n,int m, int k)
+{
+    
+    for(int i=0;i<k;i++)
+        v[i]/=n;
+    int temp=0;
+    bool bl = false;
+    for(int i=0;i<k;i++)
+    {
+        if(v[i]&1 )
+            {   if(temp+v[i]<m)
+                temp+=v[i],v[i]=0;
+                else
+                    temp=m,v[i]=0,bl=true;
+        }
+    }
+    // debug(v,temp);
+    if(temp && (m-temp)&1)
+        temp--;
+    // debug(v,temp);
+    if((m-temp)%2 == 0)
+    { for(int i=0;i<k;i++)
+    {
+        if(v[i]%2 ==0)
+                temp+=v[i];
+    }}
+    if(temp>=m)
+        bl = true;
+    return bl;
+}
+ 
+
+int fun(vector<int> &a)
+{
+    int count=0;
+    for(int i=0;i<26;i++)
+        if(a[i])
+            count++;
+    return count;
+}
+ 
+ 
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int test;
+    cin >> test;
+
+     while(test--)
+     {
+        int n;
+        cin >> n;
+        int q;
+        cin >> q;
+        vector<int> v(n);
+        for(int i=0;i<n;i++)
+            cin >> v[i];
+
+        Fenwick<int> fn;
+        fn.resz(n);
+        while(q--)
         {
-            int x;
-            scanf("%d", &x);
-            int cnt = find(Tree, x);
-            int ans = arr[x - 1];
-            for (int i = 0; i < cnt; i++)
+            int t;
+            cin >> t;
+            if(t==1)
             {
-                ans = change(ans);
-                if (ans / 10 == 0)
-                    break;
+                int l,r;
+                cin >> l >> r;
+                fn.update(l,n,1);
+                fn.update(r+1,n,-1);
+
             }
-            printf("%d\n", ans);
+            else
+            {
+                int x;
+                cin >> x;
+                int c = fn.query(x);
+                fn.update(x,n,-c);
+                if(x+1<=n)
+                fn.update(x+1,n,c);
+
+                while(c-- && v[x-1]>=10) 
+                {
+                    int temp = v[x-1];
+                    int a = 0;
+                    while(v[x-1])
+                    {
+                        a+=(v[x-1]%10);
+                        v[x-1]/=10;
+                    }
+                    v[x-1] = a;
+
+                }
+                cout << v[x-1] << endl;
+
+            }
         }
-    }
-}
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-        solve();
-
+      }
     return 0;
-}
+ 
+  }
