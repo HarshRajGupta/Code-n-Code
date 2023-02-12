cfa420f5a1439bcf9959a2daa1aab8e30033054e
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-12 13:06:12 +0530

    gridPath

---
+++ gridPath.cpp
@@ -0,0 +1,61 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    int n; cin >> n;
+    v<string> arr(n);
+    _for(i, n) cin >> arr[i];
+    v<v<int>> dp(n + 1, v<int>(n + 1));
+    ++dp[n - 1][n - 1];
+    bw(i, n - 1) {
+        bw(j, n - 1) {
+            if (arr[i][j] == '*') continue;
+            dp[i][j] += dp[i + 1][j];
+        }
+    }
+    cout << dp[0][0];
+}
+
+signed main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    solve();
+#ifdef __TAG2
+    __TAG2
+#endif
+    return 0;
+}
