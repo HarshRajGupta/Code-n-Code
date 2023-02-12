92752c56cbc424a6b0fa492b2a3b7f500cfd9942
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-12 19:14:36 +0530

    editDistance

---
+++ editDistance.cpp
@@ -0,0 +1,61 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    string a, b; cin >> a >> b;
+    v<v<int>> dp(sz(a) + 1, v<int>(sz(b) + 1));
+    rep(i, 1, sz(a) + 1) {
+        rep(j, 1, sz(b) + 1) {
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j]) + 1);
+            } else {
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+            }
+        }
+    }
+    cout << dp[sz(a)][sz(b)];
+}
+
+signed main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    solve();
+#ifdef __TAG2
+    __TAG2
+#endif
+    return 0;
+}
