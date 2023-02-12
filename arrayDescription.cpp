5ff5b30c283b5b1b1d2836a0e0cad4e11902a6de
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-12 18:46:21 +0530

    arrayDescription

---
+++ arrayDescription.cpp
@@ -0,0 +1,79 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    int n, m; cin >> n >> m;
+    v<int> arr(n);
+    _for(i, n) cin >> arr[i];
+    v<v<int>> dp(m + 1, v<int>(n + 1));
+    int prev = 0;
+    if (arr[0] == 0) {
+        rep(i, 1, m + 1) {
+            ++dp[i][0];
+        }
+    } else {
+        dp[arr[0]][0] = 1;
+    }
+    rep(i, 1, n) {
+        if (arr[i] != 0) {
+            dp[arr[i]][i] = dp[arr[i]][i - 1];
+            if (arr[i] != 1) dp[arr[i]][i] = (dp[arr[i]][i] + dp[arr[i] - 1][i - 1]);
+            if (arr[i] != m) dp[arr[i]][i] = (dp[arr[i]][i] + dp[arr[i] + 1][i - 1]);
+        } else if (prev == 0) {
+            dp[1][i] = (dp[1][i] + dp[1][i - 1] + dp[2][i - 1]) % MOD;
+            dp[m][i] = (dp[m][i] + dp[m - 1][i - 1] + dp[m][i - 1]) % MOD;
+            rep(j, 2, m) {
+                dp[j][i] = (dp[j][i] + dp[j - 1][i - 1] + dp[j][i - 1] + dp[j + 1][i - 1]) % MOD;
+            }
+        }
+    }
+    int ans = 0;
+    rep(i, 1, m + 1) {
+        ans = max(ans, dp[i][n-1]);
+    }
+    cout << ans;
+}
+
+signed main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    solve();
+#ifdef __TAG2
+    __TAG2
+#endif
+    return 0;
+}
