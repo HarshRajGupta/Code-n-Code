292929f62f46b01943ecce67c45e79f1896954cb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-12 12:23:00 +0530

    minimizingCoins

---
+++ minimizingCoins.cpp
@@ -0,0 +1,67 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    int n, m; cin >> n >> m;
+    v<int> arr(n), dp(m + 1);
+    _for(i, n) {
+        cin >> arr[i];
+        if (arr[i] <= m) dp[arr[i]] = 1;
+    }
+    sort(all(arr));
+    rep(i, 1, m + 1) {
+        int MIN = LLONG_MAX;
+        _for(j, n) {
+            if (i - arr[j] < 0) break;
+            if (dp[i - arr[j]] == -1) continue;
+            MIN = min(MIN, dp[i - arr[j]] + 1);
+        }
+        if (MIN == LLONG_MAX) MIN = -1;
+        dp[i] = MIN;
+    }
+    cout << dp[m + 1];
+}
+
+signed main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    solve();
+#ifdef __TAG2
+    __TAG2
+#endif
+    return 0;
+}
