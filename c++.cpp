cff4f3ef338977c208df9f28b85b2234bb9752a7
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-02-12 20:00:20 +0530

    c++

--- c++.cpp
+++ c++.cpp
@@ -1,45 +1,62 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int minIndex(const int startTime, const vector<int> &free) {
-    int MIN = 0;
-    for (unsigned int i = 0; i < free.size(); ++i) {
-        if (free[i] <= startTime)
-            return i;
-        if (free[i] < free[MIN])
-            MIN = i;
-    }
-    return MIN;
-}
+#ifndef debug
+#define debug(...)
+#endif
 
-int mostBooked(int n, vector<vector<int>>& meetings) {
-    sort(meetings.begin(), meetings.end());
-    vector<int> room(n, 0), free(n, 0);
-    for (auto i : meetings) {
-        int pos = minIndex(i[0], free);
-        if (free[pos] <= i[0])
-            free[pos] = i[1];
-        else
-            free[pos] += i[1] - i[0];
-        ++room[pos];
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+const int power(int n, int expo) {
+    int ans = 1; n %= MOD;
+    while (expo) {
+        if (expo & 1) ans = (ans * 1ll * n) % MOD;
+        n = (n * 1ll * n) % MOD;
+        expo >>= 1;
     }
-    int MAX = 0;
-    for (int i = 0; i < n; ++i) {
-        if (room[i] > room[MAX])
-            MAX = i;
-    }
-    return MAX;
+    return ans;
 }
 
 void solve() {
-    vector<vector<int>> s = {{18, 19}, {3, 12}, {17, 19}, {2, 13}, {7, 10}};
-    cout << mostBooked(4, s);
+    int a, b; cin >> a >> b;
+    cout << power(a, b);
 }
 
-signed main(int argc, char const *argv[]) {
-    int testCase;
-    cin >> testCase;
-    while (testCase--)
-        solve();
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    uint32_t tCs; cin >> tCs;
+    for (uint32_t tC = 0; tC++ < tCs; __RUN);
+#ifdef __TAG2
+    __TAG2
+#endif
     return 0;
 }
