a2017c26a8c5eb0456a4ca7e7367cbc928e079b9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-03 10:31:54 +0530

    demo

---
+++ demo.cpp
@@ -0,0 +1,73 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __RUN
+#define __RUN solve(), std::cout << '\n'
+#endif
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define int long long
+#define ll long double
+
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    char ans = 'N';
+    _for(i, n) {
+        if (s[i] == 'L') {
+            if (ans == 'N') {ans = 'W';}
+            else if (ans == 'S') {ans = 'E';}
+            else if (ans == 'W') {ans = 'S';}
+            else if (ans == 'E') {ans = 'N';}
+        } else {
+            if (ans == 'N') {ans = 'E';}
+            else if (ans == 'E') {ans = 'S';}
+            else if (ans == 'S') {ans = 'W';}
+            else if (ans == 'W') {ans = 'N';}
+        }
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+#ifdef __TAG1
+    __TAG1
+#endif
+    uint32_t tCs; cin >> tCs;
+    for (uint32_t tC = 0; tC++ < tCs; __RUN);
+#ifdef __TAG2
+    __TAG2
+#endif
+    return 0;
+}
+
+/*
+    N
+W       E
+    S
+*/
