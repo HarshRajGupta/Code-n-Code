c0f928a592621d8258f12accc71236d99c19f8f9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-03 12:46:26 +0530

    demo

--- demo.cpp
+++ demo.cpp
@@ -17,14 +17,14 @@ using namespace std;
 const uint64_t MOD = 1e9 + 7;
 const char ln = '\n';
 
-// #define int long long
+#define int long long
 #define ll long double
 
-// template <class T> using v = std::vector<T>;
+template <class T> using v = std::vector<T>;
 template <class T> using xHeap = std::priority_queue<T>;
 template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
-// #define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
 #define rep(i, a, n) for(auto i = a; i < n; ++i)
 #define bw(i, n) for(int64_t i = n; i >= 0; --i)
 
@@ -33,33 +33,20 @@ template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::gre
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-vector<int> solve() {
-    int n; cin >> n;
-    vector<int> arr(n);
-    vector<set<int>> ans(33);
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-        int t = 0, y = arr[i];
-        while (y) {
-            if (y & 1) ++t;
-            y = (y >> 1);
-        }
-        ans[t].insert(arr[i]);
+void solve(void) {
+    int n = 1, l = LLONG_MAX;
+    _for(i, 19) {
+        n *= i;
+        debug(n)
     }
-    vector<int> res;
-    for (int i = 0; i < 33; ++i) {
-        for (auto j : ans[i]) res.push_back(j);
-    }
-    return res;
 }
 
-signed main() {
+signed main(void) {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 #ifdef __TAG1
     __TAG1
 #endif
-    uint32_t tCs; cin >> tCs;
-    for (uint32_t tC = 0; tC++ < tCs; __RUN);
+    solve();
 #ifdef __TAG2
     __TAG2
 #endif
