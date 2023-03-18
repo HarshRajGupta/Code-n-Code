a337e29ed492233e80e6ab3b9f5a89c33d6b38e1
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-18 20:12:09 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,59 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dist[100005], pushed[100005];
-queue<int> visitMe;
-map<int, vector<int>> mp;
-map<int, bool> visited;
-
 class Solution {
-    void bfs(vector<int>& arr, int pos) {
-        if (pos == arr.size() - 1) {
-            while (!visitMe.empty()) visitMe.pop();
-            return;
-        }
-        if (!visited[arr[pos]]) {
-            visited[arr[pos]] = 1;
-            for (auto i : mp[arr[pos]]) {
-                if (!pushed[i]) {
-                    pushed[i] = 1;
-                    dist[i] = min(dist[i], dist[pos] + 1);
-                    visitMe.push(i);
-                }
-            }
-        }
-        if (pos < arr.size() && !pushed[pos + 1]) {
-            pushed[pos + 1] = 1;
-            dist[pos + 1] = min(dist[pos + 1], dist[pos] + 1);
-            visitMe.push(pos + 1);
-        }
-        if (pos > 0 && !pushed[pos - 1]) {
-            pushed[pos - 1] = 1;
-            dist[pos - 1] = min(dist[pos - 1], dist[pos] + 1);
-            visitMe.push(pos - 1);
-        }
-    }
 public:
-    int minJumps(vector<int> arr) {
-        for (int i = arr.size() - 1; i > 0; --i) {
-            dist[i] = 1e5;
-            mp[arr[i]].push_back(i);
+    int maximizeGreatness(vector<int> nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), i = 0, j = 0, ans = 0;
+        while (i < n && j < n) {
+            if (nums[j] > nums[i]) {
+                ++i; ++j; ++ans;
+            } else ++j;
         }
-        dist[0] = 0;
-        pushed[0] = 1;
-        visitMe.push(0);
-        while (!visitMe.empty()) {
-            int visiting = visitMe.front();
-            visitMe.pop();
-            bfs(arr, visiting);
-        }
-        // return dist[arr.size() - 1];
+        return ans;
     }
 };
 
 void solve(void) {
     Solution __S;
-    cout << __S.minJumps({7, 7, 7, 7, 7, 7, 7, 11});
+    cout << __S.maximizeGreatness({7, 7, 7, 7, 7, 7, 7, 11});
 }
 
 signed main(void) {
