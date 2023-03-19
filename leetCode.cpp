c4e63f4e9d08039597e629d79058aeaf5a4f56bf
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-19 07:32:31 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,33 +1,27 @@
+#ifndef ONLINE_JUDGE
 #include <bits/stdc++.h>
 using namespace std;
-
-class Solution {
-public:
-    int maximizeGreatness(vector<int> nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), i = 0, j = 0, ans = 0;
-        while (i < n && j < n) {
-            if (nums[j] > nums[i]) {
-                ++i; ++j; ++ans;
-            } else ++j;
-        }
-        return ans;
-    }
-};
-
-void solve(void) {
-    Solution __S;
-    cout << __S.maximizeGreatness({7, 7, 7, 7, 7, 7, 7, 11});
-}
-
-signed main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-#ifdef __TAG1
-    __TAG1
 #endif
-    solve();
-#ifdef __TAG2
-    __TAG2
+
+#ifndef debug
+#define debug(...)
 #endif
-    return 0;
-}
+
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define ll long long
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
+#define rep(i, a, n) for(auto i = a; i < n; ++i)
+#define bw(i, n) for(int64_t i = n; i >= 0; --i)
+
+#define ft first
+#define sd second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+#include "leetcode.h"
