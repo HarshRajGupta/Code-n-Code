809decc0d1bfc7c1451020ddf852cae5bf9bcf6c
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-19 11:36:56 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,30 +1,13 @@
-#ifndef ONLINE_JUDGE
 #include <bits/stdc++.h>
 using namespace std;
-#endif
 
-#ifndef debug
-#define debug(...)
-#endif
-
-#define ll long long
-#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int64_t i = n; i >= 0; --i)
-
-template <class T> using v = std::vector<T>;
-template <class T> using xHeap = std::priority_queue<T>;
-template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-class Solution {
+class S;
+class S {
 public:
-    /* function */
-    void test() {
-        cout << 76;
-    }
+    int t = 0;
 };
+signed main() {
+    S z;
+    return 0;
+}
 
-LeetCode
