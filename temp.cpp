f7964efa584dfc3246839bfbc9fb04a92b3df802
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-19 10:17:36 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,8 +1,3 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize ("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#endif
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,38 +5,35 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __RUN1
-#define __RUN1 ios_base::sync_with_stdio(false);cin.tie(NULL);int t;cin>>t;while(t--)solve(),cout<<'\n';
+#ifndef __RUN
+#define __RUN ios_base::sync_with_stdio(0);cin.tie(NULL);int t;cin>>t;while(t--)solve(),cout<<'\n';
 #endif
 
+#define int long long
 const uint64_t MOD = 1e9 + 7;
 const char ln = '\n';
-
-#define int long long
-#define ll long double
-
-template <class T> using v = std::vector<T>;
-template <class T> using xHeap = std::priority_queue<T>;
-template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
 #define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
 #define rep(i, a, n) for(auto i = a; i < n; ++i)
 #define bw(i, n) for(int64_t i = n; i >= 0; --i)
 
-#define ft first
-#define sd second
+template <class T> using v = std::vector<T>;
+template <class T> using xHeap = std::priority_queue<T>;
+template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-void solve() {
-    int n; cin >> n; cout << n;
+void solve() { 
+    int n; cin >> n;
+    cout << n;
 }
 
 signed main() {
 #ifdef __START
     __START
 #endif
-    __RUN1
+    __RUN
 #ifdef __END
     __END
 #endif
