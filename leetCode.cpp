b1ff41c0161faf366b6c053a21c1c9f45d351259
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-19 10:39:37 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,26 +1,11 @@
+class Solution {
+public:
+    /* function */
+    void test() {
+        /* test */
+    }
+};
+
 #ifndef ONLINE_JUDGE
-#include <bits/stdc++.h>
-// using namespace std;
-#endif
-
-#ifndef debug
-#define debug(...)
-#endif
-
-const char ln = '\n';
-
-#define ll long long
-template <class T> using v = std::vector<T>;
-template <class T> using xHeap = std::priority_queue<T>;
-template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-
-#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int64_t i = n; i >= 0; --i)
-
-#define ft first
-#define sd second
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
 #include "leetcode.h"
+#endif
