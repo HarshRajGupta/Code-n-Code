b281219cbf8326ed0d70ad304b5e0c82195358ba
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-19 11:55:43 +0530

    temp

--- temp.cpp
+++ temp.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,8 +5,13 @@ using namespace std;
 #define debug(...)
 #endif
 
+#ifndef __RUN
+#define __RUN ios_base::sync_with_stdio(0);cin.tie(NULL);int t;cin>>t;while(t--)solve(),cout<<'\n';
+#endif
+
 #define int long long
 const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
 
 #define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
 #define rep(i, a, n) for(auto i = a; i < n; ++i)
@@ -20,16 +24,15 @@ template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::gre
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-void solve(void) {
-    /* Code */
+void solve() {
+    int n; cin >> n; cout << n;
 }
 
-signed main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+signed main() {
 #ifdef __START
     __START
 #endif
-    solve();
+    __RUN
 #ifdef __END
     __END
 #endif
