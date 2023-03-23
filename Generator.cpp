d26fb7d78a0d5f096fece9c820487dfaee7d1789
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 09:12:43 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -11,7 +11,7 @@ public:
         srand(time(0));
         int n = 10;
         std::vector<int> arr(n);
-        int _rand = ((((((rand() * rand()) % 998244353) + (rand() * (-1 * (rand() & 1)))) % 1000000007) / (rand()) + (rand() * (-1 * (rand() & 1))))) * ((rand() % rand()) + rand() % rand() + (rand() * (-1 * (rand() & 1))));
+        int _rand = (((((((rand() * rand()) % 998244353) + (rand() * (-1 * (rand() & 1)))) % 1000000007) / (rand()) + (rand() * (-1 * (rand() & 1))))) * ((rand() % rand()) + rand() % rand() + (rand() * (-1 * (rand() & 1))))) / (rand() % rand());
         srand(time(new long(_rand)));
         _rand = ((((((rand() * rand()) % 998244353) + (rand() * (-1 * (rand() & 1)))) % 1000000007) / (rand()) + (rand() * (-1 * (rand() & 1))))) * ((rand() % rand()) + rand() % rand() + (rand() * (-1 * (rand() & 1))));
         _for(i, n) arr[i] = _rand;
