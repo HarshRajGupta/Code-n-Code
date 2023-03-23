915c2b22a1941998da9571172941400a6ad2e746
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 08:34:51 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-// using namespace std;
+using namespace std;
 
 #define _int long long
 #define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
@@ -8,6 +8,7 @@ const uint64_t MOD = 1e9 + 7;
 class Solution {
 public:
     void test(void) {
+        cout << time(0) << endl;
         int n = 10;
         std::vector<int> arr(n);
         _for(i, n) arr[i] = rand();
