4217651d38cf507033f654131f6eb9d43e00e1df
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 08:42:18 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -10,7 +10,7 @@ public:
     void test(void) {
         long int *z = new long(INT_MAX);
         // time_t *t = time_t(z);
-        cout << (time(new long(INT_MAX))%MOD)*(time(new long(INT_MIN))%MOD) << endl;
+        cout << (time(new long(INT_MAX))%MOD)*(time(new long(INT_MIN))%MOD)%MOD << endl;
         int n = 10;
         std::vector<int> arr(n);
         _for(i, n) arr[i] = rand();
