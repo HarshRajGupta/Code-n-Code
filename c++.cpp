646d8435fe9e47b7302d2a4e95bf569fcee1abaa
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 08:15:56 +0530

    c++

--- c++.cpp
+++ c++.cpp
@@ -10,79 +10,45 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __MAIN__
-#define __MAIN__ ios_base::sync_with_stdio(0);cin.tie(0);signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;
+#ifndef __SOLVE__
+#define __SOLVE__ ios_base::sync_with_stdio(0);cin.tie(NULL);solve();return 0;
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
 
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-#define bw(i, n) for (int32_t i = n; i >= 0; --i)
+#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
+#define bw(i, n) for(int32_t i = n; i >= 0; --i)
 
-template<class T>using v = std::vector<T>;
-template<class T>using maxHeap = std::priority_queue<T>;
-template<class T>using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template <class T> using v = std::vector<T>;
+template <class T> using maxHeap = std::priority_queue<T>;
+template <class T> using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
 #define sz(x) ((int)(x).size())
-#define all(x) (x).begin(),(x).end()
+#define all(x) (x).begin(), (x).end()
 
-const int power(int n, int expo, int MOD = 1000000007) {
-    int ans = 1; n %= MOD;
-    while (expo) {
-        if (expo & 1) ans = (ans * 1ll * n) % MOD;
-        n = (n * 1ll * n) % MOD;
-        expo >>= 1;
+void quickSort(const std::vector<int> &v) {
+    if (v.size() <= 1) return;
+    std::vector<int> left, right;
+    int pivot = v[0];
+    for (int i = 1; i < v.size(); i++) {
+        if (v[i] < pivot) left.push_back(v[i]);
+        else right.push_back(v[i]);
     }
-    // return ans;
-}
-
-int alike(v<int> arr, v<int> &p) {
-    int ans = arr[p[0]];
-    debug(ans)
-    _for(i, sz(arr)) {
-        if (arr[p[i]] != ans) {
-            return -1;
-        }
-        arr[i] = ans;
-    }
-    return ans;
+    quickSort(left);
+    quickSort(right);
+    std::vector<int> sorted;
+    sorted.insert(sorted.end(), left.begin(), left.end());
+    sorted.push_back(pivot);
+    sorted.insert(sorted.end(), right.begin(), right.end());
 }
 
 void solve(void) {
-    int n, m; cin >> n >> m;
-    v<int> arr(n), p(n);
-    _for(i, n) {
-        cin >> p[i];
-        --p[i];
-    }
-    _for(i, n) cin >> arr[i];
-    int t = alike(arr, p);
-    debug(t)
-    if (t == -1) {
-        cout << 0;
-        return;
-    }
-    if (t) {
-        int zeros = 0;
-        _for(i, n) if (!arr[i]) ++zeros;
-        debug(zeros)
-        cout << power(m, zeros);
-    } else {
-        v<int> a(arr);
-        _for(i, n) {
-            if (arr[p[i]] == 0) {
-                a[p[i]] = -1;
-            }
-            arr[i] = -1;
-        }
-        int zeros = 0;
-        _for(i, n) if (a[i] == 0) ++zeros;
-        cout << (power(m, zeros)*m) % MOD;
-    }
+    int n = 1e6;
+    std::vector<int> arr(n);
+    _for(i, n) arr[i] = rand();
+    quickSort(arr);
 }
 
-signed main(void) {__MAIN__}
+signed main(void) {__SOLVE__}
