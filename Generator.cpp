77a398d437c5a8a59d70d7232b1b88511ccd980d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 09:27:28 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -1,28 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define _int long long
+#define int long long
 #define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-const uint64_t MOD = 1e9 + 7, mod = 998244353;
-
-long long _r() {
-    srand(time(new long((((((rand()) * (-1 * (rand() & 1)) + (rand() * (-1 * (rand() & 1))))) * ((rand() % rand()) + rand() % rand()))))));
-    if (rand() < 0)
-        return (((((((((rand() * rand()) % 998244353) + (rand() * (-1 * (rand() & 1)))) % 1000000007) / (rand()) + (rand() * (-1 * (rand() & 1))))) * ((rand() % rand()) + rand() % rand() + (rand() * (-1 * (rand() & 1))))) / (rand() % rand()))) * (-1 * (rand() & 1));
-    if (rand() & 1)
-        return (((((((((rand() * rand()) % 998244353) + (rand() * (-1 * (rand() & 1)))) % 1000000007) / (rand()) + (rand() * (-1 * (rand() & 1))))) * ((rand() % rand()) + rand() % rand() + (rand() * (-1 * (rand() & 1))))))) * (-1 * (rand() & 1));
-    return rand() * (-1 * (rand() & 1));
-}
+#define r (rand() * (-1 + (rand() & 1)))
 
 class Solution {
 public:
     void test(void) {
-        srand(time(0));
-        int n = 10;
-        std::vector<int> arr(n);
-        srand(time(new long(_r())));
-        _for(i, n) arr[i] = _r();
-        debug(arr)
+        /* test */
     }
 };
 
