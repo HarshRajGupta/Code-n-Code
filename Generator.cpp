43d2bc3cb5b5daf019b709fa25304921a57d1fd4
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 09:33:24 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 #define int long long
 #define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define r (rand()*rand()%rand()*((rand()%2)?1:-1))
+#define r ((rand() + (rand() % rand())) * ((rand() & 1) ? 1ll : -1ll))
 
 class Solution {
 public:
     void test(void) {
         /* test */
-        int t = (rand() * ((rand() & 1) ? 1ll : -1ll));
+        // int t = ;
         _for(i, 10) cout << r << ' ';
     }
 };
