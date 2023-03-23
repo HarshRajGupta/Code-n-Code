875c73938ffd7112b38a0bd389b0a8f09d162a79
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 09:00:14 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -9,15 +9,6 @@ class Solution {
 public:
     void test(void) {
         srand(time(0));
-        // int r = ((rand() % mod)*(rand() % MOD)%rand())*rand();
-        // srand(time(new long(r)));
-        if (rand() & 1) {
-            srand(time(new long(rand())));
-            srand(time(new long(rand()/rand())));
-        } else {
-            srand(time(new long(rand()+rand())));
-            srand(time(new long(rand()*rand())));
-        }
         int n = 10;
         std::vector<int> arr(n);
         _for(i, n) arr[i] = (rand()*rand()/rand())*rand();
