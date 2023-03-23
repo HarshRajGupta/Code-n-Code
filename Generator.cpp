145e90b2a8b771c92c7e7b95e85a4a3781f94a32
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 08:53:12 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 #define _int long long
 #define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-const uint64_t MOD = 1e9 + 7;
+const uint64_t MOD = 1e9 + 7, mod = 998244353;
 
 class Solution {
 public:
     void test(void) {
         srand(0);
-        int r = (((rand()*rand())%rand())*rand())%rand();
-        srand(time(new long((((rand()*rand())%rand())*rand()))));
+        int r = ((rand() % mod)*(rand() % MOD)%rand())*rand();
         int n = 10;
         std::vector<int> arr(n);
         _for(i, n) arr[i] = rand();
