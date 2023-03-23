30fdff13589fe4eb2ff05a2adbeea4f60f0c1f2d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-23 08:58:19 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -8,14 +8,16 @@ const uint64_t MOD = 1e9 + 7, mod = 998244353;
 class Solution {
 public:
     void test(void) {
-        // srand(time(0));
+        srand(time(0));
         // int r = ((rand() % mod)*(rand() % MOD)%rand())*rand();
         // srand(time(new long(r)));
-        // if (rand() & 1) {
-        //     srand(time(new long(rand()/rand())));
-        // } else {
-        //     srand(time(new long(rand()*rand())));
-        // }
+        if (rand() & 1) {
+            srand(time(new long(rand())));
+            srand(time(new long(rand()/rand())));
+        } else {
+            srand(time(new long(rand()+rand())));
+            srand(time(new long(rand()*rand())));
+        }
         int n = 10;
         std::vector<int> arr(n);
         _for(i, n) arr[i] = rand();
