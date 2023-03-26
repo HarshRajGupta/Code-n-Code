c0628fe5286ba10e850b7315824b7d78b9ee7249
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-26 08:06:08 +0530

    codeVenture

--- codeVenture.cpp
+++ codeVenture.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef ONLINE_JUDGE
+#ifndef OFFLINE_JUDGE
 #pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
 #pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
 #endif
@@ -10,54 +10,24 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __SOLVE__
-#define __SOLVE__ ios_base::sync_with_stdio(0);cin.tie(NULL);solve();return 0;
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-#define bw(i, n) for(int32_t i = n; i >= 0; --i)
 
-template <class T> using v = std::vector<T>;
-template <class T> using maxHeap = std::priority_queue<T>;
-template <class T> using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-void solve(void) {
-	int n; cin >> n;
-	map<int, int> a, b;
-	_for(i, n) {
-		int t; cin >> t;
-		a[t] = 1;
+class Solution {
+public:
+	int kItemsWithMaximumSum(int numOnes, int numZeros, int numNeg, int k) {
+		int ans = 0;
+		ans += min(numOnes, k);
+		k -= min(numOnes, k);
+		k -= min(numZeros, k);
+		ans -= min(k, numNeg);
+		return ans;
 	}
-	int m = 0; cin >> m;
-	_for(i, m) {
-		int t; cin >> t;
-		b[t] = 1;
+	void test(void) {
+		cout << kItemsWithMaximumSum(1, 2, 3, 4);
+		/* test */
 	}
-	int k; cin >> k;
-	debug(a, b)
-	while (k--) {
-		int l, r; cin >> l >> r;
-		// debug(l, r)
-		if (!a[l] || !b[r]) continue;
-		if ((l ^ r) & 1) {
-			b[r] = 0;
-			--m;
-		} else {
-			a[l] = 0;
-			--n;
-		}
-	}
-	if (m > n) {
-		cout << "Ramadhir";
-	} else
-		cout << "Sardar";
-}
+};
 
-signed main(void) {__SOLVE__}
+#ifdef __TEST__
+__TEST__
+#endif
