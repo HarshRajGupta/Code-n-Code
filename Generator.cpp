2f3a9fd1098daa52b47a77a7e91ef5de512d5616
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-26 21:27:38 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -42,28 +42,29 @@ void solve(void) {
     sort(all(neg));
     int MAX = pos[pos.size() - 1] - neg[0];
     int i = 0, j = 0, sum = 0, sm = 0;
-    // while (i < sz(pos) && j < sz(neg)) {
-    //     if (sum <= 0) {
-    //         sum += pos[i];
-    //         sm += pos[i];
-    //         if (abs(sm) >= MAX) {
-    //             cout << "NO";
-    //             return;
-    //         }
-    //         ++i;
-    //         ans.push_back(i);
-    //     } else {
-    //         sum += neg[j];
-    //         ++j;
-    //         ans.push_back(j);
-    //         sm += neg[j];
-    //         if (abs(sm) >= MAX) {
-    //             cout << "NO";
-    //             return;
-    //         }
-    //         if (sm < 0) sm = 0;
-    //     }
-    // }
+    while (i < sz(pos) && j < sz(neg)) {
+        debug(sum)
+        if (sum <= 0) {
+            sum += pos[i];
+            sm += pos[i];
+            if (abs(sm) >= MAX) {
+                cout << "NO";
+                return;
+            }
+            ++i;
+            ans.push_back(pos[i]);
+        } else {
+            sum += neg[j];
+            ++j;
+            ans.push_back(neg[j]);
+            sm += neg[j];
+            if (abs(sm) >= MAX) {
+                cout << "NO";
+                return;
+            }
+            if (sm < 0) sm = 0;
+        }
+    }
     cout << "YES\n";
     _for(i, ans.size()) cout << ans[i] << ' ';
 }
