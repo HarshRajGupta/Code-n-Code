6e36c0723d250297aa1f82a268f4587bfed9c44b
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-26 20:09:19 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -1,18 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define r (rand() * ((rand() & 1) ? 1ll : -1ll))
-
-class Solution {
-public:
-    void test(void) {
-        _for(i, 10)
-            cout << r << ' ';
-    }
-};
-
-#ifdef __TEST__
-__TEST__
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
 #endif
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __MAIN__
+#define __MAIN__ ios_base::sync_with_stdio(0);cin.tie(0);signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
+
+template<class T>using v = std::vector<T>;
+template<class T>using maxHeap = std::priority_queue<T>;
+template<class T>using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+
+void solve(void) {
+    int n; cin >> n;
+    // v<int> a(n), b(n);
+    // _for(i, n) cin >> a[i];
+    // _for(i, n) cin >> b[i];
+    // _for(i, n - 1) {
+    //     if (min(a[i], b[i]) < min(a[n - 1], b[n - 1]) || max(a[i], b[i]) > max(a[n - 1], b[n - 1])) {
+    //         cout << "NO";
+    //         return;
+    //     }
+    // }
+}
+
+signed main(void) {__MAIN__}
