b5903798fa381122c63759b1f343c7f032dbef83
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-26 08:55:32 +0530

    codeVenture

--- codeVenture.cpp
+++ codeVenture.cpp
@@ -12,19 +12,58 @@ using namespace std;
 
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
+std::vector<int32_t> allPrimes;
+std::vector<bool> sieve(int32_t n = 1000007) {
+	std::vector<bool> isPrime(n + 1, true);
+	isPrime[0] = false, isPrime[1] = false;
+	for (int32_t i = 2; (i * i) <= n; ++i) {
+		if (isPrime[i]) {
+			allPrimes.push_back(i);
+			for (int j = (i * i); j <= n; j += i) isPrime[j] = false;
+		}
+	}
+	return isPrime;
+}
+
+const int lB(const std::vector<int> &arr, const int &t) {
+	if (t > arr[arr.size() - 1])
+		return (arr.size() - 1);
+	else if (t <= arr[0])
+		return -1;
+	int l = 0, r = (arr.size() - 1), ans = - 1;
+	while (l <= r) {
+		int mid = ((l + r) / 2);
+		if (arr[mid] >= t)
+			r = (mid - 1);
+		else
+			ans = mid, l = (mid + 1);
+	}
+	return ans;
+}
+
 class Solution {
 public:
-	int kItemsWithMaximumSum(int numOnes, int numZeros, int numNeg, int k) {
-		int ans = 0;
-		ans += min(numOnes, k);
-		k -= min(numOnes, k);
-		k -= min(numZeros, k);
-		ans -= min(k, numNeg);
-		return ans;
+	bool primeSubOperation(vector<int>& nums) {
+		int32_t n = nums.size();
+		std::vector<bool> isPrime = sieve(1007);
+		int ind = lB(allPrimes, nums[0]);
+		if (ind != -1) {
+			nums[0] -= allPrimes[ind];
+		}
+		for (int i = 1; i < n; ++i) {
+			int diff = nums[i] - nums[i - 1];
+			if (diff <= 1) return false;
+			ind = lB(allPrimes, diff);
+			if (ind != -1) {
+				nums[i] -= allPrimes[ind];
+			}
+		}
+        debug(nums)
+		return true;
 	}
 	void test(void) {
-		cout << kItemsWithMaximumSum(1, 2, 3, 3);
-		/* test */
+		vector<int> a = {4,9,6,10};
+		cout << primeSubOperation(a) << endl;
 	}
 };
 
