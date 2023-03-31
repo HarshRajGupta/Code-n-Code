3289dac4bf22770a08cc0c13903385349669e5f5
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-03-31 22:43:24 +0530

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,51 +1,59 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize ("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#endif
-
 #include <bits/stdc++.h>
 using namespace std;
+
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
+#endif
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#ifndef __RUN
-#define __RUN solve(), std::cout << '\n'
+#ifndef __MAIN__
+#define __MAIN__ ios_base::sync_with_stdio(0);cin.tie(0);signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;
 #endif
 
+#define int long long
 const uint64_t MOD = 1e9 + 7;
 const char ln = '\n';
 
-#define int long long
-#define ll long double
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
 
-template <class T> using v = std::vector<T>;
-template <class T> using xHeap = std::priority_queue<T>;
-template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T>using v = std::vector<T>;
+template<class T>using maxHeap = std::priority_queue<T>;
+template<class T>using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
-#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int64_t i = n; i >= 0; --i)
-
-#define ft first
-#define sd second
 #define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
+#define all(x) (x).begin(),(x).end()
 
-void solve() { 
-    cout << 2;
+const int power(int n, int expo, int m) {
+    int ans = 1;
+    while (expo) {
+        if (expo & 1) ans = (ans * 1ll * n);
+        if (ans > m) return ans;
+        n = (n * 1ll * n);
+        expo >>= 1;
+    }
+    return ans;
 }
 
-signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-#ifdef __TAG1
-    __TAG1
-#endif
-    uint32_t tCs; cin >> tCs;
-    for(uint32_t tC = 0; tC++ < tCs; __RUN);
-#ifdef __TAG2
-    __TAG2
-#endif
-    return 0;
+int NthRoot(int n, int m) {
+    int l = 1, r = m, ans = m;
+    while (l < r) {
+        int mid = (l + r) >> 1;
+        if (power(mid, n, m) < m) l = mid + 1;
+        else r = mid - 1, ans = mid;
+    }
+    return (power(ans, n, m) == m ? ans : -1);
 }
+
+void solve(void) {
+    /* Code */
+    cout << NthRoot(2, 1e9);
+}
+
+signed main(void) {__MAIN__}
