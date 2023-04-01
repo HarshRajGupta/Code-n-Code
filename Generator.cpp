a3310e1fee4f2030a0f633c5f05ee76ce8132a92
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-01 07:53:34 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -1,34 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef OFFLINE_JUDGE
-#pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse","sse2","sse3","ssse3","sse4","popcnt","abm","mmx","avx","avx2")
-#endif
-
-#ifndef debug
-#define debug(...)
-#endif
-
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#define r (rand() * (rand() & 1 ? -1 : 1) * 1ll * (rand() % 69))
 
 class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        cout << "hello world" << endl;
-        return nullptr;
+    void generateTestCase(void) {
+        /* generate test case */
+        cout << r << ' ';
     }
+public:
     void test(void) {
-        /* test */
-        reverseList(nullptr);
+        int n = 20; cout << n << '\n';
+        rep(i, 0, n) {
+            generateTestCase();
+        }
     }
 };
 
