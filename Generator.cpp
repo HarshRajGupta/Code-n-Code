f4a7e422c47ecc1fce7505054da65beddbe600d8
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-01 08:03:40 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -9,7 +9,7 @@ const int MOD = 1e9 + 7;
 class Solution {
     void generateTestCase(void) {
         /* generate test case */
-        int pr = rand() * (rand() % 210 + 1);
+        int pr = rand() * (rand() % 10 + 1);
         cout << pr << ' ';
     }
 public:
