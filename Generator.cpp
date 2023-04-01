9c1e53264f13e424e48c413911564208e79334de
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-01 11:06:27 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -2,7 +2,7 @@
 // 107 211 997 2111 10007 20011 100003 200003 1000003 10000019
 #define pr ((rand() * (rand()%7+1) * (rand()%7+1) * 1ll % MOD)/ (rand()%7+1))
 #define r (pr * (rand() & 1 ? -1 : 1))
-static std::bitset<2750177> isPrime; std::vector<int> primes;
+std::bitset<2750177> isPrime; std::vector<int> primes;
 class Solution {
     void static buildPrimeArray(void) {
         isPrime[0] = isPrime[1] = 1;
