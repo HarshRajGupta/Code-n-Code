f0b26b7bf232bce7201d60f1cba7f81eb174bfba
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-01 10:54:58 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -8,8 +8,12 @@ class Solution {
         isPrime[0] = isPrime[1] = false;
         for (int i = 2; (i * i) <= 10000019; ++i) if (isPrime[i]) for (int j = (i * i); j <= 10000019; j += i) isPrime[j] = false;
         for (int i = 2; i <= 10000019; ++i) if (isPrime[i]) primes.push_back(i);
-            auto t = (1ll << 32) - 1;
-        debug(primes, t);
+        for (int i = 2; i <= 10000019; ++i) {
+            int z = isPrime[i];
+            if(z) debug(i)
+        }
+        // auto t = (1ll << 32) - 1;
+        
     }
     const static long long MOD = 1e9 + 7, L = 211;
     void static generateArray(size_t n = rand() % L + 1) {
