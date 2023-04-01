833dd1d3e4c57df563f65b144d8cf9530fbe81cb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-01 08:14:28 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1e9 + 7;
+const long long MOD = 1e9 + 7;
 
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-#define r ((rand() * (rand() % 107 + 1) * (rand() & 1 ? -1ll : 1ll)) % MOD)
+#define pr ((rand() * (rand() % 7 + 1) * (rand() % 7 + 1) * 1ll % MOD) / (rand() % 7 + 1))
+#define r (pr * (rand() & 1 ? -1ll : 1ll))
 
 class Solution {
     void generateTestCase(void) {
         /* generate test case */
-        int pr = ((rand() * (rand() % 7 + 1) * (rand() % 7 + 1)) % MOD) / (rand() % 7 + 1);
-        cout << pr << ' ';
+        cout << r << ' ';
     }
 public:
     void test(void) {
         int n = 20; cout << n << '\n';
-        rep(i, 0, n) {
+        for (int i = 0; i < n; ++i) {
             generateTestCase();
         }
     }
