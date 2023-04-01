1fb2e242528e88a66a662576705f205ebed3519e
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-01 08:30:26 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
-// using namespace std;
 
 const long long MOD = 1e9 + 7;
 #define pr ((rand() * (rand() % 7 + 1) * (rand() % 7 + 1) * 1ll % MOD) / (rand() % 7 + 1))
-#define r (pr * (rand() & 1 ? -1ll : 1ll))
+#define r ((pr * (rand() & 1 ? -1ll : 1ll)))
 
 class Solution {
+    void generateArray(int n = rand()) {
+        printf("%d\n", n); while (n--) printf("%d ", r); printf("\n");
+    }
+    void generateString(int n = rand()) {
+        std::cout << n << '\n'; while (n--) std::cout << (char)('a' + rand() % 26); std::cout << '\n';
+    }
     void generateTestCase(void) {
         /* generate test case */
-        int t = rand() % 200009; std::cout << t << '\n';
-        while (t--) std::cout << pr << ' ';
-
     }
 public:
     void test(void) {
         int t = rand() % 7; std::cout << t << '\n';
-        while (t--) generateTestCase(), std::cout << '\n';
+        while (t--) generateTestCase();
     }
 };
 
