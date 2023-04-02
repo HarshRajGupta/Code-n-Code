a92347ffeb6783162c72bf24fb93bb84d5443e24
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-02 19:12:18 +0530

    hack

--- hack.cpp
+++ hack.cpp
@@ -30,56 +30,56 @@ public:
         while (!q.empty()) {
             int pos = q.front()[0], cnt = q.front()[1];
             q.pop();
-            for (auto it = nums.lower_bound(pos); it != nums.end() && *it <= pos + k - 1; ++it) {
-                int j = *it;
-                if (j < 0 || j >= n) break;
-                if (arr[j] == -2 && (((j - pos + 1) & 1) == (k & 1)) && (j - pos + 1 + 2 * min(pos, n - j - 1)) >= k) {
-                    arr[j] = cnt;
-                    q.push({j, cnt + 1});
-                    --it;
-                    nums.erase(j);
-                }
-            }
-            if (pos > *nums.rbegin()) {
-                for (auto it = nums.rbegin(); it != nums.rend() && *it >= pos - k + 1; ++it) {
-                    auto j = *it;
-                    if (j < 0 || j >= n) break;
-                    if (arr[j] == -2 && (((pos - j + 1) & 1) == (k & 1)) && (pos - j + 1 + 2 * min(j, n - pos - 1)) >= k) {
-                        debug(pos, j)
-                        arr[j] = cnt;
-                        q.push({j, cnt + 1});
-                        --it;
-                        nums.erase(j);
-                    }
-                }
-            }
-            else for (auto it = nums.lower_bound(pos); it != nums.begin() && *it >= pos - k + 1; --it) {
-                auto j = *it;
-                if (j < 0 || j >= n) break;
-                if (arr[j] == -2 && (((pos - j + 1) & 1) == (k & 1)) && (pos - j + 1 + 2 * min(j, n - pos - 1)) >= k) {
-                    debug(pos, j)
-                    arr[j] = cnt;
-                    q.push({j, cnt + 1});
-                    ++it;
-                    nums.erase(j);
-                }
-            }
-            debug(nums, pos)
-            if ((*nums.begin() >= pos - k + 1 && *nums.begin() < pos)) {
-                int j = *nums.begin();
-                if (arr[j] == -2 && (((pos - j + 1) & 1) == (k & 1)) && (pos - j + 1 + 2 * min(j, n - pos - 1)) >= k) {
-                    arr[j] = cnt;
-                    q.push({j, cnt + 1});
-                    nums.erase(j);
-                }
-            }
+            // for (auto it = nums.lower_bound(pos); it != nums.end() && *it <= pos + k - 1; ++it) {
+            //     int j = *it;
+            //     if (j < 0 || j >= n) break;
+            //     if (arr[j] == -2 && (((j - pos + 1) & 1) == (k & 1)) && (j - pos + 1 + 2 * min(pos, n - j - 1)) >= k) {
+            //         arr[j] = cnt;
+            //         q.push({j, cnt + 1});
+            //         --it;
+            //         nums.erase(j);
+            //     }
+            // }
+            // if (pos > *nums.rbegin()) {
+            //     for (auto it = nums.lower_bound(*nums.rbegin()); it != nums.begin() && *it >= pos - k + 1; --it) {
+            //         auto j = *it;
+            //         if (j < 0 || j >= n) break;
+            //         if (arr[j] == -2 && (((pos - j + 1) & 1) == (k & 1)) && (pos - j + 1 + 2 * min(j, n - pos - 1)) >= k) {
+            //             debug(pos, j)
+            //             arr[j] = cnt;
+            //             q.push({j, cnt + 1});
+            //             ++it;
+            //             nums.erase(j);
+            //         }
+            //     }
+            // }
+            // else
+                // for (auto it = nums.lower_bound(pos); it != nums.begin() && *it >= pos - k + 1; --it) {
+                //     auto j = *it;
+                //     if (j < 0 || j >= n) break;
+                //     if (arr[j] == -2 && (((pos - j + 1) & 1) == (k & 1)) && (pos - j + 1 + 2 * min(j, n - pos - 1)) >= k) {
+                //         debug(pos, j)
+                //         arr[j] = cnt;
+                //         q.push({j, cnt + 1});
+                //         ++it;
+                //         nums.erase(j);
+                //     }
+                // }
+            // if ((*nums.begin() >= pos - k + 1 && *nums.begin() < pos)) {
+            //     int j = *nums.begin();
+            //     if (arr[j] == -2 && (((pos - j + 1) & 1) == (k & 1)) && (pos - j + 1 + 2 * min(j, n - pos - 1)) >= k) {
+            //         arr[j] = cnt;
+            //         q.push({j, cnt + 1});
+            //         nums.erase(j);
+            //     }
+            // }
         }
         for (auto& i : arr) if (i == -2) i = -1;
         return arr;
     }
     void test(void) {
         vector<int> a = {1, 2};
-        auto ans = minReverseOperations(5, 4, a, 2);
+        auto ans = minReverseOperations(4, 0, a, 4);
         debug(ans);
     }
 };
