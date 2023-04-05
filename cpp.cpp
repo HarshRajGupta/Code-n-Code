033ca13a29ab5fdb9f4a9ce695b21b0055f87bd9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-05 21:27:21 +0530

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -3,42 +3,20 @@ using namespace std;
 
 const int MOD = 1000000007;
 
-long long gcd(long long a, long long b, long long &x, long long &y) {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
+const long long invMod(long long n) {
+    long long ans = 1, expo = MOD - 2; n %= MOD;
+    while (expo) {
+        if (expo & 1) ans = (ans * 1ll * n) % MOD;
+        n = (n * 1ll * n) % MOD;
+        expo >>= 1;
     }
-    long long x1, y1;
-    long long g = gcd(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - (a / b) * y1;
-    return g;
-}
-
-long long mod_inv(long long a, long long m) {
-    long long x, y;
-    long long g = gcd(a, m, x, y);
-    if (g != 1) {
-        return -1;
-    } else {
-        return (x % m + m) % m;
-    }
+    return ans;
 }
 
 int main() {
     long long a = 532634674575485LL;
     long long b = 143232634734LL;
-
-    long long inv_b = mod_inv(b, MOD);
-    if (inv_b == -1) {
-        cout << "Error: b is not invertible modulo MOD" << endl;
-        return 0;
-    }
-
-    long long ans = (a * inv_b) % MOD;
-
-    cout << ans << endl;
-
+    cout << a / b;
+    cout << a*invMod(b);
     return 0;
 }
