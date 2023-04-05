0d85ae75668eec4f02468172df06853fa52a2175
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-05 20:09:08 +0530

    hack

--- hack.cpp
+++ hack.cpp
@@ -10,48 +10,40 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __SOLVE__
-#define __SOLVE__ ios_base::sync_with_stdio(0);cin.tie(NULL);solve();return 0;
+#ifndef __MAIN__
+#define __MAIN__ ios_base::sync_with_stdio(0);cin.tie(0);signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
 
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-#define bw(i, n) for(int32_t i = n; i >= 0; --i)
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
 
-template <class T> using v = std::vector<T>;
-template <class T> using maxHeap = std::priority_queue<T>;
-template <class T> using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T>using v = std::vector<T>;
+template<class T>using maxHeap = std::priority_queue<T>;
+template<class T>using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
 #define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-const int lB(const std::vector<int> &arr, const int t, int l = 0, int r = -1) {
-    if (r == -1) r = (arr.size() - 1);
-    if (arr[l] >= t) return l - 1;
-    if (arr[r] < t) return r;
-    int ans = l - 1, mid;
-    while (l <= r) {
-        mid = (l + r) >> 1;
-        if (arr[mid] >= t) r = (mid - 1);
-        else ans = mid, l = (mid + 1);
-    }
-    return ans;
-}
+#define all(x) (x).begin(),(x).end()
 
 void solve(void) {
-    int n, m; cin >> n >> m;
-    v<int> a(n), prefixSum(n + 1);
-    _for(i, n) cin >> a[i];
-    sort(all(a));
-    _for(i, n) prefixSum[i + 1] = prefixSum[i] + a[i];
-    while (m--) {
-        int x; cin >> x;
-        int ans = lB(a, x);
-        cout << (ans + 1)*x  - 2 * prefixSum[ans + 1] + prefixSum[n] - (n - ans - 1)*x << ' ';
+    int n, prev = 0, ans = 0; cin >> n;
+    string s; cin >> s;
+    _for(i, n - 1) {
+        prev ^= s[i] - '0';
+        if (prev) ++ans;
     }
+    int MAX = 0;
+    prev = 1;
+    _for(i, n - 1) {
+        prev ^= s[i] - '0';
+        if (prev) ++MAX;
+    }
+    cout << max(MAX, ans);
 }
 
-signed main(void) {__SOLVE__}
+signed main(void) {__MAIN__}
