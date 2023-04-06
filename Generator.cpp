803a888c23780d377de782cff0f84715d7e47023
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-06 20:30:15 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -42,7 +42,7 @@ class Solution {
     }
 
     void generateTestCase(void) {
-        printf("%d %d", pr, pr);
+        printf("%d %d\n", pr, pr);
         /* generate test case */
         // generateArray();
         // generatePositiveArray();
