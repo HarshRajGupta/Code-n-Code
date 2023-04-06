04bf5aa1b2eaa7487a96c9401b6f4e938972e451
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-06 20:46:03 +0530

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -14,12 +14,12 @@ using namespace std;
 #define __MAIN__ ios_base::sync_with_stdio(0);cin.tie(0);signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;
 #endif
 
-#define int long long
+// #define int long long
 const uint64_t MOD = 1e9 + 7;
 const char ln = '\n';
 
 #define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int i = a; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
 #define foreach(i, x) for (auto &i : x)
 #define bw(i, n) for (int32_t i = n; i >= 0; --i)
 
@@ -40,15 +40,25 @@ int ans(int &a, int &b, int &k) {
     return (k - 1 + ceil(a, k) + ceil(b, k));
 }
 
+bool check(int a, int b, int t) {
+    int res = INT_MAX;
+    rep(k, 1, max(a, b) + 1) {
+        int t = ans(a, b, k);
+        if (res > t) res = t;
+    }
+    return res == t;
+}
+
 void solve(void) {
     int a, b; cin >> a >> b;
-    int res = LLONG_MAX;
+    int res = INT_MAX;
     rep(k, 1, max(a, b) + 1) {
         int t = ans(a, b, k);
         if (res < t) break;
         if (t > 0) res = t;
     }
-    cout << res;
+    // cout << res;
+    cout << check(a, b, res);
 }
 
 signed main(void) {__MAIN__}
