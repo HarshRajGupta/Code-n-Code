6a10a8cf1436caf86cdf23878590a4a54b9c9cbb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-06 20:16:30 +0530

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,22 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MOD = 1000000007;
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
+#endif
 
-const long long invMod(long long n) {
-    long long ans = 1, expo = MOD - 2; n %= MOD;
-    while (expo) {
-        if (expo & 1) ans = (ans * 1ll * n) % MOD;
-        n = (n * 1ll * n) % MOD;
-        expo >>= 1;
-    }
-    return ans;
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __MAIN__
+#define __MAIN__ ios_base::sync_with_stdio(0);cin.tie(0);signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
+
+template<class T>using v = std::vector<T>;
+template<class T>using maxHeap = std::priority_queue<T>;
+template<class T>using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+
+const int ceil (const long double &n, const long double d = 1) {
+    if (n > (d * ((int)(n / d))))
+        return (n / d) + 1;
+    return (n / d);
 }
 
-int main() {
-    long long a = 532634674575485LL;
-    long long b = 143232634734LL;
-    cout << a / b << ' ';
-    cout << ((a % MOD) * (invMod(b) % MOD)) % MOD;
-    return 0;
+void solve(void) {
+    int a, b; cin >> a >> b;
+    int ans = 0, k = sqrt(min(a, b));
+    ans += (k - 1);
+    ans += ceil(a, k) + ceil(b, k);
+    cout << ans;
 }
+
+signed main(void) {__MAIN__}
+
+/*
+
+*/
