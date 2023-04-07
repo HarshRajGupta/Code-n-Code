0f76d7892f3e54b7b061139fcf6001eb29c9bac9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-07 11:43:01 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -2,50 +2,74 @@
 // 107 211 997 2111 10007 20011 100003 200003 1000003 10000019
 #define pr ((rand() * (rand()%7+1) * (rand()%7+1) * 1ll % MOD)/ (rand()%7+1))
 #define r (pr * (rand() & 1 ? -1 : 1))
+
 std::bitset<2750177> isPrime; std::vector<int> primes;
-class Solution {
-    void static preCompute(void) {
+const long long MOD = 1e9 + 7, L = 211;
+
+class testCase {
+public:
+    static void preCompute(void) {
         isPrime[0] = isPrime[1] = 1;
         for (int i = 2; (i * i) <= 2750177; ++i) if (!isPrime[i]) for (int j = (i * i); j <= 2750177; j += i) isPrime[j] = true;
         for (int i = 2; i <= 2750177; ++i) if (!isPrime[i]) primes.push_back(i);
     }
-    const static long long MOD = 1e9 + 7, L = 211;
-    void static generateArray(size_t n = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%lld ", r); printf("\n");
+    void generateArray(size_t n = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << r << ' ';
+        std::cout << std::endl;
     }
-    void static generatePositiveArray(size_t n = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%lld ", pr); printf("\n");
+    void generatePositiveArray(size_t n = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << pr << ' ';
+        std::cout << std::endl;
     }
-    void static generateBinaryArray(size_t n = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%d ", rand() & 1); printf("\n");
+    void generateBinaryArray(size_t n = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << (rand() & 1) << ' ';
+        std::cout << std::endl;
     }
-    void static generatePrimeArray(size_t n = rand() % L + 1, size_t z = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%d ", primes[rand() % std::min(primes.size(), z)]); printf("\n");
+    void generatePrimeArray(size_t n = rand() % L + 1, size_t z = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << primes[rand() % std::min(primes.size(), z)] << ' ';
+        std::cout << std::endl;
     }
-    void static generateLowerString(size_t n = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%c", 'a' + rand() % 26); printf("\n");
+    void generateLowerString(size_t n = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << (char)('a' + rand() % 26);
+        std::cout << std::endl;
     }
-    void static generateUpperString(size_t n = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%c", 'A' + rand() % 26); printf("\n");
+    void generateUpperString(size_t n = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << (char)('A' + rand() % 26);
+        std::cout << std::endl;
     }
-    void static generateBinaryString(size_t n = rand() % L + 1) {
-        printf("%d\n", n); while (n--) printf("%d", rand() & 1); printf("\n");
+    void generateBinaryString(size_t n = rand() % L + 1) {
+        std::cout << n << '\n';
+        while (n--) std::cout << (rand() & 1);
+        std::cout << std::endl;
     }
-    void static generateMatrix(size_t n = rand() % L + 1, size_t m = rand() % L + 1) {
-        printf("%d %d\n", n, m); while (n--) generateArray(m);
+    void generateMatrix(size_t n = rand() % L + 1, size_t m = rand() % L + 1) {
+        std::cout << n << ' ' << m << '\n';
+        while (n--) generateArray(m);
     }
-    void static generatePositiveMatrix(size_t n = rand() % L + 1, size_t m = rand() % L + 1) {
-        printf("%d %d\n", n, m); while (n--) generatePositiveArray(m);
+    void generatePositiveMatrix(size_t n = rand() % L + 1, size_t m = rand() % L + 1) {
+        std::cout << n << ' ' << m << '\n';
+        while (n--) generatePositiveArray(m);
     }
-    void static generateBinaryMatrix(size_t n = rand() % L + 1, size_t m = rand() % L + 1) {
-        printf("%d %d\n", n, m); while (n--) generateBinaryString(m);
+    void generateBinaryMatrix(size_t n = rand() % L + 1, size_t m = rand() % L + 1) {
+        std::cout << n << ' ' << m << '\n';
+        while (n--) generateBinaryArray(m);
     }
+};
 
+class Solution : private testCase {
     void generateTestCase(void) {
-        std::cout << (pr * pr) % MOD << ' ' << (pr * pr) % MOD << '\n';
-        // std::cout << (pr) % MOD << ' ' << (pr) % MOD << '\n';
         /* generate test case */
-        // generateArray();
+        // std::cout << pr << '\n';
+        // std::cout << (pr * pr) % MOD << '\n';
+        // std::cout << (pr * pr) % MOD << ' ' << (pr * pr) % MOD << '\n';
+        // std::cout << (pr) % MOD << ' ' << (pr) % MOD << '\n';
+        generateArray();
         // generatePositiveArray();
         // generateBinaryArray();
         // generatePrimeArray();
