21180de46017be5e8a0162b6af4701d6bfd5e649
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-07 12:02:59 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -2,7 +2,7 @@
 // 107 211 997 2111 10007 20011 100003 200003 1000003 10000019
 
 struct testCase {
-#define pr ((rand() * (rand()%7+1) * (rand()%7+1) * 1ll % MOD)/ (rand()%7+1))
+#define pr ((rand() * ((((rand() * (rand() % rand())) % MOD) / (rand() % rand())) % MOD)) / (rand() % rand()) % MOD)
 #define r (pr * (rand() & 1 ? -1 : 1))
     static const long long MOD = 1e9 + 7, L = 211;
     std::bitset<2750177> isPrime; std::vector<int> primes;
@@ -64,7 +64,7 @@ struct testCase {
 class Solution : private testCase {
     void generateTestCase(void) {
         /* generate test case */
-        std::cout << (rand() * ((((rand() * (rand() % rand())) % MOD) / (rand() % rand())) % MOD)) / (rand() % rand()) % MOD  << ' '  << rand() << '\n';
+        std::cout << pr << ' '  << rand() << '\n';
         // std::cout << (pr * pr) % MOD << '\n';
         // std::cout << (pr * pr) % MOD << ' ' << (pr * pr) % MOD << '\n';
         // std::cout << (pr) % MOD << ' ' << (pr) % MOD << '\n';
