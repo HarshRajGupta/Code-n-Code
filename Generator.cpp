9174b6a9e76e2a6867b9a2832886ef56ddfcfc8e
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-07 19:02:19 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -65,7 +65,7 @@ struct testCase {
 class Solution : private testCase {
     void generateTestCase(void) {
         /* generate test case */
-        std::cout <<  1ll * (rand() % rand()) * (rand() % rand()) << ' '  << rand() << '\n';
+        std::cout <<  1ll * ((rand() % rand()) * (rand() % rand())) % rand() << ' '  << rand() << '\n';
         // std::cout << (pr * pr) % MOD << '\n';
         // std::cout << (pr * pr) % MOD << ' ' << (pr * pr) % MOD << '\n';
         // std::cout << (pr) % MOD << ' ' << (pr) % MOD << '\n';
