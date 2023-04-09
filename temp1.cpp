ea2012f27e02cbdd7b624f03c069032b5adb7a67
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-09 09:22:29 +0530

    temp1

--- temp1.cpp
+++ temp1.cpp
@@ -1,52 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef ONLINE_JUDGE
+#ifndef OFFLINE_JUDGE
 #pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
+#pragma GCC target("sse","sse2","sse3","ssse3","sse4","popcnt","abm","mmx","avx","avx2")
 #endif
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#ifndef __SOLVE__
-#define __SOLVE__ ios_base::sync_with_stdio(0);cin.tie(NULL);solve();return 0;
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-#define bw(i, n) for(int32_t i = n; i >= 0; --i)
 
-template <class T> using v = std::vector<T>;
-template <class T> using maxHeap = std::priority_queue<T>;
-template <class T> using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-vector<int> fact(100);
-void factorial() {
-    fact[0] = 1;
-    rep(i, 1, 100) fact[i] = (i * fact[i - 1]) % MOD;
-}
-
-const int invMod(int n) {
-    int ans = 1, expo = MOD - 2; n %= MOD;
-    while (expo) {
-        if (expo & 1) ans = (ans * 1ll * n) % MOD;
-        n = (n * 1ll * n) % MOD;
-        expo >>= 1;
+class Solution {
+public:
+    int minimumVisitedCells(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<long long>> dp(n, vector<long long>(m, INT_MAX));
+        dp[n - 1][m - 1] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                if (i == n - 1 && j == m - 1) {
+                    continue;
+                }
+                int x = grid[i][j];
+                for (int k = j + 1; k <= j + x && k < m; ++k) {
+                    dp[i][j] = min(dp[i][j], 1ll + dp[i][k]);
+                }
+                for (int k = i + 1; k <= i + x && k < n; ++k) {
+                    dp[i][j] = min(dp[i][j], 1ll + dp[k][j]);
+                }
+            }
+        }
+        debug(dp)
+        return (dp[0][0] == INT_MAX ? -1 : dp[0][0]);
     }
-    return ans;
-}
+    void test(void) {
+        vector<vector<int>> t = {{3, 4, 2, 1}, {4, 2, 3, 1}, {2, 1, 0, 0}, {2, 4, 0, 0}};
+        cout << minimumVisitedCells(t);
+        /* test */
+    }
+};
 
-void solve(void) {
-    int t = 2;
-    cout << (t >> 1);
-}
-
-signed main(void) {__SOLVE__}
+#ifdef __TEST__
+__TEST__
+#endif
