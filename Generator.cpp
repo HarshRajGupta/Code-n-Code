17e47b06807b1201a939ce186f75d1a06e8297f2
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-09 18:35:38 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -99,7 +99,7 @@ class Solution : private testCase {
         // generateArray();
         // generatePositiveArray();
         // generatePermutation();
-        generateUniqueArray(500);
+        generateUniqueArray(50);
         // generateBinaryArray();
         // generatePrimeArray();
         // generateLowerString();
