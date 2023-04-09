e05980c90f47acb45b2055b59375d4384e6e7442
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-09 18:09:56 +0530

    c++

--- c++.cpp
+++ c++.cpp
@@ -1,70 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef OFFLINE_JUDGE
+#ifdef ONLINE_JUDGE
 #pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse","sse2","sse3","ssse3","sse4","popcnt","abm","mmx","avx","avx2")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
 #endif
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-
-class Solution {
-    const int lB(const std::vector<int> &arr, const int &t) {
-        if (t > arr[arr.size() - 1])
-            return (arr.size() - 1);
-        else if (t <= arr[0])
-            return -1;
-        int l = 0, r = (arr.size() - 1), ans = - 1;
-        while (l <= r) {
-            int mid = ((l + r) / 2);
-            if (arr[mid] >= t)
-                r = (mid - 1);
-            else
-                ans = mid, l = (mid + 1);
-        }
-        return ans;
-    }
-    long long minMoves(vector<int>& nums) {
-        if (!nums.size()) return 0;
-        sort(nums.begin(), nums.end());
-        vector<long long> prefixSum(nums.size() + 1);
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
-        }
-        long long MIN = LLONG_MAX;
-        for (int i = 0; i < nums.size(); ++i) {
-            int idx = lB(nums, nums[i]);
-            MIN = min(MIN, ((nums[i] * 1ll * (idx + 1) - prefixSum[idx + 1]) + (prefixSum[nums.size()] - prefixSum[idx + 1] - (nums[i] * 1ll * (nums.size() - idx - 1)))));
-        }
-        debug(MIN, nums)
-        return MIN;
-    }
-public:
-    long long makeSubKSumEqual(vector<int>& arr, int k) {
-        long long ans = 0;
-        map<int, int> mp;
-        for (int i = 0; i < k; ++i) {
-            vector<int> tmp;
-            for (int j = i; j < arr.size() && !mp[j]; j += k, j %= arr.size()) {
-                mp[j] = 1;
-                tmp.push_back(arr[j]);
-            }
-            ans += minMoves(tmp);
-            debug(k, ans)
-        }
-        return ans;
-    }
-    void test(void) {
-        vector<int> arr = {2, 5, 5, 7};
-        cout << makeSubKSumEqual(arr, 3);
-        /* test */
-    }
-};
-
-#ifdef __TEST__
-__TEST__
+#ifndef __SOLVE__
+#define __SOLVE__ ios_base::sync_with_stdio(0);cin.tie(NULL);solve();return 0;
 #endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+
+#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
+#define bw(i, n) for(int32_t i = n; i >= 0; --i)
+
+template <class T> using v = std::vector<T>;
+template <class T> using maxHeap = std::priority_queue<T>;
+template <class T> using minHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+std::vector<int32_t> allPrimes;
+std::vector<bool> sieve(int32_t n = 1000007) {
+    std::vector<bool> isPrime(n + 1, true);
+    isPrime[0] = false, isPrime[1] = false;
+    for (int32_t i = 2; (i * i) <= n; ++i) {
+        if (isPrime[i]) {
+            allPrimes.push_back(i);
+            for (int j = (i * i); j <= n; j += i) isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
+
+void solve(void) {
+    sieve();
+    debug(allPrimes)
+}
+
+signed main(void) {__SOLVE__}
