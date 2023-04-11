a8b460f52a73a5eae6ed8e8fbd246b1e61d0231c
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-11 08:48:27 +0530

    Generator

--- Generator.cpp
+++ Generator.cpp
@@ -31,7 +31,7 @@ struct testCase {
         fflush(stdout);
     }
     void generateUniqueArray(size_t n = rand() % L + 1) {
-        size_t N = n * (n < 121 ? (rand() % 5) + 6 : sqrt((rand() % n) + sqrt(n))) / (sqrt(sqrt(rand() % n) + 1));
+        size_t N = n * (n < 121 ? (rand() % 7) + 3 : sqrt((rand() % n) + sqrt(n))) / (sqrt(sqrt(rand() % n) + 1));
         debug(N)
         while (N <= 2 * n) N = (N + rand() % n) % 400009;
         std::cout << n << '\n';
