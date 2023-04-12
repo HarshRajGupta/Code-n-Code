62892b7f3b28ee7d86c5b1be221a274ec2a4fd69
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-12 19:10:44 +0530

    c++

--- c++.cpp
+++ c++.cpp
@@ -30,13 +30,27 @@ template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(),(x).end()
 
-void static preCompute(void) {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout.flush();
+std::vector<int32_t> allPrimes;
+std::vector<bool> sieve(const int32_t n = 1000007) {
+    std::vector<bool> isPrime(n + 1, true);
+    isPrime[0] = false, isPrime[1] = false;
+    for (int32_t i = 2; (i * i) <= n; ++i)
+        if (isPrime[i])
+            for (int j = (i * i); j <= n; j += i)
+                isPrime[j] = false;
+    for (int32_t i = 2; i <= n; ++i)
+        if (isPrime[i])
+            allPrimes.push_back(i);
+    return isPrime;
 }
 
 void solve(void) {
-    int n; cin >> n;
-    cout << n / 2;
+    cout << sz(allPrimes);
+}
+
+static void preCompute(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cout.flush();
+    sieve();
 }
 
 __MAIN__
