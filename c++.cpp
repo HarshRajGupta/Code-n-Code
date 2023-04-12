6cfeab1ee1671aef56859ba090cb2128267c4bd4
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-04-12 19:50:42 +0530

    c++

--- c++.cpp
+++ c++.cpp
@@ -1,25 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef OFFLINE_JUDGE
+#ifdef ONLINE_JUDGE
 #pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse","sse2","sse3","ssse3","sse4","popcnt","abm","mmx","avx","avx2")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
 #endif
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-
-class Solution {
-public:
-    /* function */
-    static void test() {
-        cout << 1;
-    }
-};
-
-#ifdef __TEST__
-__TEST__
+#ifndef __SOLVE__
+#define __SOLVE__ signed main() {solve(); return 0;}
 #endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+
+#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
+#define bw(i, n) for(int32_t i = n; i >= 0; --i)
+
+template<class T>using v = vector<T>;
+template<class T>using maxHeap = priority_queue<T>;
+template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    /* Code */
+}
+
+__SOLVE__
