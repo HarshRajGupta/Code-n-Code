739b42f30d3382d752c5caa47e9a1108d2f431ba
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-17 10:28:14 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -1,89 +1,52 @@
-#ifndef __FAST_IO
-#pragma GCC optimize("O3", "Ofast", "unroll-loops", "omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
-#endif
-
 #include <bits/stdc++.h>
 using namespace std;
 
 #ifndef debug
-#define debug(...) ;
+#define debug(...)
 #endif
 
-const unsigned long long MOD = 1e9 + 7;
-const char ln = '\n';
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
-#define int long long
-#define ll long double
-
-#define Y std::cout << "YES";
-#define N std::cout << "NO";
-
-#define _for(i, n) for (int i = 0; i < n; ++i)
-#define rep(i, a, n) for (auto i = a; i < n; ++i)
-#define bw(i, n) for (int i = n; i >= 0; --i)
-
-#define v std::vector
-#define maxHeap std::priority_queue<int>
-#define minHeap std::priority_queue<int, std::vector<int>, std::greater<int>>
-
-#define ft first
-#define sd second
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-v<int> oddLog = {
-    0,
-    1,
-    10,
-    200,
-    2000,
-    30000,
-    300000,
-    4000000,
-    40000000,
-    500000000,
-    5000000000
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x = 0, ListNode *next = NULL) : val(x), next(next) {}
 };
 
-v<int> evenLog = {
-    0,
-    0,
-    1,
-    10,
-    200,
-    2000,
-    30000,
-    300000,
-    4000000,
-    40000000,
-    500000000,
-    5000000000
-};
-void solve() {
-    int a, b; cin >> a >> b;
-    int firstDigit = (a / ((int)pow(10, (int)log10(a))));
-    int CountOfB  = (firstDigit + 1) * (evenLog[log10(a)]);
-    if (firstDigit >= b && (((int)log10(a)) & 1)) {
-        CountOfB += (int)pow(10, (int)log10(a));
-    }
-    rep(i, 1, 9) {
-        int temp = (firstDigit + 1) * (oddLog[log10(a)]);
-        if (firstDigit >= i && (((int)log10(a)) & 1)) {
-            CountOfB += (int)pow(10, (int)log10(a));
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return NULL;
+        if (!head->next || !head->next->next) return head;
+        ListNode *odd, *even, *codd, *ceven, *it = head;
+        int pos = 1;
+        while (it) {
+            if (pos) {
+                if (odd) {
+                    codd->next = it;
+                    codd = codd->next;
+                }
+                else {
+                    odd = codd = it;
+                }
+            } else {
+                if (even) {
+                    ceven->next = it;
+                    ceven = ceven->next;
+                } else {
+                    even = ceven = it;
+                }
+            }
+            it = it->next;
         }
+        codd->next = even;
+        return odd;
     }
-}
+    void test() {
+        /* test */
+    }
+};
 
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-#ifdef __TAG1
-    __TAG1
+#ifdef __TEST__
+__TEST__
 #endif
-    solve();
-#ifdef __TAG2
-    __TAG2
-#endif
-    return 0;
-}
