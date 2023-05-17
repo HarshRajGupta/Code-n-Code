fe9ba5ddd928aff692549dd78e83c931ea341569
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-17 18:12:05 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -7,55 +7,32 @@ using namespace std;
 
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x = 0, ListNode *next = NULL) : val(x), next(next) {}
-    void traverse(function<void(int)> f = [](int x) {cout << x << ' ';}) {
-        f(val);
-        if (next) next->traverse(f);
-    }
-};
-
 class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
-        if (!head) return NULL;
-        if (!head->next || !head->next->next) return head;
-        ListNode *odd = NULL, *even = NULL, *codd = NULL, *ceven = NULL, *it = head;
-        int pos = 1;
-        while (it != NULL) {
-            debug(it->val);
-            const auto temp = it->next;
-            if (pos) {
-                if (codd != NULL) {
-                    codd->next = it;
-                    codd = codd->next;
-                }
-                else {
-                    odd = codd = it;
-                }
-                pos = 0;
-            } else {
-                if (ceven != NULL) {
-                    ceven->next = it;
-                    ceven = ceven->next;
-                } else {
-                    even = ceven = it;
-                }
-                pos = 1;
-            }
-            it = temp;
+    const int uB(const std::vector<int> &arr, const int t, int l = 0, int r = -1) {
+        if (r == -1) r = (arr.size() - 1);
+        if (t < arr[l]) return l;
+        if (t > arr[r]) return r + 1;
+        int ans = r + 1, mid;
+        while (l <= r) {
+            mid = (l + r) >> 1;
+            if (arr[mid] <= t) l = (mid + 1);
+            else ans = mid, r = (mid - 1);
         }
-        ceven->next = NULL;
-        if (codd) codd->next = even;
-        return odd;
+        return ans;
+    }
+    int64_t ceil (long double n, long double d = 1) {
+        return (n / d) + (n > (d * ((int64_t)(n / d))));
+    }
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end());
+        vector<int> ans(spells.size());
+
     }
     void test() {
-        auto h = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        h->traverse();
-        auto res = oddEvenList(h);
-        res->traverse();
+        /* test */
+        vector<int> arr = {1, 3, 4, 5, 6, 7, 8, 9};
+        cout << arr[uB(arr, 5)];
     }
 };
 
