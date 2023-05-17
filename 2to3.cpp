a9b4aad0c36e89ec8e1c1c8af0d0178db186d9ce
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-17 11:42:40 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -8,10 +8,49 @@ using namespace std;
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
 class Solution {
+    string itos(int &n) {
+        if (n == 0) return "0";
+        string res;
+        while (n) {
+            res.push_back(n % 10 + '0');
+            n /= 10;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
 public:
-    /* function */
+    string removeDuplicates(string s, int k) {
+        stack<string> stringHash;
+        int count = 0;
+        string res;
+        for (auto &i : s) {
+            if (stringHash.empty() || stringHash.top() != string(1, i)) {
+                stringHash.push(itos(count));
+                stringHash.push(string(1, i));
+                count = 1;
+            } else {
+                ++count;
+                if (count == k) {
+                    stringHash.pop();
+                    count = stoi(stringHash.top());
+                    stringHash.pop();
+                }
+            }
+        }
+        stringHash.push(itos(count));
+        while (!stringHash.empty()) {
+            int count = stoi(stringHash.top());
+            stringHash.pop();
+            while (count--) {
+                res += (stringHash.top());
+            }
+            if (!stringHash.empty()) stringHash.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
     void test() {
-        cout << 1;
+
     }
 };
 
