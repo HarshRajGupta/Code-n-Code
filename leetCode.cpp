f978e2f2a1eb7d6a67fc02b329dd686c6646a6b4
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-18 06:58:45 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -8,18 +8,27 @@ using namespace std;
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
 class Solution {
-    int subSeq(const vector<int> &nums, const int t, int index = 0, int sum = 0) {
-        if (index == nums.size() || sum >= t) {
-            return (sum == t);
-        }
-        return subSeq(nums, t, index + 1, sum) + subSeq(nums, t, index, sum + nums[index]);
-    }
 public:
-    int combinationSum4(vector<int>& nums, int target) {
-        return subSeq(nums, target);
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ans(temperatures.size(), 0);
+        for (int i = ans.size() - 2; i >= 0; --i) {
+            if (temperatures[i] < temperatures[i + 1]) ans[i] = 1;
+            else {
+                int j = ans[i + 1];
+                while (j && temperatures[i] >= temperatures[i + j + 1] ) {
+                    if (ans[i + j + 1]) j += ans[i + j + 1];
+                    else j = 0;
+                }
+                if (temperatures[i] < temperatures[i + j + 1])
+                    ans[i] = j + 1;
+            }
+        }
+        return ans;
     }
     void test() {
-        /* test */
+        vector<int> temp = {55, 38, 53, 81, 61, 93, 97, 32, 43, 78};
+        vector<int> ans = dailyTemperatures(temp);
+        debug(ans)
     }
 };
 
