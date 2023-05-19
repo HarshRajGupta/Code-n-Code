3e77b43d40b97f52d745c6f389a59ebaa01aadfd
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-19 20:09:53 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,63 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","Ofast","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "popcnt", "abm", "mmx", "avx", "avx2")
+#endif
+
 #ifndef debug
 #define debug(...)
 #endif
 
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        const int n = nums.size();
-        vector<vector<int>> pro(n + 1, vector<int>(2));
-        int MAX = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            if (nums[i] > 0) {
-                if (pro[i + 1][0]) {
-                    pro[i][0] = pro[i + 1][0] * nums[i];
-                } else {
-                    pro[i][0] = nums[i];
-                }
-                if (pro[i + 1][1]) {
-                    pro[i][1] = pro[i + 1][1] * nums[i];
-                } else {
-                    pro[i][1] = nums[i];
-                }
-            } else if (nums[i] < 0) {
-                if (pro[i + 1][0]) {
-                    pro[i][1] = pro[i + 1][0] * nums[i];
-                } else {
-                    pro[i][1] = nums[i];
-                }
-                if (pro[i + 1][1]) {
-                    pro[i][0] = pro[i + 1][1] * nums[i];
-                } else {
-                    pro[i][0] = nums[i];
-                }
-            }
-            MAX = max(MAX, pro[i][0]);
-        }
-    }
-    int findMin(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1, mid;
-        while (l < r) {
-            mid = (l + r) >> 1;
-            if (nums[mid] == nums[r]) --r;
-            else if (nums[mid] >= nums[l]) l = mid + 1;
-            else r = mid;
-        }
-        return nums[l];
-    }
-    void test() {
-        rep(i, 5, 25) {
-            int t = (-1 * i) % 5;
-            debug(i, t)
-        }
-    }
-};
-
-#ifdef __TEST__
-__TEST__
+#ifndef __MAIN__
+#define __MAIN__ signed main() {preCompute(); signed t; cin >> t; while (t--)solve(), cout << '\n'; return 0;}
 #endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
+
+template<class T>using v = vector<T>;
+template<class T>using maxHeap = priority_queue<T>;
+template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+
+void solve(void) {
+    int n; string s;
+    cin >> n >> s;
+    set<string> st;
+    rep(i, 0, n - 1) {
+        string t = {s[i], s[i + 1]};
+        st.insert(t);
+    }
+    cout << sz(st);
+}
+
+static bool preCompute(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush(); return 0;
+}
+
+__MAIN__
