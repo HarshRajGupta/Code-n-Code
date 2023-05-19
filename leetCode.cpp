d3616e86df3cc8fecf082ee20ca262dca8e0dd38
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-19 16:44:58 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -5,62 +5,56 @@ using namespace std;
 #define debug(...)
 #endif
 
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
+
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        const int n = height.size();
-        vector<int> leftMonotony(n), rightMonotony(n, 0), prefixSum(n + 1);
-        int currMax = 0;
-        for (int i = 0; i < n; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + height[i];
-            if (currMax < height[i]) {
-                currMax = height[i];
-                rightMonotony[i] = 0;
-            } else {
-                int j = i - 1;
-                while (height[j] < height[i]) {
-                    j -= rightMonotony[j];
-                }
-                rightMonotony[i] = i - j;
-            }
-        }
-        currMax = 0;
+    int maxProduct(vector<int>& nums) {
+        const int n = nums.size();
+        vector<vector<int>> pro(n + 1, vector<int>(2));
+        int MAX = 0;
         for (int i = n - 1; i >= 0; --i) {
-            if (currMax < height[i]) {
-                currMax = height[i];
-                leftMonotony[i] = 0;
-            } else {
-                int j = i + 1;
-                while (height[j] < height[i]) {
-                    j += leftMonotony[j];
+            if (nums[i] > 0) {
+                if (pro[i + 1][0]) {
+                    pro[i][0] = pro[i + 1][0] * nums[i];
+                } else {
+                    pro[i][0] = nums[i];
                 }
-                leftMonotony[i] = j - i;
+                if (pro[i + 1][1]) {
+                    pro[i][1] = pro[i + 1][1] * nums[i];
+                } else {
+                    pro[i][1] = nums[i];
+                }
+            } else if (nums[i] < 0) {
+                if (pro[i + 1][0]) {
+                    pro[i][1] = pro[i + 1][0] * nums[i];
+                } else {
+                    pro[i][1] = nums[i];
+                }
+                if (pro[i + 1][1]) {
+                    pro[i][0] = pro[i + 1][1] * nums[i];
+                } else {
+                    pro[i][0] = nums[i];
+                }
             }
+            MAX = max(MAX, pro[i][0]);
         }
-        debug(height, leftMonotony, rightMonotony, prefixSum)
-        int ans = 0, i = 0;
-        while (height[i] != currMax) {
-            int j = i + leftMonotony[i];
-            debug(i, j)
-            ans += ((j - i - 1) * height[i]) - (prefixSum[j] - prefixSum[i + 1]);
-            debug(i, j, ans)
-            i = j;
+    }
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1, mid;
+        while (l < r) {
+            mid = (l + r) >> 1;
+            if (nums[mid] == nums[r]) --r;
+            else if (nums[mid] >= nums[l]) l = mid + 1;
+            else r = mid;
         }
-        debug(ans)
-        currMax = i;
-        i = n - 1;
-        while (i != currMax) {
-            int j = i - rightMonotony[i];
-            debug(i, j)
-            ans += (i - j - 1) * height[i] - (prefixSum[i] - prefixSum[j + 1]);
-            debug(i, j, ans)
-            i = j;
-        }
-        return ans;
+        return nums[l];
     }
     void test() {
-        vector<int> t = {2, 0, 2};
-        cout << trap(t) << endl;
+        rep(i, 5, 25) {
+            int t = (-1 * i) % 5;
+            debug(i, t)
+        }
     }
 };
 
