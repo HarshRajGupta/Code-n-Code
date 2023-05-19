2e92446542ca0bb37d2db5003eaec27e215570e3
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-19 21:29:36 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -237,7 +237,7 @@ void solve(void) {
 }
 
 static bool preCompute(void) {
-    // ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush(); return 0;
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush(); return 0;
 }
 
 __MAIN__
