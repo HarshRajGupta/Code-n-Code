3485a242fe5583055a01429d146126e2ff14c4c0
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 20:03:52 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -5,7 +5,7 @@ using namespace  __gnu_debug;
 class Solution {
 public:
     void test() {
-        vector<set<int>> arr(2); 
+        set<int> arr[2];
         arr[1].insert(7);
         debug(arr)
     }
