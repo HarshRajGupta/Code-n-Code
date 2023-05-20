9795d1b9c52f0bf595725f3ed0cbbfa5220306c0
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 22:08:39 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -1,12 +1,30 @@
 #include <bits/stdc++.h>
-using namespace  __gnu_debug;
+using namespace __gnu_debug;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
 class Solution {
 public:
+    int characterReplacement(string s, int k) {
+        vector<int> cnt(26, 0);
+        int i = 0, j = 0, MAX = 0, count = 0;
+        while (j < s.size()) {
+            ++cnt[s[j] - 'A'];
+            if (j - i + 1 - cnt[s[j] - 'A'] > k) {
+                cnt[s[i] - 'A']--;
+                ++i;
+            }
+            MAX = max(MAX, j - i + 1);
+            ++j;
+        }
+        return MAX;
+    }
     void test() {
-        vector<set<int>> arr(5);
-        arr[1].insert(7);
-        debug(arr)
+        cout << characterReplacement("BAAAB", 2);
     }
 };
 
