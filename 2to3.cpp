076a4a60c049f1a16d73851508f911450fa9bbd9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 19:00:16 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -1,21 +1,11 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-#ifndef debug
-#define debug(...)
-#endif
-
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
 class Solution {
 public:
     /* function */
     void test() {
-        vector<map<int, int>> mp(2);
-        mp[1][0] = 4;
-        mp[0][1] = 9;
-        mp[0][7] = 1;
-        debug(mp)
+        std::vector<int> a = {1, 7, -1};
+        debug(a)
     }
 };
 
