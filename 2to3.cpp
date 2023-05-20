adab76bb99f11c9af2ab6e6de5da75298f6a2fb0
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 19:25:15 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x = 0, TreeNode *left = NULL, TreeNode *right = NULL) : val(x), left(left), right(right) {}
+};
+
 
 class Solution {
 public:
     /* function */
     void test() {
-        std::vector<int> a = {1, 7, -1};
-        debug(a)
+        // TreeNode *root = new TreeNode(1, new TreeNode(2, NULL, new TreeNode(5)), new TreeNode(3));
+        // debug(root)
+        debug::ListNode *head = new debug::ListNode(1, new debug::ListNode(2, new debug::ListNode(3, new debug::ListNode(4, new debug::ListNode(5)))));
+        debug(head)
     }
 };
 
