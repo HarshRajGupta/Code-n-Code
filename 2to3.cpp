d6478995d596f6037f0fcee1cc562f6fa2fa37ff
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 20:01:09 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -5,7 +5,8 @@ using namespace  __gnu_debug;
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        string S = "12"; debug(S)
+        string S = "12"; 
+        debug(S)
     }
     /* function */
     void test() {
