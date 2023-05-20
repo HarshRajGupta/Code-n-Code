e3b28b0cfd453e68ab8cd7716f51dc41afdbf9ac
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 06:46:43 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -8,49 +8,27 @@ using namespace std;
 #define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 
 class Solution {
-    string itos(int &n) {
-        if (n == 0) return "0";
-        string res;
-        while (n) {
-            res.push_back(n % 10 + '0');
-            n /= 10;
-        }
-        reverse(res.begin(), res.end());
-        return res;
-    }
 public:
-    string removeDuplicates(string s, int k) {
-        stack<string> stringHash;
-        int count = 0;
-        string res;
-        for (auto &i : s) {
-            if (stringHash.empty() || stringHash.top() != string(1, i)) {
-                stringHash.push(itos(count));
-                stringHash.push(string(1, i));
-                count = 1;
+    int longSubarrWthSumDivByK(int arr[], int n, int k) {
+        vector<int> prefixSum(n + 1);
+        map<int, pair<int, int>> mp;
+        pair<int, int> testPair;
+        int MAX = 0;
+        rep(i, 0, n) {
+            prefixSum[i + 1] = (prefixSum[i] + arr[i]) % k;
+            if (mp[prefixSum[i + 1]] == testPair) {
+                mp[prefixSum[i + 1]] = {i + 1, i + 1};
             } else {
-                ++count;
-                if (count == k) {
-                    stringHash.pop();
-                    count = stoi(stringHash.top());
-                    stringHash.pop();
-                }
+                auto [l, r] = mp[prefixSum[i + 1]];
+                mp[prefixSum[i + 1]] = {min(i + 1, l), max(i + 1, r)};
+                MAX = max(MAX, mp[prefixSum[i + 1]].second - mp[prefixSum[i + 1]].first + 1);
             }
         }
-        stringHash.push(itos(count));
-        while (!stringHash.empty()) {
-            int count = stoi(stringHash.top());
-            stringHash.pop();
-            while (count--) {
-                res += (stringHash.top());
-            }
-            if (!stringHash.empty()) stringHash.pop();
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        return MAX;
     }
     void test() {
-
+        int vt[] = {20, 13, 11, 14, 2, 7, 4, 2, 6, 7, 6, 13, 6};
+        cout << longSubarrWthSumDivByK(vt, 13, 1);
     }
 };
 
