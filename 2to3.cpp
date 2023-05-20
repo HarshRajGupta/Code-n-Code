7a15e35b31b4158aebb74685872e2d186e1f974d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-20 19:53:53 +0530

    2to3

--- 2to3.cpp
+++ 2to3.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
-using namespace __dataStructures;
-
-// struct TreeNode {
-//     int val;
-//     TreeNode *left, *right;
-//     TreeNode(int x = 0, TreeNode *left = NULL, TreeNode *right = NULL) : val(x), left(left), right(right) {}
-// };
+using namespace  __gnu_debug;
 
 
 class Solution {
 public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+
+    }
     /* function */
     void test() {
-        TreeNode *root = new TreeNode(1, new TreeNode(2, NULL, new TreeNode(5)), new TreeNode(3));
-        debug(root)
-        auto head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        debug(head)
-        std::cout << head->val;
+
     }
 };
 
