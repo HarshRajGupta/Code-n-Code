d97db199ebad8f207ef26eb05aa5268003972db0
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-23 17:08:11 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -6,17 +6,9 @@ using namespace __gnu_debug;
 #endif
 
 class Solution {
-    int factorial(int n) {
-        int res = 1;
-        for (int i = 2; i <= n; ++i)
-            res *= i;
-        return res;
-    }
-
 public:
     void test() {
         /* test */
-        cout << factorial(10);
     }
 };
 
