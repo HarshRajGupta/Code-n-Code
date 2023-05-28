6d0e56fff8f1fc98b0c249b7c6e3bc0211506c9f
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-28 08:17:57 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -40,6 +40,7 @@ public:
         /* test */
         vector<vector<int>> grid = {{1,2,3},{4,5,6},{7,8,9}};
         vector<vector<int>> ans = differenceOfDistinctValues(grid);
+        debug(ans)
     }
 };
 
