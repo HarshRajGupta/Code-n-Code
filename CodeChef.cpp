0b0323809788536584acb5a38c4183968998fbd6
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-05-28 08:21:44 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -36,11 +36,38 @@ public:
                 i++; j++;
             }
         }
+        for (int k = 0; k < grid.size(); ++k) {
+            unordered_map<int, int> mp, mp2;
+            int i = k, j = 0, count = 0, pre = 0;
+            while (i < grid.size() && j < grid[0].size()) {
+                mp[grid[i][j]]++;
+                if (mp[grid[i][j]] == 1) {
+                    count++;
+                }
+                i++;
+                j++;
+            }
+            i = k, j = 0;
+            while (i < grid.size() && j < grid[0].size()) {
+                // debug(i, j, count, pre)
+                mp[grid[i][j]]--;
+                if (mp[grid[i][j]] == 0) {
+                    count--;
+                }
+                debug(i, j, k, count, pre)
+                ans[i][j] = abs(count - pre);
+                mp2[grid[i][j]]++;
+                if (mp2[grid[i][j]] == 1) {
+                    pre++;
+                }
+                i++; j++;
+            }
+        }
         return ans;
     }
     void test() {
         /* test */
-        vector<vector<int>> grid = {{1,2,3},{4,5,6},{7,8,9}};
+        vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         vector<vector<int>> ans = differenceOfDistinctValues(grid);
         debug(ans)
     }
