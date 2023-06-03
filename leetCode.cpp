0c930ca607b178420038798ef4a7312d7fd8decf
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-03 18:28:41 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -7,9 +7,25 @@ using namespace __gnu_debug;
 
 class Solution {
 public:
+    int MaximizeEqualNumbers(int n, int k, vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        int i = 0, j = 1;
+        while (j < n) {
+            if (nums[j] - nums[i] <= (2 * k)) {
+                ans = max(ans, j - i + 1);
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return ans;
+    }
     void test() {
-        ListNode* l = new ListNode(1);
-        debug(l);
+        int n = 4, k = 1;
+        vector<int> nums = {2, 1, 5, 2};
+        cout << MaximizeEqualNumbers(n, k, nums) << endl;
+        /* test */
     }
 };
 
