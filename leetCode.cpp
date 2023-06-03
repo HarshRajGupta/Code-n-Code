92f88ef7c760e6b2975046032faf3208306a7e04
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-03 18:47:22 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -14,6 +14,21 @@ const int lB(const std::vector<int> &arr, const int t, int l = 0, int r = -1) {
     return ans;
 }
 
+const int bS(const vector<int> &arr, const int t, int l = 0, int r = -1) {
+    if (r == -1)
+        r = (arr.size() - 1);
+    while (l <= r) {
+        const int mid = (l + r) >> 1;
+        if (arr[mid] == t)
+            return mid;
+        else if (arr[mid] > t)
+            r = (mid - 1);
+        else
+            l = (mid + 1);
+    }
+    return ((arr[l] == t) ? l : ((arr[r] == t) ? r : -1));
+}
+
 vector<int> find_x(int n, int k, vector<int> &q) {
     int curr = 0, pos = 1;
     bool inc = true;
@@ -33,9 +48,12 @@ vector<int> find_x(int n, int k, vector<int> &q) {
     debug(arr)
     vector<int> ans;
     for (int i : q) {
-        int pos = lB(arr, i + 1);
-        debug(i, pos)
-        ans.push_back(pos + 1);
+        int pos = bS(arr, i);
+        if (pos != -1) ans.push_back(pos + 1);
+        else {
+            pos = lB(arr, i);
+            ans.push_back(pos + 1);
+        }
     }
     return ans;
 }
