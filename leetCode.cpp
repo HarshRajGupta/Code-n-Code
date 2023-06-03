35e0e2ece11c0ffef9025c161d57ec927f2bf0ad
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-03 18:43:46 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -1,34 +1,49 @@
 #include <bits/stdc++.h>
-using namespace __gnu_debug;
+using namespace std;
 
-#ifndef debug
-#define debug(...)
-#endif
+const int lB(const std::vector<int> &arr, const int t, int l = 0, int r = -1) {
+    if (r == -1) r = (arr.size() - 1);
+    if (arr[l] >= t) return l - 1;
+    if (arr[r] < t) return r;
+    int ans = l - 1, mid;
+    while (l <= r) {
+        mid = (l + r) >> 1;
+        if (arr[mid] >= t) r = (mid - 1);
+        else ans = mid, l = (mid + 1);
+    }
+    return ans;
+}
 
-class Solution {
-public:
-    int MaximizeEqualNumbers(int n, int k, vector<int>& nums) {
-        int ans = 0;
-        sort(nums.begin(), nums.end());
-        int i = 0, j = 1;
-        while (i <= j && j < n) {
-            if (nums[j] - nums[i] <= (2 * k)) {
-                ans = max(ans, j - i + 1);
-                j++;
-            } else {
-                i++;
-            }
+vector<int> find_x(int n, int k, vector<int> &q) {
+    int curr = 0, pos = 1;
+    bool inc = true;
+    vector<int> arr;
+    while (curr < n * n) {
+        if (pos >= n) inc = false;
+        if (inc) {
+            curr += pos;
+            pos++;
+            arr.push_back(curr);
+        } else {
+            curr += pos;
+            pos--;
+            arr.push_back(curr);
         }
-        return ans;
     }
-    void test() {
-        int n = 4, k = 1;
-        vector<int> nums = {2, 3, 4, 3};
-        cout << MaximizeEqualNumbers(n, k, nums) << endl;
-        /* test */
+    vector<int> ans;
+    for (int i : q) {
+        int pos = lB(arr, i);
+        if (pos == -1) ans.push_back(i);
+        else ans.push_back(i - arr[pos]);
     }
-};
+    return ans;
+}
 
-#ifdef __TEST__
-__TEST__
-#endif
+signed main(void) {
+    int n, q; cin >> n >> q;
+    vector<int> queries(q);
+    for (int &i : queries) cin >> i;
+    vector<int> ans = find_x(n, q, queries);
+    for (int i : ans) cout << i << ' ';
+    return 0;
+}
