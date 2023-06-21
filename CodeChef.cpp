be5ed4f5670050f924bdc9d83be118e9ad302a66
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-21 19:42:00 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -26,7 +26,7 @@ public:
         return ans;
     }
     void test() {
-        auto root = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(7))), new TreeNode(8, new TreeNode(13), new TreeNode(4, nullptr, new TreeNode(5))));
+        auto root = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))), new TreeNode(8, new TreeNode(13), new TreeNode(4, nullptr, new TreeNode(5))));
         debug(fun(root, 22));
     }
 };
