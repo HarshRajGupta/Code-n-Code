b29aacd23159288935a35bace54ac830e8fc6deb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-21 19:41:32 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -7,69 +7,27 @@ using namespace __gnu_debug;
 
 class Solution {
 public:
-    vector<vector<int>> differenceOfDistinctValues(vector<vector<int>>& grid) {
-        vector<vector<int>> ans = vector<vector<int>>(grid.size(), vector<int>(grid[0].size(), 0));
-        for (int k = 0; k < grid[0].size(); ++k) {
-            unordered_map<int, int> mp, mp2;
-            int i = 0, j = k, count = 0, pre = 0;
-            while (i < grid.size() && j < grid[0].size()) {
-                mp[grid[i][j]]++;
-                if (mp[grid[i][j]] == 1) {
-                    count++;
-                }
-                i++;
-                j++;
-            }
-            i = 0, j = k;
-            while (i < grid.size() && j < grid[0].size()) {
-                // debug(i, j, count, pre)
-                mp[grid[i][j]]--;
-                if (mp[grid[i][j]] == 0) {
-                    count--;
-                }
-                debug(i, j, k, count, pre)
-                ans[i][j] = abs(count - pre);
-                mp2[grid[i][j]]++;
-                if (mp2[grid[i][j]] == 1) {
-                    pre++;
-                }
-                i++; j++;
-            }
+    vector<vector<int>> ans;
+    void checkPath(TreeNode* root, vector<int> &path, int sum) {
+        if (root == nullptr) {
+            if (sum == 0) ans.push_back(path);
+            return;
         }
-        for (int k = 0; k < grid.size(); ++k) {
-            unordered_map<int, int> mp, mp2;
-            int i = k, j = 0, count = 0, pre = 0;
-            while (i < grid.size() && j < grid[0].size()) {
-                mp[grid[i][j]]++;
-                if (mp[grid[i][j]] == 1) {
-                    count++;
-                }
-                i++;
-                j++;
-            }
-            i = k, j = 0;
-            while (i < grid.size() && j < grid[0].size()) {
-                // debug(i, j, count, pre)
-                mp[grid[i][j]]--;
-                if (mp[grid[i][j]] == 0) {
-                    count--;
-                }
-                debug(i, j, k, count, pre)
-                ans[i][j] = abs(count - pre);
-                mp2[grid[i][j]]++;
-                if (mp2[grid[i][j]] == 1) {
-                    pre++;
-                }
-                i++; j++;
-            }
-        }
+        if (sum < 0) return;
+        path.push_back(root->val);
+        sum -= root->val;
+        checkPath(root->left, path, sum);
+        checkPath(root->right, path, sum);
+        path.pop_back();
+    }
+    vector<vector<int>> fun(TreeNode* root, int s) {
+        vector<int> path;
+        checkPath(root, path, s);
         return ans;
     }
     void test() {
-        /* test */
-        vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        vector<vector<int>> ans = differenceOfDistinctValues(grid);
-        debug(ans)
+        auto root = new TreeNode(5, new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(7))), new TreeNode(8, new TreeNode(13), new TreeNode(4, nullptr, new TreeNode(5))));
+        debug(fun(root, 22));
     }
 };
 
