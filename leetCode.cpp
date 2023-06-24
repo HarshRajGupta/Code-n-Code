e20e94f9de6ba62474af5fec86199001e792a076
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-24 14:11:06 +0530

    leetCode

--- leetCode.cpp
+++ leetCode.cpp
@@ -6,6 +6,18 @@ using namespace __gnu_debug;
 #endif
 
 class Solution {
+    const int uB(const std::vector<int> &arr, const int t, int l = 0, int r = -1) {
+        if (r == -1) r = (arr.size() - 1);
+        if (t < arr[l]) return l;
+        if (t > arr[r]) return r + 1;
+        int ans = r + 1, mid;
+        while (l <= r) {
+            mid = (l + r) >> 1;
+            if (arr[mid] <= t) l = (mid + 1);
+            else ans = mid, r = (mid - 1);
+        }
+        return ans;
+    }
 public:
     vector<int> fun(int n, vector<int> &nums) {
         vector<int> ans(n, 1), arr = {nums[0]};
@@ -15,7 +27,7 @@ public:
                 ans[i] = (arr.size());
             }
             else {
-                int low = upper_bound(arr.begin(), arr.end(), nums[i]) - arr.begin();
+                int low = uB(arr, nums[i]);
                 arr[low] = nums[i];
                 ans[i] = low + 1;
             }
