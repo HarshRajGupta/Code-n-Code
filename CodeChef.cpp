2417dcc8ba1f693a8ecd0f4376ffa7ea1415a133
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-25 13:41:46 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -10,24 +10,25 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __SOLVE__
-#define __SOLVE__ signed main() {solve(); return 0;}
+#ifndef __MAIN__
+#define __MAIN__ signed main() {preCompute(); signed t; cin >> t; while (t--)solve(), cout << '\n'; return 0;}
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
 
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
-#define bw(i, n) for(int32_t i = n; i >= 0; --i)
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
 
 template<class T>using v = vector<T>;
 template<class T>using maxHeap = priority_queue<T>;
 template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 #define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
+#define all(x) (x).begin(),(x).end()
 
 long long equi (int n, vector<int> &a) {
     int even = 0, odd = 0, MAX = 0;
@@ -85,9 +86,44 @@ long long equi (int n, vector<int> &a) {
     }
 }
 
-void solve(void) {
-    v<int> a = {1, 2, 2};
-    cout << equi(a.size(), a);
+long long equi2 (int n, vector<int> &a) {
+    int even = 0, odd = 0, MAX = 0;
+    for (auto &i : a) {
+        if (i & 1) ++odd;
+        else ++even;
+        MAX = max(MAX, i);
+    }
+    long long ans = 0;
+    if (odd & 1 && even & 1)
+        return -1;
+    if ((odd & 1 && !(MAX & 1)) || (even & 1 && MAX & 1))
+        ++MAX;
+    if (odd & 1)
+        ans += even / 2;
+    if (even & 1)
+        ans += odd / 2;
+    for (auto &i : a) {
+        ans += (MAX - i) / 2;
+    }
+    return ans;
 }
 
-__SOLVE__
+void solve(void) {
+    int n; cin >> n;
+    v<int> a(n);
+    foreach(i, a) cin >> i;
+    int one = equi(n, a), two = equi2(n, a);
+    if (equi(n, a) != equi2(n, a)) {
+        debug(one, two, a)
+    }
+}
+
+bool preCompute(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush(); return 0;
+}
+
+__MAIN__
+
+/*
+ * @ScratchPad
+*/
