47baa3372034ad693cae1868531f1eed626cc9b8
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-25 13:43:58 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -118,7 +118,7 @@ void solve(void) {
     v<int> a(n);
     foreach(i, a) cin >> i;
     int one = equi(n, a), two = equi2(n, a);
-    if (one != 2) {
+    if (one != two) {
         debug(one, two, a)
     }
 }
