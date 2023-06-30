f6a60cd3fb31c55432ea62860c2a0b1ac50c0445
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-06-30 18:43:47 +0530

    geeksForGeeks

---
+++ geeksForGeeks.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace __gnu_debug;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+class Solution {
+	
+
+public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        // Your code goes here
+    }
+    void test() {
+        /* test */
+    }
+};
+
+/*
+ > @ScratchPad 
+*/
+
+#ifdef __TEST__
+__TEST__
+#endif
