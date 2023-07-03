7532273b121ee3a8b529d0d2e8e9bd5267eecd3a
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-03 14:21:09 +0530

    CodeChef

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -1,3 +1,8 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,36 +10,38 @@ using namespace std;
 #define debug(...)
 #endif
 
+#ifndef __MAIN__
+#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
+#endif
 
+#define int long long
 const uint64_t MOD = 1e9 + 7;
 const char ln = '\n';
 
-#define int long long
-#define ll long double
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+#define bw(i, n) for (int32_t i = n; i >= 0; --i)
 
-template <class T> using v = std::vector<T>;
-template <class T> using xHeap = std::priority_queue<T>;
-template <class T> using nHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T>using v = vector<T>;
+template<class T>using maxHeap = priority_queue<T>;
+template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
-#define _for(i, n) for(int64_t i = 0; i < (int64_t)n; ++i)
-#define rep(i, a, n) for(auto i = a; i < n; ++i)
-#define bw(i, n) for(int64_t i = n; i >= 0; --i)
-
-#define ft first
-#define sd second
-#define sz(x) ((int64_t)(x).size())
-#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
 
 void solve() {
-    /* Code */
-    int n; 
-    cin >> n;
-    cout << n;
+    int n; cin >> n;
+    if (n <= 15) cout << "YES";
+    else cout << "NO";
 }
 
-signed main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-        uint32_t tCs = 4;
-    for (uint32_t tC = 0; tC++ < tCs; solve(), cout << ln);
-        return 0;
+void preCompute() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
 }
+
+/**
+ * @ScratchPad
+ */
+
+__MAIN__
