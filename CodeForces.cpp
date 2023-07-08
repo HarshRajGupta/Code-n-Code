32323833692d411167d45c7f5bf447734f5086be
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-08 20:28:07 +0530

    CodeForces

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -14,6 +14,7 @@ class Solution {
         int num = 0;
         int res = 1e5;
         if (s[pos] == '0') return -1;
+        if (dp[pos] != 1e5) return dp[pos];
         for (int i = pos; i < s.size(); ++i) {
             num = (num * 2) + s[i] - '0';
             if (powersOf5.find(num) != powersOf5.end()) {
@@ -21,11 +22,11 @@ class Solution {
                 if (t != -1) res = min(res, 1 + t);
             }
         }
-        return res == 1e5 ? -1 : res;
+        return dp[pos] = (res == 1e5 ? -1 : res);
     }
 public:
     int minimumBeautifulSubstrings(string s) {
-        dp = vector<int>(15, -1);
+        dp = vector<int>(15, 1e5);
         return ans(s, 0);
     }
     void test() {
