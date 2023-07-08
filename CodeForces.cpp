d92580ebd5209c4b6a47bd0c8a7591e7b97aaebb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-08 20:26:58 +0530

    CodeForces

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -6,23 +6,30 @@ using namespace __gnu_debug;
 #endif
 
 class Solution {
-public:
-    int alternatingSubarray(vector<int>& nums) {
-        int ans = -1;
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            if (nums[i + 1] != nums[i] + 1) continue;
-            for (int j = i + 1; j < nums.size(); ++j) {
-                if ((j - i) & 1 && nums[j] != nums[i + 1]) break;
-                if (!((j - i) & 1) && nums[j] != nums[i]) break;
-                ans = max(ans, j - i + 1);
+    vector<int> dp;
+    set<int> powersOf5 = {1, 5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, 9765625, 48828125, 244140625};
+
+    int ans(string s, int pos) {
+        int num = 0;
+        int res = 1e5;
+        if (s[pos] == '0') return -1;
+        for (int i = pos; i < s.size(); ++i) {
+            num = (num * 2) + s[i] - '0';
+            if (powersOf5.find(num) != powersOf5.end()) {
+                int t = ans(s, i + 1);
+                if (t != -1) res = min(res, 1 + t);
             }
         }
-        return ans;
+        return res == 1e5 ? -1 : res;
+    }
+public:
+    int minimumBeautifulSubstrings(string s) {
+        dp = vector<int>(15, -1);
+        return ans(s, 0);
     }
     void test() {
-        vector<int> a = {4,5,6};
-        cout << alternatingSubarray(a);
         /* test */
+        cout << minimumBeautifulSubstrings("1011");
     }
 };
 
