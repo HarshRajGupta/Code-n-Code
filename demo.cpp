1a29c91484be3eac2687c9f5137669a92a14cbc8
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-09 18:08:12 +0530

    demo

--- demo.cpp
+++ demo.cpp
@@ -9,7 +9,6 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         vector<vector<int>> ans(mat.size(), vector<int>(mat[0].size(), 1e9));
-        vector<vector<bool>> visited(mat.size(), vector<bool>(mat[0].size(), false));
         queue<pair<vector<int>, int>> q;
         for (int i = 0; i < mat.size(); ++i) {
             for (int j = 0; j < mat[0].size(); ++j) {
