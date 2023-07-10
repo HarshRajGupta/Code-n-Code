7f5fb9f937d7941200fb4dbdca76a3d7fcb4a45a
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-10 08:08:40 +0530

    test

--- test.cpp
+++ test.cpp
@@ -1,32 +1,58 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
+
 #include <bits/stdc++.h>
-using namespace __gnu_debug;
+using namespace std;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        for (auto &i : nums) {
-            minHeap.push(i);
-            if (minHeap.size() > k) minHeap.pop();
+#ifndef __MAIN__
+#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+
+template<class T>using v = vector<T>;
+template<class T>using maxHeap = priority_queue<T>;
+template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+
+void solve() {
+    int n; cin >> n;
+    v<int> arr(n);
+    foreach(i, arr) cin >> i;
+    int ans = 0, prev = 0, sum = 0;
+    foreach (i, arr) {
+        if (i < 0 && prev) continue;
+        else if (i < 0) {
+            prev = 1;
+            ++ans;
+        } else {
+            prev = 0;
         }
-        return minHeap.top();
+        sum += abs(i);
     }
-    static void test() {
-        cout << 1;
-    }
-    Solution() {
-        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    }
-};
+    cout << sum << ' ' << ans;
+}
+
+static void preCompute() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
+}
 
 /**
  * @ScratchPad
  */
 
-#ifdef __TEST__
-__TEST__
-#endif
+__MAIN__
