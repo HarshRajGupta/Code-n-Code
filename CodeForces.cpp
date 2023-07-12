4b4f353e5ff1a4469aec85538a4d7a7740138c8c
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-12 20:06:04 +0530

    CodeForces

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -1,30 +1,64 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
+
 #include <bits/stdc++.h>
-using namespace __gnu_debug;
+using namespace std;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-struct zz {
-    int a, b;
-};
+#ifndef __MAIN__
+#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
+#endif
 
-class Solution {
-public:
-    /* function */
-    void test() {
-        pair<int, int> k;
-        cout << sizeof(k);
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+
+template<class T>using v = vector<T>;
+template<class T>using maxHeap = priority_queue<T>;
+template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+
+
+int SIZE = 14;
+void solve() {
+    int n; cin >> n;
+    v<int> arr(SIZE);
+    debug(arr)
+    _for(i, n) {
+        int t; cin >> t;
+        debug(t)
+        ++arr[t];
     }
-    Solution() { 
-        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+    _for(i, n) {
+        int t; cin >> t;
+        --arr[t + 1];
     }
-};
+    debug(arr)
+    int MAX = 0;
+    rep(i, 1, SIZE) {
+        arr[i] += arr[i - 1];
+        MAX = max(MAX, arr[i]);
+    }
+    cout << MAX;
+}
+
+static void preCompute() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
+}
 
 /**
  * @ScratchPad
  */
 
-#ifdef __TEST__
-__TEST__
-#endif
+__MAIN__
