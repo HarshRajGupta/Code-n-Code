0ff2a7e362fe5eea345149b4dae9a708532016b2
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-12 19:55:57 +0530

    CodeForces

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -5,33 +5,18 @@ using namespace __gnu_debug;
 #define debug(...)
 #endif
 
-class Solution {
-    vector<int> dp;
-    set<int> powersOf5 = {1, 5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, 9765625, 48828125, 244140625};
+struct zz {
+    int a, b;
+};
 
-    int ans(string s, int pos) {
-        if (pos == s.size()) return 0;
-        int num = 0;
-        int res = 1e5;
-        if (s[pos] == '0') return -1;
-        if (dp[pos] != 1e5) return dp[pos];
-        for (int i = pos; i < s.size(); ++i) {
-            num = (num * 2) + s[i] - '0';
-            if (powersOf5.find(num) != powersOf5.end()) {
-                int t = ans(s, i + 1);
-                if (t != -1) res = min(res, 1 + t);
-            }
-        }
-        return dp[pos] = (res == 1e5 ? -1 : res);
-    }
+class Solution {
 public:
-    int minimumBeautifulSubstrings(string s) {
-        dp = vector<int>(15, 1e5);
-        return ans(s, 0);
-    }
+    /* function */
     void test() {
-        /* test */
-        cout << minimumBeautifulSubstrings("1110");
+        cout << sizeof(zz);
+    }
+    Solution() { 
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
     }
 };
 
