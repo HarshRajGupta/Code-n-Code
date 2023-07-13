b6a52c2ee3967eb2a48ef2fc0022eab7c62dd35d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-13 19:37:25 +0530

    CodeForces

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -1,53 +1,73 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
-#endif
-
 #include <bits/stdc++.h>
-using namespace std;
+using namespace __gnu_debug;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#ifndef __MAIN__
-#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-
-template<class T>using v = vector<T>;
-template<class T>using maxHeap = priority_queue<T>;
-template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(),(x).end()
-
-void solve() {
-    string s1, s2; cin >> s1 >> s2;
-    int  MIN = 0, MAX = 0;
-    _for(i, s1.size()) {
-        if (s1[i] == '?' || s2[i] == '?') {
-            ++MAX;
-        } else if (s1[i] != s2[i]) {
-            ++MAX, ++MIN;
-        }
+vector<int> fact(1e5 + 7, 1), invFact(1e5 + 7);
+const int invMod(int n, const int MOD = 1e9 + 7) {
+    int ans = 1, expo = MOD - 2; n %= MOD;
+    while (expo) {
+        if (expo & 1) ans = (ans * 1ll * n) % MOD;
+        n = (n * 1ll * n) % MOD;
+        expo >>= 1;
     }
-    cout << MIN << ' ' << MAX;
+    return ans;
 }
 
-static void preCompute() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
+const int MOD = 1e9 + 7;
+void preCom() {
+    for (int i = 2; i < 1e5 + 7; ++i) {
+        fact[i] = fact[i] * fact[i - 1] % MOD;
+        invFact[i] = invMod(fact[i]);
+    }
 }
+
+class Solution {
+
+
+public:
+    int ans(string s, int k) {
+        vector<int> arr(26);
+        for (auto &i : s) {
+            ++arr[i - 'a'];
+        }
+        sort(arr.begin(), arr.end(), greater<int>());
+        set<int, greater<int>> a;
+        map<int, int> mp;
+        for (auto &i : arr) {
+            ++mp[i];
+            a.insert(i);
+        }
+        int ans = 1, ele = k;
+        for (auto &i : a) {
+            debug(ans, i, mp[i])
+            if (!i) break;
+            if (mp[i] <= ele) {
+                for (int j = 0; j < mp[i]; ++j) {
+                    ans = (ans * i) % MOD;
+                }
+                ele -= mp[i];
+            } else {
+                ans = ((((ans * fact[mp[i]]) % MOD * fact[mp[i] - ele]) % MOD * invFact[ele]) % MOD * i) % MOD;
+            }
+        }
+        return ans;
+    }
+    void test() {
+        string z = "abcdefghijjj";
+        cout << ans(z, 8);
+    }
+    Solution() {
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    }
+};
 
 /**
  * @ScratchPad
  */
 
-__MAIN__
+#ifdef __TEST__
+__TEST__
+#endif
