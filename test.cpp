143ca19c5a42bf4450d4474eb22272aa2daf09e2
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-15 19:17:34 +0530

    test

--- test.cpp
+++ test.cpp
@@ -25,7 +25,8 @@ public:
             ans += (end - len) + 1;
             debug(start, ans, len, end, count);
             if (nums[start] & 1) {
-                len = end;
+                len = end - 1;
+                --count;
             }
             ++start;
             debug(start, ans, len, end, count);
