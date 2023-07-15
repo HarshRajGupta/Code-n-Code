91d78a6562dba81a6c5e0b986af8811a01c649cc
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-15 19:32:26 +0530

    test

--- test.cpp
+++ test.cpp
@@ -10,13 +10,10 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int start = 0, len = -1, end = 0, count = 0, ans = 0;
         while (start < nums.size()) {
-            debug(start, ans, len, end, count);
             while (count < k && len < (int)nums.size()) {
                 ++len;
                 if (nums[len] & 1) ++count;
             }
-            debug(count < k, len < nums.size())
-            debug(start, len, count);
             if (count < k) {
                 break;
             }
