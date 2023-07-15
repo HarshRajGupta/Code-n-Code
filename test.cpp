3ccc6942ac6016450f3c173af8420b354a905c6a
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-15 18:09:48 +0530

    test

--- test.cpp
+++ test.cpp
@@ -6,48 +6,19 @@ using namespace __gnu_debug;
 #endif
 
 class Solution {
-    unordered_set<int> dp[15][30];
-    int n = 0;
-    unordered_set<int> subSeqSums(vector<int>& nums, int pos = 0, int selected = 0) {
-        if ((pos >= (n << 1)) || (selected >= n)) return {};
-        if ((nums.size() - pos) < (n - selected)) return {};
-        if (dp[selected][pos].size()) return dp[selected][pos];
-        if ((nums.size() - pos) == (n - selected)) {
-            int sum = 0;
-            for (int i = pos; i < (n << 1); ++i) sum += nums[i];
-            return dp[selected][pos] = {sum};
-        }
-        dp[selected][pos] = subSeqSums(nums, pos + 1, selected);
-        auto select = subSeqSums(nums, pos + 1, selected + 1);
-        for (auto &i : select) dp[selected][pos].insert(i + nums[pos]);
-        return  dp[selected][pos];
-    }
 public:
-    int minimumDifference(vector<int>& nums) {
-        n = nums.size() >> 1;
-        auto subSeqSum = subSeqSums(nums);
-        int sum = 0;
-        for (auto &i : nums) sum += i;
-        // auto lb = subSeqSum.lower_bound((sum >> 1)), ub = subSeqSum.upper_bound((sum >> 1));
-        // int MIN = abs(*lb - (sum - *lb));
-        // if (lb != subSeqSum.begin()) {
-        //     MIN = min(MIN, abs(*prev(lb) - (sum - *prev(lb))));
-        // } else if (lb != subSeqSum.end() && next(lb) != subSeqSum.end()) {
-        //     MIN = min(MIN, abs(*next(lb) - (sum - *next(lb))));
-        // }
-        // return MIN;
-        int MIN = INT_MAX;
-        for (auto &i : subSeqSum) {
-            MIN = min(MIN, abs(i - (sum - i)));
-        }
-        return MIN;
+    int minimumTotal(vector<vector<int>>& triangle, int i = 0, int j = 0) {
+        if (i == triangle.size()) return 0;
+        if (j == triangle[i].size()) return 1e8;
+        int I = minimumTotal(triangle, i + 1, j), I1 = minimumTotal(triangle, i + 1, j + 1);
+        return min(I, I1) + triangle[i][j];
     }
     void test() {
-        vector<int> a = { -14140, -98008, 80911, 64733, 18511, -39401, -53396, -79279, -69147, 25736, 27904, 77651, 12454, 58113, 55196, 77506, -77486, -32003};
-        cout << minimumDifference(a);
+        vector<vector<int>> t = {{2},{3,4},{6,5,7},{4,1,8,3}};
+        cout << minimumTotal(t);
     }
     Solution() {
-        ios::sync_with_stdio(0); cin.tie(0);
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     }
 };
 
