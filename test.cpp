b9394a6afe0a01a429d7046b22a9581a7dd8b4b1
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-16 08:24:49 +0530

    test

--- test.cpp
+++ test.cpp
@@ -28,6 +28,7 @@ public:
         for (int i = 0; i < nums.size() - 1; ++i) {
             debug(count[i], total, i);
             if ((count[i] * 2 > (i + 1)) && ((total - count[i]) * 2) > (nums.size() - i)) {
+                debug(total - count[i])
                 return i;
             }
         }
