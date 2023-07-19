5814232e16f13302b3d455e6f053bc51a5986c94
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-19 18:27:27 +0530

    test

--- test.cpp
+++ test.cpp
@@ -1,45 +1,44 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
-#endif
-
 #include <bits/stdc++.h>
-using namespace std;
+using namespace __gnu_debug;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#ifndef __MAIN__
-#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-
-template<class T>using v = vector<T>;
-template<class T>using maxHeap = priority_queue<T>;
-template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(),(x).end()
-
-void solve() {
-    int n; cin >> n;
-    cout << sqrt(n >> 1);
-}
-
-static void preCompute() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
-}
+class Solution {
+public:
+    bool canJoin(vector<string> &a) {
+        vector<int> charCount(26);
+        for (auto &i : a) {
+            charCount[i[0]]--;
+            charCount[i[1]]++;
+        }
+        bool posFound = false, negFound = false;
+        for(int i = 0; i < 26; ++i) {
+            if (charCount[i] > 0) {
+                if (posFound) return false;
+                posFound = true;
+            }
+            if (charCount[i] < 0) {
+                if (negFound) return false;
+                negFound = true;
+            }
+        }
+        return true;
+    }
+    void test() {
+        vector<string> s = {"he", "ll"};
+        cout << canJoin(s);
+    }
+    Solution() {
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    }
+};
 
 /**
  * @ScratchPad
  */
 
-__MAIN__
+#ifdef __TEST__
+__TEST__
+#endif
