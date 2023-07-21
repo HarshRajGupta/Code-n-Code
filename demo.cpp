1f71fd38a041e51f27d406dd445bcd388426f10e
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-21 09:52:55 +0530

    demo

--- demo.cpp
+++ demo.cpp
@@ -10,15 +10,16 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __SOLVE__
-#define __SOLVE__ signed main() {solve(); return 0;}
+#ifndef __MAIN__
+#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
 
-#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
 #define foreach(i, x) for (auto &i : x)
 
 template<class T>using v = vector<T>;
@@ -26,18 +27,41 @@ template<class T>using maxHeap = priority_queue<T>;
 template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 #define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
+#define all(x) (x).begin(),(x).end()
 
-void solve(void) {
+const int uB(const std::vector<int> &arr, const int t, int l = 0, int r = -1) {
+    if (r == -1) r = (arr.size() - 1);
+    if (t < arr[l]) return l;
+    if (t > arr[r]) return r + 1;
+    int ans = r + 1, mid;
+    while (l <= r) {
+        mid = (l + r) >> 1;
+        if (arr[mid] <= t) l = (mid + 1);
+        else ans = mid, r = (mid - 1);
+    }
+    return ans;
+}
+
+void solve() {
     int n; cin >> n;
-    n %= 6;
-    if (n == 0 || n == 1 || n == 3) {
-        cout << "YES";
-    } else cout << "NO";
+    v<int> arr(n);
+    foreach(i, arr) cin >> i;
+    sort(all(arr));
+    int ans = 0;
+    _for(i, n) {
+        if (uB(arr, arr[i]) > (n >> 1)) {
+            ++ans;
+        }
+    }
+    cout << ans;
+}
+
+static void preCompute() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
 }
 
 /**
  * @ScratchPad
  */
 
-__SOLVE__
+__MAIN__
