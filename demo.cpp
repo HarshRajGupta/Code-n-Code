6f07453f0c4362b8ebf471e71acae6355931adae
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-21 09:15:28 +0530

    demo

--- demo.cpp
+++ demo.cpp
@@ -10,16 +10,15 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __MAIN__
-#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
+#ifndef __SOLVE__
+#define __SOLVE__ signed main() {solve(); return 0;}
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
 
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define _for(i, n) for(int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for(int32_t i = a; i < (int32_t)n; ++i)
 #define foreach(i, x) for (auto &i : x)
 
 template<class T>using v = vector<T>;
@@ -27,22 +26,18 @@ template<class T>using maxHeap = priority_queue<T>;
 template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 #define sz(x) ((int)(x).size())
-#define all(x) (x).begin(),(x).end()
+#define all(x) (x).begin(), (x).end()
 
-void solve() {
+void solve(void) {
     int n; cin >> n;
-    int m = sqrt(n >> 1);
-    debug(m)
-    while ((m * (m + 1) >> 1) <= n) ++m;
-    cout << m - 1;
-}
-
-static void preCompute() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
+    n %= 6;
+    if (n == 0 || n == 1 || n == 3) {
+        cout << "YES";
+    } else cout << "NO";
 }
 
 /**
  * @ScratchPad
  */
 
-__MAIN__
+__SOLVE__
