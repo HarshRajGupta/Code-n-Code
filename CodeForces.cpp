03d006f521aad63d41a9f9f263f489b53906ef1d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-28 09:09:19 +0530

    CodeForces

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -1,101 +1,45 @@
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
+
 #include <bits/stdc++.h>
-using namespace __gnu_debug;
+using namespace std;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-vector<int> fact(27, 1), invFact(27 + 7);
-const int invMod(int n, const int MOD = 1e9 + 7) {
-    int ans = 1, expo = MOD - 2; n %= MOD;
-    while (expo) {
-        if (expo & 1) ans = (ans * 1ll * n) % MOD;
-        n = (n * 1ll * n) % MOD;
-        expo >>= 1;
-    }
-    return ans;
+#ifndef __MAIN__
+#define __MAIN__ signed main(){preCompute();signed t;cin>>t;while(t--)solve(),cout<<'\n';return 0;}
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+
+template<class T>using v = vector<T>;
+template<class T>using maxHeap = priority_queue<T>;
+template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+
+void solve() {
+    int n; 
+    cout << n;
 }
 
-const int MOD = 1e9 + 7;
-void preCom() {
-    invFact[0] = invFact[1] = invMod(1);
-    for (int i = 2; i <= 26; ++i) {
-        fact[i] = (fact[i - 1] * 1ll * i) % MOD;
-        invFact[i] = invMod(fact[i]);
-    }
-    debug(fact)
+static void preCompute() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.flush();
 }
-
-int nCr(int n, int r) {
-    long long p = 1, k = 1;
-    if (n - r < r)
-        r = n - r;
-
-    if (r != 0) {
-        while (r) {
-            p *= n;
-            k *= r;
-            long long m = __gcd(p, k);
-            p /= m;
-            k /= m;
-
-            n--;
-            r--;
-        }
-    }
-
-    else
-        p = 1;
-
-    return p % MOD;
-}
-class Solution {
-
-
-public:
-    int ans(string s, int k) {
-        vector<int> arr(26);
-        for (auto &i : s) {
-            ++arr[i - 'a'];
-        }
-        sort(arr.begin(), arr.end(), greater<int>());
-        set<int, greater<int>> a;
-        map<int, int> mp;
-        for (auto &i : arr) {
-            ++mp[i];
-            a.insert(i);
-        }
-        int ans = 1, ele = k;
-        for (auto &i : a) {
-            debug(ans, i, mp[i], ele)
-            if (!i) break;
-            if (mp[i] <= ele) {
-                for (int j = 0; j < mp[i]; ++j) {
-                    ans = (ans * 1ll * i) % MOD;
-                }
-                ele -= mp[i];
-            } else {
-                debug(nCr(mp[i], ele))
-                ans = (ans * 1ll * nCr(mp[i], ele)) % MOD;
-                break;
-            }
-        }
-        return ans;
-    }
-    void test() {
-        preCom();
-        string z = "abcdefghijjj";
-        cout << ans(z, 8);
-    }
-    Solution() {
-        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    }
-};
 
 /**
  * @ScratchPad
  */
 
-#ifdef __TEST__
-__TEST__
-#endif
+__MAIN__
