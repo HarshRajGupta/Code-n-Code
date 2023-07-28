09fe080b07f61b8aabb3eeca8a58bb2ab9c8ad16
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-28 10:16:33 +0530

    test

--- test.cpp
+++ test.cpp
@@ -1,52 +1,60 @@
 #include <bits/stdc++.h>
 using namespace __gnu_debug;
+using namespace std;
 
 #ifndef debug
 #define debug(...)
 #endif
 
 class Solution {
-    char fun(int n, vector<int> &inDegree, vector<int> &outDegree, vector<int> &same) {
-        int posFound = 0, negFound = 0;
-        for (int i = 0; i < 26; ++i) {
-            if (same[i] && !inDegree[i] && !outDegree[i]) {
-                if (same[i] == n) return '1';
-                return '0';
-            }
-            int count = inDegree[i] - outDegree[i];
-            if (count > 0) {
-                if (posFound || count != 1) return '0';
-                posFound = true;
-            } else if (count < 0) {
-                if (negFound || count != -1) return '0';
-                negFound = true;
-            }
-        }
-        return '1';
+  char fun(int n, vector<int> &inDegree, vector<int> &outDegree,
+           vector<int> &same) {
+    int posFound = 0, negFound = 0;
+    for (int i = 0; i < 26; ++i) {
+      if (same[i] && !inDegree[i] && !outDegree[i]) {
+        if (same[i] == n)
+          return '1';
+        return '0';
+      }
+      int count = inDegree[i] - outDegree[i];
+      if (count > 0) {
+        if (posFound || count != 1)
+          return '0';
+        posFound = true;
+      } else if (count < 0) {
+        if (negFound || count != -1)
+          return '0';
+        negFound = true;
+      }
     }
+    return '1';
+  }
+
 public:
-    string canJoin(vector<string> &a) {
-        string ans;
-        vector<int> inDegree(26), outDegree(26), same(26);
-        for (int j = 0; j < a.size(); ++j) {
-            auto i = a[j];
-            if (i[0] == i[1]) {
-                same[i[0] - 'a']++;
-            } else {
-                inDegree[i[0] - 'a']++;
-                outDegree[i[1] - 'a']++;
-            }
-            ans.push_back(fun(j + 1, inDegree, outDegree, same));
-        }
-        return ans;
+  string canJoin(vector<string> &a) {
+    string ans;
+    vector<int> inDegree(26), outDegree(26), same(26);
+    for (int j = 0; j < a.size(); ++j) {
+      auto i = a[j];
+      if (i[0] == i[1]) {
+        same[i[0] - 'a']++;
+      } else {
+        inDegree[i[0] - 'a']++;
+        outDegree[i[1] - 'a']++;
+      }
+      ans.push_back(fun(j + 1, inDegree, outDegree, same));
     }
-    void test() {
-        vector<string> s = {"ab", "ba", "bq"};
-        cout << canJoin(s);
-    }
-    Solution() {
-        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    }
+    return ans;
+  }
+  void test() {
+    vector<string> s = {"ab", "ba", "bq"};
+    cout << canJoin(s);
+  }
+  Solution() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+  }
 };
 
 /**
