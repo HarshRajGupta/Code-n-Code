3bda43e65395f6840c5874bd7bdd9639b3fd8999
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-30 09:20:48 +0530

    demo

--- demo.cpp
+++ demo.cpp
@@ -16,37 +16,35 @@ class Solution {
 		return 0;
 	}
 	map<int, map<int, int>> dp;
-	int mid(string &low, string &high, int pos, int prev) {
-		if (pos == high.size()) return 0;
+	int mid(int high, int pos, int prev) {
+		if (pos == high) return 0;
 		if (dp.find(pos) != dp.end() && dp[pos].find(prev) != dp[pos].end()) {
 			return dp[pos][prev];
 		}
 		int ans = 0;
 		if (prev > 0 && prev < 9) {
-			ans = (mid(low, high, pos + 1, prev - 1) + 0ll +  mid(low, high, pos + 1, prev + 1)) % MOD;
+			ans = (mid(high, pos + 1, prev - 1) + 0ll +
+				   mid(high, pos + 1, prev + 1)) %
+				  MOD;
 		} else if (prev == 0) {
-			ans = (0ll + mid(low, high, pos + 1, prev + 1)) % MOD;
+			ans = (0ll + mid(high, pos + 1, prev + 1)) % MOD;
 		} else if (prev == 9) {
-			ans = (0ll + mid(low, high, pos + 1, prev - 1)) % MOD;
+			ans = (0ll + mid(high, pos + 1, prev - 1)) % MOD;
 		}
-		if (pos + 1 >= low.size()) {
-			ans = (ans + 1) % MOD;
-		}
+		ans = (ans + 1) % MOD;
 		return dp[pos][prev] = ans;
 	}
-public:
-	int countSteppingNumbers(string low, string high) {
+
+   public:
+	int countNumbers(string high) {
 		int ans = 0;
-		for (int i = low[0] - '0' + 1; i < high[0] - '0'; ++i) {
-			ans = (ans + mid(low, high, 0, i)) % MOD;
+		for (int i = 1; i < high[0] - '0'; ++i) {
+			ans = (ans + mid(high.size(), 0, i)) % MOD;
 		}
-		debug(dp)
-		return ans;
+		debug(dp) return ans;
 	}
 	void test() {
-		{
-			cout << countSteppingNumbers("0", "81");
-		}
+		{ cout << countNumbers("81"); }
 	}
 	Solution() {
 		ios::sync_with_stdio(0);
