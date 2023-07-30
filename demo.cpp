35506d0bb0d39703ead1f9478be626dff8ea83cb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-30 09:24:45 +0530

    demo

--- demo.cpp
+++ demo.cpp
@@ -15,11 +15,12 @@ class Solution {
 		if (s > high) return 1;
 		return 0;
 	}
-	map<int, map<int, int>> dp;
+	map<int, map<int, map<int, int>>> dp;
 	int mid(int high, int pos, int prev) {
 		if (pos == high) return 0;
-		if (dp.find(pos) != dp.end() && dp[pos].find(prev) != dp[pos].end()) {
-			return dp[pos][prev];
+		if (dp.find(high) != dp.end() && dp[high].find(pos) != dp[high].end() &&
+			dp[high][pos].find(prev) != dp[high][pos].end()) {
+			return dp[high][pos][prev];
 		}
 		int ans = 0;
 		if (prev > 0 && prev < 9) {
@@ -32,7 +33,7 @@ class Solution {
 			ans = (0ll + mid(high, pos + 1, prev - 1)) % MOD;
 		}
 		ans = (ans + 1) % MOD;
-		return dp[pos][prev] = ans;
+		return dp[high][pos][prev] = ans;
 	}
 
    public:
@@ -41,6 +42,11 @@ class Solution {
 		for (int i = 1; i < high[0] - '0'; ++i) {
 			ans = (ans + mid(high.size(), 0, i)) % MOD;
 		}
+		for (int i = 1; i < high.size(); ++i) {
+			ans = (ans + 9ll * mid(high.size(), i, 0)) % MOD;
+		}
+		for (int i = 1; i < high.size(); ++i)
+			;
 		debug(dp) return ans;
 	}
 	void test() {
