2ea2c3a5ff37631fb81de37f530561bfa3c0f5d5
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-03 01:01:06 +0530

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -19,7 +19,7 @@ using namespace std;
     }
 #endif
 
-#define int long long
+// #define int long long
 const uint64_t MOD = 1e9 + 7;
 
 #define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
@@ -36,29 +36,41 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-int dp[100007];
+vector<vector<int>> dp(2e5 + 7, vector<int>(2, -1));
 
-int dfs(v<v<int>> &tree, int node = 0) {
+int dfs(const v<v<int>> &tree, int parent = -1, int node = 0, bool canPick = true) {
+    if (dp[node][canPick] != -1) return dp[node][canPick];
+    debug(parent, node, canPick)
     int ans = 0;
     foreach (child, tree[node]) {
-        ans += dfs(tree, child);
+        if (child != parent) ans += dfs(tree, node, child, true);
     }
-    return dp[node] = ans + 1;
+    if (!canPick) {
+        return dp[node][false] = ans;
+    }
+    int mx = ans;
+    foreach (child, tree[node]) {
+        if (child != parent) {
+            int t1 = dfs(tree, node, child, false), t2 = dfs(tree, node, child, true);
+            mx = max(mx, ans + t1 - t2);
+        }
+    }
+    return dp[node][canPick] = mx;
 }
 
-
 void solve(void) {
-    int n; cin >> n;
+    int n;
+    cin >> n;
     v<v<int>> tree(n);
     _for(i, n - 1) {
-        int t; cin >> t;
-        tree[t - 1].push_back(i + 1);
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        tree[u].push_back(v);
+        tree[v].push_back(u);
     }
     debug(tree)
-    dfs(tree);
-    _for(i, n) {
-        cout << dp[i] - 1 << " ";
-    }
+    cout << dfs(tree);
 }
 
 /**
