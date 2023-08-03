6f7bf81f5e03d0c6b6f657db32446994196c4125
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-03 07:21:59 +0530

    buildPre

--- buildPre.cpp
+++ buildPre.cpp
@@ -40,20 +40,36 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-const int power(int n, int expo, int MOD = 1e9 + 7) {
-    int ans = 1; n %= MOD;
-    while (expo) {
-        if (expo & 1) ans = (ans * 1ll * n) % MOD;
-        n = (n * 1ll * n) % MOD;
-        expo >>= 1;
-    }
-    return ans;
+vector<int32_t> allPrimes;
+vector<bool> sieve(const int32_t n = 1000007) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = false, isPrime[1] = false;
+    for (int32_t i = 2; (i * i) <= n; ++i)
+        if (isPrime[i])
+            for (int j = (i * i); j <= n; j += i)
+                isPrime[j] = false;
+    for (int32_t i = 2; i <= n; ++i)
+        if (isPrime[i])
+            allPrimes.push_back(i);
+    return isPrime;
 }
 
 void solve() {
-    int a, b, c; cin >> a >> b >> c;
-    cout  << (power(a, power(b, c)));
-
+    int n; cin >> n;
+    map<int, int> mp;
+    for (auto &i : allPrimes) {
+        if (i * i > n) break;
+        while (n % i == 0) {
+            mp[i]++;
+            n /= i;
+        }
+    }
+    if (n > 1) mp[n]++;
+    int ans = 1;
+    for (auto &i : mp) {
+        ans = ans * (i.second + 1) % MOD;
+    }
+    cout << ans;
 }
 
 static void preCompute() {
@@ -61,7 +77,7 @@ static void preCompute() {
     cin.tie(0);
     cout.tie(0);
     cout.flush();
-
+    sieve();
 }
 
 /**
