69ad84abeb3c1fd992618bef509007f9acc0737f
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-04 10:10:46 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -34,10 +34,7 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-void solve(){
-    cout << 1;
-}
-
+void solve() { cout << 1; }
 
 static void preCompute() {
 	ios::sync_with_stdio(0);
