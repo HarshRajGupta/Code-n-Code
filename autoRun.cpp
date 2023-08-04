4324c711335c20ced7ea62a269dc904eb9cdcf1d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-04 10:00:25 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -1,44 +1,52 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
 
-    int xNotY(int x, int y, int n) {
-        return ((n / x) - (n / ((x * y) / __gcd(x, y))));
-    }
-    int notXY(int x, int y, int n) {
-        return (n - (n / x) - (n / y) + (n / ((x * y) / __gcd(x, y))));
-    }
+#ifndef debug
+#define debug(...)
+#endif
 
-    int tot(int x, int y, int n, int u1, int u2) {
-        int ans = notXY(x, y, n);
-        if (u1 + u2 < ans) return 1;
-        ans += min(u1, xNotY(y, x, n));
-        ans += min(u2, xNotY(x, y, n));
-        // debug(x, y, n, u1, u2, xNotY(x, y, n), xNotY(y, x, n), notXY(x, y, n), ans)
-        if (u1 + u2 < ans) return 1;
-        if (u1 + u2 > ans) return -1;
-        return 0;
-    }
-public:
-    int minimizeSet(int d1, int d2, int u1, int u2) {
-        int l = 0, r = INT_MAX;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            int c = tot(d1, d2, mid, u1, u2);
-            if (c == 1) {
-                r = mid - 1;
-            } else if (c == -1) {
-                l = mid + 1;
-            } else {
-                r = mid;
-                break;
-            }
-        }
-        int ans = max(l, r);
-        for (int i = ans - 1; i >= 1; --i) {
-            int a = tot(d1, d2, i, u1, u2);
-            if (a >= 0)
-                ans = i;
-            else break;
-        }
-        return ans;
-    }
-};
+#ifndef __MAIN__
+#define __MAIN__                           \
+	signed main() {                        \
+		preCompute();                      \
+		signed t;                          \
+		cin >> t;                          \
+		while (t--) solve(), cout << '\n'; \
+		return 0;                          \
+	}
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+
+template <class T>
+using v = vector<T>;
+template <class T>
+using maxHeap = priority_queue<T>;
+template <class T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(){
+    cout << 1;
+}
+
+
+static void preCompute() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+/**
+ * @ScratchPad
+ */
+
+__MAIN__
