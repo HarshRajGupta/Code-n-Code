ee2f82cd1bc76ca0fb0047ce5820f5145e52f2c3
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-05 08:44:19 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -1,54 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <thread>
 
-#ifndef debug
-#define debug(...)
-#endif
-
-#ifndef __MAIN__
-#define __MAIN__                           \
-	signed main() {                        \
-		preCompute();                      \
-		signed t;                          \
-		cin >> t;                          \
-		while (t--) solve(), cout << '\n'; \
-		return 0;                          \
-	}
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-
-template <class T>
-using v = vector<T>;
-template <class T>
-using maxHeap = priority_queue<T>;
-template <class T>
-using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-	int n;
-	cin >> n;
-	n = ((++n) + (n++) - (n));
-	cout << n;
+void childFunction() {
+    std::cout << "Child thread: ID = " << std::this_thread::get_id() << std::endl;
 }
 
-static void preCompute() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+int main() {
+    std::cout << "Parent thread: ID = " << std::this_thread::get_id() << std::endl;
+
+    std::thread childThread(childFunction); // Create a new child thread
+
+    childThread.join(); // Wait for the child thread to finish
+
+    return 0;
 }
-
-/**
- * @ScratchPad
- */
-
-__MAIN__
