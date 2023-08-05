1fa4eb49a41b8c2b8b8d33db0d13f5fe8de459df
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-05 20:20:45 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -1,54 +1,54 @@
 #include <bits/stdc++.h>
+
+#include <vector>
 using namespace std;
+using namespace __gnu_debug;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#ifndef __MAIN__
-#define __MAIN__                           \
-	signed main() {                        \
-		preCompute();                      \
-		signed t;                          \
-		cin >> t;                          \
-		while (t--) solve(), cout << '\n'; \
-		return 0;                          \
+class Solution {
+	bool canDo(vector<int>& nums1, vector<int>& nums2, int x, int t) {
+		vector<pair<int, int>> arr;
+		for (int i = 0; i < nums1.size(); ++i) {
+			arr.emplace_back(nums1[i] + t * nums2[i], i);
+		}
+		sort(arr.begin(), arr.end(), greater<pair<int, int>>());
+		debug(arr) return false;
 	}
-#endif
 
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-
-template <class T>
-using v = vector<T>;
-template <class T>
-using maxHeap = priority_queue<T>;
-template <class T>
-using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-	int a = 1, b = 10;
-	function<int(int)> f = [a, &b](int n) { return b = (a + b + n); };
-	f(5);
-	cout << b << ' ' << a;
-}
-
-static void preCompute() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-}
+   public:
+	int minimumTime(vector<int>& nums1, vector<int>& nums2, int x) {
+		int l = 0, r = 1e9, ans = -1;
+		while (l < r) {
+			int m = (l + r) / 2;
+			if (canDo(nums1, nums2, x, m)) {
+				r = m;
+				ans = m;
+			} else {
+				l = m + 1;
+			}
+		}
+		return ans;
+	}
+	void test() {
+		{
+			vector<int> a = {1, 2, 3}, b = {1, 2, 3};
+			cout << minimumTime(a, b, 4);
+		}
+	}
+	Solution() {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+	}
+};
 
 /**
  * @ScratchPad
  */
 
-__MAIN__
+#ifdef __TEST__
+__TEST__
+#endif
