fc7c6a3d6b09d52a6082a570e95092525b89525d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-05 10:02:02 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -1,9 +1,3 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize("O3", "fast-math", "unroll-loops", "no-stack-protector", \
-                         "omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
-#endif
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,16 +5,20 @@ using namespace std;
 #define debug(...)
 #endif
 
-#ifndef __SOLVE__
-#define __SOLVE__   \
-    signed main() { \
-        solve();    \
-        return 0;   \
+#ifndef __MAIN__
+#define __MAIN__                           \
+    signed main() {                        \
+        preCompute();                      \
+        signed t;                          \
+        cin >> t;                          \
+        while (t--) solve(), cout << '\n'; \
+        return 0;                          \
     }
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
+const char ln = '\n';
 
 #define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
 #define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
@@ -34,16 +32,21 @@ template <class T>
 using minHeap = priority_queue<T, vector<T>, greater<T>>;
 
 #define sz(x) ((int)(x).size())
-
 #define all(x) (x).begin(), (x).end()
 
-void solve(void) {
+void solve() {
     int n;
-    cout << n - 4;
+    cout << n;
+}
+
+static void preCompute() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 }
 
 /**
  * @ScratchPad
  */
 
-__SOLVE__
+__MAIN__
