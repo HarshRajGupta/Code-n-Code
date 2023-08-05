f41a57c06948285d3a854a2ad314a6e6cabab559
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-05 08:56:32 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -1,17 +1,49 @@
-#include <iostream>
-#include <thread>
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3", "fast-math", "unroll-loops", "no-stack-protector", \
+                         "omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
 
-void childFunction() {
-	std::cout << "Child thread: ID = " << std::this_thread::get_id()
-			  << std::endl;
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __SOLVE__
+#define __SOLVE__   \
+    signed main() { \
+        solve();    \
+        return 0;   \
+    }
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+
+template <class T>
+using v = vector<T>;
+template <class T>
+using maxHeap = priority_queue<T>;
+template <class T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    int n; 
+    cin >> n;
+    cout << n;
 }
 
-int main() {
-	std::thread childThread(childFunction);	 // Create a new child thread
-    std::cout << "Parent thread: ID = " << std::this_thread::get_id()
-              << std::endl;
+/**
+ * @ScratchPad
+ */
 
-	childThread.join();	 // Wait for the child thread to finish
-
-	return 0;
-}
+__SOLVE__
