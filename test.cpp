d2cd5573f3a64fa3d3ebd0cc4ac2972c19f06157
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-05 20:29:45 +0530

    test

--- test.cpp
+++ test.cpp
@@ -1,46 +1,52 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize("O3","fast-math","unroll-loops","no-stack-protector","omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
-#endif
-
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+using namespace __gnu_debug;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-
-#ifndef __SOLVE__
-#define __SOLVE__ signed main() {solve(); return 0;}
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-#define bw(i, n) for (int32_t i = n; i >= 0; --i)
-
-template<class T>using v = vector<T>;
-template<class T>using maxHeap = priority_queue<T>;
-template<class T>using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-	string s; cin >> s;
-	int ans = 0;
-	for (auto& i : s) {
-		ans += (i - '0');
+class Solution {
+	int cost(vector<int>& adj, int n) {
+		int ans = 0;
+		for (int i = 0; i < adj.size() - 1; ++i) {
+			ans = max(ans, ((adj[i + 1] - adj[i]) >> 1) +
+							   ((adj[i + 1] - adj[i]) & 1));
+		}
+		ans = max(ans, ((n - 1 - adj.back() + adj[0]) >> 1) + ((n-1 - adj.back() + adj[0])&1));
+		return ans;
 	}
-	cout << ans << endl;
-}
+
+   public:
+	int minimumSeconds(vector<int>& nums) {
+		map<int, vector<int>> mp;
+		for (int i = 0; i < nums.size(); ++i) {
+			mp[nums[i]].push_back(i);
+		}
+		int ans = INT_MAX;
+		for (auto& [k, v] : mp) {
+			ans = min(ans, cost(v, nums.size()));
+		}
+		return ans;
+	}
+	void test() {
+		{
+			vector<int> nums = {5,5,5,5};
+			cout << minimumSeconds(nums);
+		}
+	}
+	Solution() {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+	}
+};
 
 /**
  * @ScratchPad
  */
 
-__SOLVE__
+#ifdef __TEST__
+__TEST__
+#endif
