8df72ad168100bbd69d3845a15a31444e0faf53b
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-05 08:48:36 +0530

    autoRun

--- autoRun.cpp
+++ autoRun.cpp
@@ -2,15 +2,14 @@
 #include <thread>
 
 void childFunction() {
-    std::cout << "Child thread: ID = " << std::this_thread::get_id() << std::endl;
+	std::cout << "Child thread: ID = " << std::this_thread::get_id()
+			  << std::endl;
 }
 
 int main() {
-    std::cout << "Parent thread: ID = " << std::this_thread::get_id() << std::endl;
+	std::thread childThread(childFunction);	 // Create a new child thread
 
-    std::thread childThread(childFunction); // Create a new child thread
+	childThread.join();	 // Wait for the child thread to finish
 
-    childThread.join(); // Wait for the child thread to finish
-
-    return 0;
+	return 0;
 }
