6132ff258557441de1c9aee6af849fe407a6518d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-10 23:04:18 +0530

    oop

--- oop.cpp
+++ oop.cpp
@@ -1,32 +1,51 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+using namespace __gnu_debug;
 
-class parent {
-	int a = 0;
+#ifndef debug
+#define debug(...)
+#endif
 
+class Solution {
    public:
-	parent(int t = 0) : a(t) { cout << "parent: " << a << endl; }
-	int get() { return a; }
-	int set(int x) { return a = x; }
-	void print() { cout << "Parent" << endl; }
-	virtual int t() {
-		cout << "parent: t" << a << endl;
-		return 0;
-	};
-};
-
-class derived : public parent {
-   public:
-	derived(int t = 0) : parent(t) { cout << "derived: " << t << endl; }
-	void print() { cout << "Derived" << endl; }
-	int t() {
-		cout << "t" << endl;
-		return 0;
+	int fun(int n, vector<int> people, vector<char> status) {
+		int MAX = 0, curr = 0;
+		map<int, int> mp;
+		for(int i = 0; i < n; ++i) {
+			if (status[i] == '+') {
+				if (mp[people[i]] == 0) {
+					mp[people[i]] = 1;
+					curr++;
+				} else {
+					return -1;
+				}
+			} else if (mp[people[i]] != 1) {
+				return -1;
+			} else {
+				mp[people[i]] = 0;
+				curr--;
+			}
+			MAX = max(MAX, curr);
+		}
+		return MAX;
+	}
+	void test() {
+		vector<int> a= {1, 2, 1};
+		vector<char> b = {'+', '-', '+'};
+		cout << fun(3, a, b);
+	}
+	Solution() {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
 	}
 };
 
-signed main(void) {
-	parent z(10);
-	z.t();
-	return 0;
-}
+/**
+ * @ScratchPad
+ */
+
+#ifdef __TEST__
+__TEST__
+#endif
