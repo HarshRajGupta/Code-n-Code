fd2cf7e2d131002e88653e83517a7ce7b05d30f9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-10 23:24:18 +0530

    oop

--- oop.cpp
+++ oop.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+#include <string>
 #include <vector>
 using namespace std;
 using namespace __gnu_debug;
@@ -9,10 +11,27 @@ using namespace __gnu_debug;
 
 class Solution {
    public:
+	bool mod(string a, string b, int i, int j, vector<vector<int>> &dp) {
+		if (j == b.size()) return true;
+		if (i == a.size()) return false;
+		if (dp[i][j] != -1) return dp[i][j];
+		if (a[i] == b[j] || a[i] + 1 == b[j])
+			return dp[i][j] = mod(a, b, i + 1, j + 1, dp);
+		return dp[i][j] = mod(a, b, i + 1, j, dp);
+	}
+	vector<bool> fff(vector<string> newPass, vector<string> oldPass) {
+		vector<bool> ans(newPass.size());
+		for (int i = 0; i < newPass.size(); ++i) {
+			vector<vector<int>> dp(newPass[i].size(),
+								   vector<int>(oldPass[i].size(), -1));
+			ans[i] = mod(newPass[i], oldPass[i], 0, 0, dp);
+		}
+		return ans;
+	}
 	int fun(int n, vector<int> people, vector<char> status) {
 		int MAX = 0, curr = 0;
 		map<int, int> mp;
-		for(int i = 0; i < n; ++i) {
+		for (int i = 0; i < n; ++i) {
 			if (status[i] == '+') {
 				if (mp[people[i]] == 0) {
 					mp[people[i]] = 1;
@@ -20,9 +39,9 @@ class Solution {
 				} else {
 					return -1;
 				}
-			} else if (mp[people[i]] != 1) {
+			} else if (mp[people[i]] != 1)
 				return -1;
-			} else {
+			else {
 				mp[people[i]] = 0;
 				curr--;
 			}
@@ -31,9 +50,15 @@ class Solution {
 		return MAX;
 	}
 	void test() {
-		vector<int> a= {1, 2, 1};
-		vector<char> b = {'+', '+', '-'};
-		cout << fun(3, a, b);
+		vector<string> a = {
+			"aaccbbee",
+			"aab"
+		}, b = {
+			"bdbf",
+			"aee"
+		};
+		auto x = fff(a, 	b);
+		debug(x)
 	}
 	Solution() {
 		ios::sync_with_stdio(0);
