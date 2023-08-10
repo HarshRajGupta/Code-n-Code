a2966263e7d9fea689b3d876af2c062b6d5fd47d
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-10 22:28:29 +0530

    oop

---
+++ oop.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class parent {
+	int a = 0;
+
+   public:
+	int get() { return a; }
+	int set(int x) { return a = x; }
+	void print() { cout << "Parent" << endl; }
+};
+
+class derived : public parent {
+	int b = 0;
+
+   public:
+	int get() { return b; }
+	int set(int x) { return b = x; }
+	void print() { cout << "Derived" << endl; }
+};
+
+signed main(void) {
+	derived d;
+	d.set(10);
+	cout << d.get();
+	return 0;
+}
