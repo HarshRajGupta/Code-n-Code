9a794fd4d157e8b3df497556b83aa96bf37a4dda
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-10 22:36:44 +0530

    oop

--- oop.cpp
+++ oop.cpp
@@ -5,21 +5,22 @@ class parent {
 	int a = 0;
 
    public:
+	parent(int t = 0) : a(t) { cout << "parent: " << a << endl; }
 	int get() { return a; }
 	int set(int x) { return a = x; }
 	void print() { cout << "Parent" << endl; }
 };
 
 class derived : public parent {
-	int b = 0;
-
    public:
+   	derived(int t = 0) : parent(t) { cout << "derived: " << t << endl; }
 	void print() { cout << "Derived" << endl; }
 };
 
 signed main(void) {
-	derived d;
-	d.set(1);
-	cout << d.get();
+	derived* d = new derived(5);
+	cout << d->get();
+	d->set(1);
+	cout << d->get();
 	return 0;
 }
