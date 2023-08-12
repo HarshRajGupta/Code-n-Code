e72b82a2466aef2067ed45591ab21d34737cd8c9
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-12 12:03:33 +0530

    oop

--- oop.cpp
+++ oop.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
+
+#include <climits>
 #include <string>
+#include <vector>
 using namespace std;
 using namespace __gnu_debug;
 
@@ -7,32 +10,34 @@ using namespace __gnu_debug;
 #define debug(...)
 #endif
 
-class Solution {
-	vector<char> word = {'W', 'O', 'R', 'D'};
+vector<char> word = {'W', 'O', 'R', 'D'};
 
-   public:
-	int ans(string s, int pos = 0, int prev = 0) {
-		if (pos == s.size()) {
-			if (prev == 3) return 0;
-			return -1;
-		}
-		if (prev == 3) {
-			int res = ans(s, pos + 1, prev);
-			if (res == -1) return (s[pos] == word[prev]);
-			return res + (s[pos] == word[prev]);
-		}
-		if (s[pos] == word[prev]) {
-			int res = ans(s, pos + 1, prev);
-			if (res == -1) return -1;
-			return res + 1;
-		}
-		if (s[pos] == word[prev + 1]) {
-			int res = ans(s, pos + 1, prev + 1);
-			if (res == -1) return -1;
-			return res + 1;
-		}
-		return ans(s, pos + 1, prev);
+vector<vector<int>> dp(200, vector<int>(7, -2));
+int ans(string s, int pos = 0, int prev = 0) {
+	if (pos == s.size()) {
+		if (prev == 3) return 0;
+		return -1;
 	}
+	if (dp[pos][prev] != -2) return dp[pos][prev];
+	if (prev == 3) {
+		int res = ans(s, pos + 1, prev);
+		if (res == -1) return dp[pos][prev] = (s[pos] == word[prev]);
+		return dp[pos][prev] = res + (s[pos] == word[prev]);
+	}
+	if (s[pos] == word[prev]) {
+		int res = ans(s, pos + 1, prev);
+		if (res == -1) return dp[pos][prev] = -1;
+		return dp[pos][prev] = res + 1;
+	}
+	if (s[pos] == word[prev + 1]) {
+		int res = ans(s, pos + 1, prev + 1);
+		if (res == -1) return dp[pos][prev] = -1;
+		return dp[pos][prev] = res + 1;
+	}
+	return dp[pos][prev] = ans(s, pos + 1, prev);
+}
+class Solution {
+   public:
 	void test() {
 		string s = "777WWWVBDJBVAAVOOHBFRHIRDORDDDRRRDDDDEEE";
 		cout << ans(s);
