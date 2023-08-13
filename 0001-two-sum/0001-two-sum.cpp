52d0636889f229f7c94c55c9939f5d7d7ed080f6
Harsh Raj Gupta <1amharsh.rg@gmail.com>
2023-08-13 22:42:48 +0530

    Time: 16 ms (59.97%), Space: 12.1 MB (7.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef debug
+#define debug(...)
+#endif
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> nums, int target) {
         map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++) {
-            if(mp[target-nums[i]] != 0) {
-                return {mp[target-nums[i]] - 1, i};
-            }
-            mp[nums[i]] = i+1;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mp[target - nums[i]]) return {i, mp[target - nums[i]] - 1};
+            mp[nums[i]] = i + 1;
         }
-     return {};   
+        return {};
+    }
+    void test() {
+        debug(twoSum({1, 2, 3, 4}, 3));
     }
 };
+
+#ifdef __TEST__
+__TEST__
+#endif
