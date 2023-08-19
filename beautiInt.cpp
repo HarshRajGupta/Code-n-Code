2141b7827d3b7a956e1154e710e71fbc8cb76cbb
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-19 21:15:57 +0530

    beautiInt

---
+++ beautiInt.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_debug;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+class Solution {
+	int mod[10][10];
+	string l, r;
+	int k;
+	int cnt(int pos, int even, int odd, int rem, bool isLow) {
+		if (pos == r.size()) {
+			if (rem == 0 && even == odd) return 1;
+			return 0;
+		}
+		int ans = 0;
+		if (isLow) {
+			for (int i = l[pos] - '0'; i <= r[pos] - '0'; i++) {
+				if (i & 1)
+					ans += cnt(pos + 1, even, odd + 1,
+							   (rem + (mod[i][r.size() - pos])) % k, isLow);
+				else
+					ans += cnt(pos + 1, even + 1, odd,
+							   (rem + (mod[i][r.size() - pos])) % k, isLow);
+			}
+		} else {
+			for (int i = l[pos] - '0'; i < r[pos] - '0'; i++) {
+				if (i & 1)
+					ans += cnt(pos + 1, even, odd + 1,
+							   (rem + (mod[i][r.size() - pos])) % k, true);
+				else
+					ans += cnt(pos + 1, even + 1, odd,
+							   (rem + (mod[i][r.size() - pos])) % k, true);
+			}
+			if ((r[pos] - '0') & 1)
+				ans +=
+					cnt(pos + 1, even, odd + 1,
+						(rem + (mod[r[pos] - '0'][r.size() - pos])) % k, false);
+			else
+				ans +=
+					cnt(pos + 1, even + 1, odd,
+						(rem + (mod[r[pos] - '0'][r.size() - pos])) % k, false);
+		}
+
+		return ans;
+	}
+
+   public:
+	int numberOfBeautifulIntegers(int low, int high, int K) {
+		k = K;
+		l = "", r = "";
+		for (int i = 0; i < 9; ++i) {
+			l.push_back('0' + low % 10);
+			r.push_back('0' + high % 10);
+			low /= 10;
+			high /= 10;
+		}
+		reverse(l.begin(), l.end());
+		reverse(r.begin(), r.end());
+		debug(l, r);
+		for (int i = 0; i < 10; ++i) {
+			for (int j = 0; j < 10; j++) {
+				mod[i][j] = (i *1ll* (int)pow(10, j)) % k;
+			}
+		}
+		return cnt(0, 0, 0, k, false);
+	}
+	void test() {
+		cout << numberOfBeautifulIntegers(10, 20, 3);
+	}
+	Solution() {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+	}
+};
+
+/**
+ * @ScratchPad
+ */
+
+#ifdef __TEST__
+__TEST__
+#endif
