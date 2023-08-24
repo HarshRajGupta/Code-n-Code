c3da68bee1ff5cf0d2ab10b18a5f089b20b219c3
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-24 21:03:52 +0530

    codeForces@894

--- codeForces@894.cpp
+++ codeForces@894.cpp
@@ -39,7 +39,7 @@ unsigned long long fun(int n) { return n * 1ull * (n + 1) / 2; }
 void solve() {
 	int n;
 	cin >> n;
-	int l = 1, r = 1e9 + 7, ans = 26489564210;
+	int l = 1, r = 1e9 + 7, ans = 2648956421;
 	while (l <= r) {
 		int mid = (l + r) / 2;
 		if (fun(mid) >= n) {
