8cf05f9806b0a4ecfe3b2923be27fd094b7813c4
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-24 20:41:12 +0530

    codeForces@894

--- codeForces@894.cpp
+++ codeForces@894.cpp
@@ -43,27 +43,10 @@ const int ub(const vector<int> &arr, int t, int l = 0, int r = -1) {
 	int ans = r + 1, mid;
 	while (l <= r) {
 		mid = (l + r) >> 1;
-		if (arr[mid] <= t)
+		if (arr[mid] < t)
 			l = (mid + 1);
 		else
 			ans = mid, r = (mid - 1);
-	}
-	return ans;
-}
-
-const int lb(const vector<int> &arr, int t, int l = 0, int r = -1) {
-	if (r == -1) r = (arr.size() - 1);
-	if (arr[l] >= t)
-		return l - 1;
-	else if (arr[r] < t)
-		return r;
-	int ans = l - 1, mid;
-	while (l <= r) {
-		mid = (l + r) >> 1;
-		if (arr[mid] >= t)
-			r = (mid - 1);
-		else
-			ans = mid, l = (mid + 1);
 	}
 	return ans;
 }
