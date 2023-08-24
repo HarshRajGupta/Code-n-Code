5b9f6facddb6ea7270051c982c9faac8f9d811d6
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-24 21:03:12 +0530

    codeForces@894

--- codeForces@894.cpp
+++ codeForces@894.cpp
@@ -34,14 +34,12 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-int fun(int n) { return n * 1ull * (n + 1) / 2; }
+unsigned long long fun(int n) { return n * 1ull * (n + 1) / 2; }
 
 void solve() {
 	int n;
 	cin >> n;
 	int l = 1, r = 1e9 + 7, ans = 2648956421;
-	unsigned long long z = n*1ull*(n+1)/2;
-	debug(z)
 	while (l <= r) {
 		int mid = (l + r) / 2;
 		if (fun(mid) >= n) {
