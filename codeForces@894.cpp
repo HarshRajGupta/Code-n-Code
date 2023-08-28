b3d481f57f78443b5480684abadbfd27dd57e986
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-08-28 15:11:08 +0530

    codeForces@894

--- codeForces@894.cpp
+++ codeForces@894.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
+using namespace std;
 
 int phi(int n) {
-    int result = n; // Initialize result with n
-
-    for (int p = 2; p * p <= n; ++p) {
-        if (n % p == 0) {
-            while (n % p == 0) {
-                n /= p;
-            }
-            result -= result / p;
-        }
-    }
-
-    if (n > 1) {
-        result -= result / n;
-    }
-
-    return result;
+	int result = n;
+	for (int p = 2; p * p <= n; ++p) {
+		if (n % p == 0) {
+			while (n % p == 0) {
+				n /= p;
+			}
+			result -= result / p;
+		}
+	}
+	if (n > 1) {
+		result -= result / n;
+	}
+	return result;
 }
 
 int main() {
-    int x;
-    std::cout << "Enter a number (x): ";
-    std::cin >> x;
-
-    int result = phi(x);
-    std::cout << "Count of numbers less than " << x << " with no common factors: " << result << std::endl;
-
-    return 0;
+	int n, m;
+	cin >> n;
+	while(n--) {
+		cin >> m;
+		cout << phi(m) << ' ';
+	}
+	return 0;
 }
