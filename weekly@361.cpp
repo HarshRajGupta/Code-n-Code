55b41434c4bda480a1bbcbbd6d0ae6f2db62de83
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-03 08:14:45 +0530

    weekly@361

---
+++ weekly@361.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __gnu_debug;
+
+#ifndef debug
+#define debug(...)
+#endif
+
+class Solution {
+	int check(string &num, string &t, int i, int j) {
+		if (j < 0 || i < 0) return 0;
+		if (num[i] == t[j]) return check(num, t, i - 1, j - 1);
+		return check(num, t, i - 1, j) + 1;
+	}
+
+   public:
+	int minimumOperations(string &num) {
+		int ans = num.size();
+		string t = "00";
+		ans = min(ans, check(num, t, num.size() - 1, 0));
+		t = "50";
+		ans = min(ans, check(num, t, num.size() - 1, 0));
+		t = "25";
+		ans = min(ans, check(num, t, num.size() - 1, 0));
+		t = "75";
+		ans = min(ans, check(num, t, num.size() - 1, 0));
+		return ans;
+	}
+	void test() {
+		{
+			string t = "2245047";
+			debug(minimumOperations(t));
+		}
+	}
+	Solution() {
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+	}
+};
+
+/**
+ * @ScratchPad
+ */
+
+#ifdef __TEST__
+__TEST__
+#endif
