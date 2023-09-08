98482f21ac40454eb9e74a89b571b948dd824535
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-08 14:35:04 +0530

    buildPre

--- buildPre.cpp
+++ buildPre.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 map<int, int> mp;
+map<int, map<int, int>> dp;
 
 int countSubset(vector<int> &arr, int pos, int target) {
 	if (target == 0) return 1;
 	if (pos >= arr.size() || arr[pos] > target) return 0;
+	if (dp.find(pos) != dp.end() && dp[pos].find(target) != dp[pos].end())
+		return dp[pos][target];
 	int ans = 0;
 	for (int i = 0; i <= mp[arr[pos]]; i++) {
 		ans += countSubset(arr, pos + 1, target - (arr[pos] * i));
 	}
-	return ans;
+	return dp[pos][target] = ans;
 }
 
 signed main(void) {
@@ -23,6 +26,6 @@ signed main(void) {
 		a.insert(t);
 	}
 	vector<int> arr(a.begin(), a.end());
-    cout << countSubset(arr, 0, x) << endl;
+	cout << countSubset(arr, 0, x) << endl;
 	return 0;
 }
