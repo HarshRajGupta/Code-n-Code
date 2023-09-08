a791cfe7bd401d307bc6fef5bf93f5d0f80fce65
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-08 10:32:28 +0530

    buildPre

--- buildPre.cpp
+++ buildPre.cpp
@@ -1,52 +1,10 @@
-#ifdef ONLINE_JUDGE
-#pragma GCC optimize("O3", "fast-math", "unroll-loops", "no-stack-protector", \
-                         "omit-frame-pointer")
-#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
-#endif
+#include <stdio.h>
 
-#include <bits/stdc++.h>
-using namespace std;
-
-#ifndef debug
-#define debug(...)
-#endif
-
-#ifndef __SOLVE__
-#define __SOLVE__   \
-    signed main() { \
-        solve();    \
-        return 0;   \
-    }
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-
-template <class T>
-using v = vector<T>;
-template <class T>
-using maxHeap = priority_queue<T>;
-template <class T>
-using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-void solve(void) {
+int main(void) {
     char *arr = "Deltax";
     char c;
     for (int i = 0; i < 3; ++i) {
 		c = *arr++;
 	}
-	cout << c;
+	printf("%c", c);
 }
-
-/**
- * @ScratchPad
- */
-
-__SOLVE__
