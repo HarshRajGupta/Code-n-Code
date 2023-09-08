a7a29a5159416a186b7cd697b49fd2e489137c0f
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-08 10:16:14 +0530

    buildPre

--- buildPre.cpp
+++ buildPre.cpp
@@ -1,57 +1,49 @@
-#include <bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3", "fast-math", "unroll-loops", "no-stack-protector", \
+                         "omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
 
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int32_t> allPrimes;
-vector<bool> sieve(const int32_t n = 100007) {
-	vector<bool> isPrime(n + 1, true);
-	isPrime[0] = false, isPrime[1] = false;
-	for (int32_t i = 2; (i * i) <= n; ++i)
-		if (isPrime[i])
-			for (int j = (i * i); j <= n; j += i) isPrime[j] = false;
-	for (int32_t i = 2; i <= n; ++i)
-		if (isPrime[i]) allPrimes.push_back(i);
-	return isPrime;
+#ifndef debug
+#define debug(...)
+#endif
+
+#ifndef __SOLVE__
+#define __SOLVE__   \
+    signed main() { \
+        solve();    \
+        return 0;   \
+    }
+#endif
+
+#define int long long
+const uint64_t MOD = 1e9 + 7;
+
+#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
+#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
+#define foreach(i, x) for (auto &i : x)
+
+template <class T>
+using v = vector<T>;
+template <class T>
+using maxHeap = priority_queue<T>;
+template <class T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve(void) {
+    int x = 97;
+    int y = sizeof(x++);
+    cout << x;
 }
 
-vector<int> primeFactorization(int n) {
-	vector<int> factors;
-	for (auto &i : allPrimes) {
-		if (i * i > n) break;
-		while (n % i == 0) {
-			n /= i;
-			factors.push_back(i);
-		}
-	}
-	return factors;
-}
+/**
+ * @ScratchPad
+ */
 
-int countNumbersWithNoCommonFactors(int x) {
-	vector<int> factors = primeFactorization(x);
-	debug(x, factors) int result = x;
-	for (int factor : factors) result = result * (factor - 1) / factor;
-	return result;
-}
-
-vector<int> fun(vector<int> &a) {
-	vector<bool> isPrime = sieve();
-	vector<int> ans = a;
-	for (int i = 0; i < a.size(); ++i) {
-		if (isPrime[a[i]])
-			ans[i] = a[i] - 1;
-		else
-			ans[i] = countNumbersWithNoCommonFactors(a[i]);
-	}
-	return ans;
-}
-
-signed main(void) {
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	for (auto &i : a) cin >> i;
-	auto ans = fun(a);
-	for (auto &i : ans) cout << i << " ";
-	return 0;
-}
+__SOLVE__
