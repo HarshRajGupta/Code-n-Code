286ca0a5cd5e5637e2eedcc38856c195c0eb6202
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-08 14:35:34 +0530

    buildPre

--- buildPre.cpp
+++ buildPre.cpp
@@ -9,9 +9,8 @@ int countSubset(vector<int> &arr, int pos, int target) {
 	if (dp.find(pos) != dp.end() && dp[pos].find(target) != dp[pos].end())
 		return dp[pos][target];
 	int ans = 0;
-	for (int i = 0; i <= mp[arr[pos]]; i++) {
+	for (int i = 0; i <= mp[arr[pos]]; i++)
 		ans += countSubset(arr, pos + 1, target - (arr[pos] * i));
-	}
 	return dp[pos][target] = ans;
 }
 
@@ -19,8 +18,7 @@ signed main(void) {
 	int n, x;
 	cin >> n >> x;
 	set<int> a;
-	for (int i = 0; i < n; i++) {
-		int t;
+	for (int i = 0, t; i < n; i++) {
 		cin >> t;
 		mp[t]++;
 		a.insert(t);
