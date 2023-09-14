995aac2f340db4631fced8cb9e796888ada37bc5
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-14 10:33:29 +0530

    buildPre

--- buildPre.cpp
+++ buildPre.cpp
@@ -35,7 +35,7 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
-    cout << "\a3";
+    cout << "\t3";
 }
 
 static void preCompute() {
