3f94844831f2d86b95f14d445b549218938d77fe
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-24 15:16:07 +0530

    starters@101

--- starters@101.cpp
+++ starters@101.cpp
@@ -1,63 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef debug
-#define debug(...)
-#endif
-
-#ifndef __MAIN__
-#define __MAIN__                           \
-	signed main() {                        \
-		preCompute();                      \
-		signed t;                          \
-		cin >> t;                          \
-		while (t--) solve(), cout << '\n'; \
-		return 0;                          \
+int func(vector<int> &arr) {
+	int ans = 1, count = 1;
+	for (int i = 1; i < arr.size(); i++) {
+		if (arr[i] == arr[i - 1] + 1)
+			++count;
+		else
+			count = 1;
+		ans = max(ans, count);
 	}
-#endif
-
-#define int long long
-const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
-
-#define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
-#define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
-#define foreach(i, x) for (auto &i : x)
-
-template <class T>
-using v = vector<T>;
-template <class T>
-using maxHeap = priority_queue<T>;
-template <class T>
-using minHeap = priority_queue<T, vector<T>, greater<T>>;
-
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-
-int ans(int a, int b) {
-	int z = a ^ b, pos = 0;
-	while (z) {
-		z >>= 1;
-		pos++;
-	}
-	if(!pos) return 0;
-	return a ^ (1 << (pos - 1));
+	return ans;
 }
 
-void solve() {
-	int a, b;
-	cin >> a >> b;
-	cout << ans(a, b);
+signed main(void) {
+	int n; cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	cout << func(arr) << endl;
+	return 0;
 }
-
-static void preCompute() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-}
-
-/**
- * @ScratchPad
- */
-
-__MAIN__
