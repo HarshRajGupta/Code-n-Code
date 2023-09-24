ba944617c3a25427887af8a1307caec77f92a3de
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-09-24 15:45:17 +0530

    starters@100

--- starters@100.cpp
+++ starters@100.cpp
@@ -1,26 +1,26 @@
-#include <bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3", "fast-math", "unroll-loops", "no-stack-protector", \
+                         "omit-frame-pointer")
+#pragma GCC target("sse", "sse2", "sse3", "sse4", "abm", "mmx", "avx", "avx2")
+#endif
 
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 #ifndef debug
 #define debug(...)
 #endif
 
-#ifndef __MAIN__
-#define __MAIN__                           \
-	signed main() {                        \
-		preCompute();                      \
-		signed t;                          \
-		cin >> t;                          \
-		while (t--) solve(), cout << '\n'; \
-		return 0;                          \
-	}
+#ifndef __SOLVE__
+#define __SOLVE__   \
+    signed main() { \
+        solve();    \
+        return 0;   \
+    }
 #endif
 
 #define int long long
 const uint64_t MOD = 1e9 + 7;
-const char ln = '\n';
 
 #define _for(i, n) for (int32_t i = 0; i < (int32_t)n; ++i)
 #define rep(i, a, n) for (int32_t i = a; i < (int32_t)n; ++i)
@@ -36,38 +36,32 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 
-vector<bool> isPrime(100008, true);
-vector<bool> sieve(const int32_t n = 100007) {
-	isPrime[0] = false, isPrime[1] = false;
-	for (int32_t i = 2; (i * i) <= n; ++i)
-		if (isPrime[i])
-			for (int j = (i * i); j <= n; j += i) isPrime[j] = false;
-	return isPrime;
-}
-
-void solve() {
-	int n;
-	cin >> n;
-	if (isPrime[n - 2]) {
-		_for(i, n - 2) { cout << i + 3 << ' '; }
-		cout << "1 2";
-	} else if (isPrime[n - 3]) {
-		_for(i, n - 3) { cout << i + 4 << ' '; }
-		cout << "1 2 3";
-	} else{
-		cout << -1;
+int maxConcurrentJobs(vector<int> &jobStartTimes, vector<int> &jobEndTimes) {
+	int ans = 0;
+	map<int, int> mp;
+	for (int i = 0; i < jobStartTimes.size(); i++) {
+		mp[jobStartTimes[i]]++;
+		mp[jobEndTimes[i] + 1]--;
 	}
+	int curr = 0;
+	for (auto &i : mp) {
+		curr += i.second;
+		ans = max(ans, curr);
+	}
+	return ans;
 }
 
-static void preCompute() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	sieve();
+void solve(void) {
+    int n; cin >> n;
+    v<int> a(n), b(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+    	cout << maxConcurrentJobs(a, b) << endl;
+
 }
 
 /**
  * @ScratchPad
  */
 
-__MAIN__
+__SOLVE__
