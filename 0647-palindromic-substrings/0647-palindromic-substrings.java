b67d991e9dd2f264b1145dcae528cb7b2faf743a
Harsh Raj Gupta <85221003+HarshRajGupta@users.noreply.github.com>
2023-09-29 15:36:32 +0530

    Time: 3 ms (72.28%), Space: 40.8 MB (45.63%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,23 +1,20 @@
-class Solution 
-{ 
+class Solution {
     public int countSubstrings(String s) {
-        int ans = s.length();
-        boolean [][] arr = new boolean[s.length()][s.length()];
-        for(int i = 0; i < s.length() - 1; ++i) {
-            arr[i][i] = true;
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                arr[i][i + 1] = true;
+        int ans = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            int l = i, r = i + 1;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                --l;
+                ++r;
+                ++ans;
+            }
+            l = i; r = i;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                --l;
+                ++r;
                 ++ans;
             }
         }
-        for(int i = 2; i < s.length(); ++i) {
-            for(int j = 0; j < s.length() - i; ++j) {
-                if ((s.charAt(j) == s.charAt(j + i)) && (arr[j + 1][j + i - 1])) {
-                    ++ans;
-                    arr[j][j + i] = true;
-                }
-            }
-        }
-		return ans;
+        return ans;
     }
-} 
+}
