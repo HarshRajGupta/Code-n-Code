0a4a70c2d7074c7f76dc1a4b2d1f2c0372de8218
Harsh Raj Gupta <85221003+HarshRajGupta@users.noreply.github.com>
2023-09-29 14:51:43 +0530

    Time: 320 ms (11.21%), Space: 152.5 MB (5.2%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,33 @@
+class Solution 
+{ 
+    int[][] arr = new int[3007][3007];
+    boolean isPalindrome(String s, int l, int r) {
+		// System.out.println(l + " " + r);
+		if (l >= r || l >= s.length() || r < 0)
+			return true;
+		if (arr[l][r] != 0)
+			return arr[l][r] == 1;
+		if (s.charAt(l) != s.charAt(r)) {
+			arr[l][r] = -1;
+			return false;
+		}
+		if (isPalindrome(s, l + 1, r - 1)) {
+			arr[l][r] = 1;
+			return true;
+		} else {
+			arr[l][r] = -1;
+			return false;
+		}
+	}
+    public int countSubstrings(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); ++i) {
+			for (int j = i + 1; j <= s.length(); ++j) {
+				if (isPalindrome(s, i, j - 1)) {
+					++ans;
+				}
+			}
+		}
+		return ans;
+    }
+} 
