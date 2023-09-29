2c7472e12f7288e87da4ececd2d8e9258dd805c8
Harsh Raj Gupta <85221003+HarshRajGupta@users.noreply.github.com>
2023-09-29 15:15:39 +0530

    Time: 12 ms (34.67%), Space: 44.2 MB (11.81%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,33 +1,8 @@
 class Solution 
 { 
-    int[][] arr = new int[1007][1007];
-    boolean isPalindrome(String s, int l, int r) {
-		// System.out.println(l + " " + r);
-		if (l >= r || l >= s.length() || r < 0)
-			return true;
-		if (arr[l][r] != 0)
-			return arr[l][r] == 1;
-		if (s.charAt(l) != s.charAt(r)) {
-			arr[l][r] = -1;
-			return false;
-		}
-		if (isPalindrome(s, l + 1, r - 1)) {
-			arr[l][r] = 1;
-			return true;
-		} else {
-			arr[l][r] = -1;
-			return false;
-		}
-	}
     public int countSubstrings(String s) {
         int ans = s.length();
-        // for (int i = 0; i < s.length(); ++i) {
-		// 	for (int j = i + 1; j <= s.length(); ++j) {
-		// 		if (isPalindrome(s, i, j - 1)) {
-		// 			++ans;
-		// 		}
-		// 	}
-		// }
+         int[][] arr = new int[s.length()][s.length()];
         for(int i = 0; i < s.length() - 1; ++i) {
             arr[i][i] = 1;
             if (s.charAt(i) == s.charAt(i + 1)) {
