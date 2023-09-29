e329a2a816537fe4af449da922c33ff60102afea
Harsh Raj Gupta <85221003+HarshRajGupta@users.noreply.github.com>
2023-09-29 15:13:55 +0530

    Time: 26 ms (19.25%), Space: 55.2 MB (5.2%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -20,14 +20,31 @@ class Solution
 		}
 	}
     public int countSubstrings(String s) {
-        int ans = 0;
-        for (int i = 0; i < s.length(); ++i) {
-			for (int j = i + 1; j <= s.length(); ++j) {
-				if (isPalindrome(s, i, j - 1)) {
-					++ans;
-				}
-			}
-		}
+        int ans = s.length();
+        // for (int i = 0; i < s.length(); ++i) {
+		// 	for (int j = i + 1; j <= s.length(); ++j) {
+		// 		if (isPalindrome(s, i, j - 1)) {
+		// 			++ans;
+		// 		}
+		// 	}
+		// }
+        for(int i = 0; i < s.length(); ++i) {
+            arr[i][i] = 1;
+        }
+        for(int i = 0; i < s.length() - 1; ++i) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                arr[i][i + 1] = 1;
+                ++ans;
+            }
+        }
+        for(int i = 2; i < s.length(); ++i) {
+            for(int j = 0; j < s.length() - i; ++j) {
+                if (s.charAt(j) == s.charAt(j + i) && (arr[j + 1][j + i - 1] == 1)) {
+                    ++ans;
+                    arr[j][j + i] = 1;
+                }
+            }
+        }
 		return ans;
     }
 } 
